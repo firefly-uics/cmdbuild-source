b11f009b004952b1ff97e32a60fe658a75040d4e
Martino Valenti <m.valenti@tecnoteca.it>
2011-03-07 14:38:25 +0100

    Refactoring Improt CSV interface

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/management/utilities/importcsv/CSVGrid.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/management/utilities/importcsv/CSVGrid.js
@@ -17,11 +17,13 @@ CMDBuild.Management.CSVGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 		});
 		
 		this.validFlag = new Ext.form.Checkbox({
+			labelWidth: 0,
+			hideLabel: true,
 			boxLabel: this.translation.shownonvalid,
 			checked: false,		
 			scope: this,
 			handler: function(obj, checked) {			
-				this.filterStore();			
+				this.filterStore();
 			}
 		});
 		
@@ -30,8 +32,7 @@ CMDBuild.Management.CSVGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 		});
 		
 		this.toolBar = new Ext.Toolbar({
-			style: {padding: '5px 0 5px 10px'},
-	        items: [this.validFlag, '-',this.searchField]
+	        items: [this.validFlag, '-', this.searchField]
 		});
 		
 		Ext.apply(this, {
@@ -49,94 +50,95 @@ CMDBuild.Management.CSVGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 	},
 	
 	updateGrid: function(attributes,c){		
-		var attributesToShow = this.setAttributesToShow(attributes, c.headers || [])
-		var editorFields = this.setEditorFields(attributesToShow);
-		var headers = this.setHeaders(attributesToShow, editorFields);
-		var storefields = this.setStorefields(headers);
+		var storeConf = this.setAttributesToShow(attributes, c.headers || []);
 		this.store = new Ext.data.JsonStore({
 			root: "rows",
             totalProperty: 'results',
-	        fields: storefields,
+	        fields: storeConf.dataIndexes,
 	        remoteSort: this.remoteSort
 		});
-		this.updateStoreRecords(c.rows)
-		this.reconfigure(this.store, new Ext.grid.ColumnModel(headers));
-		this.filterStore()
+		
+		this.updateStoreRecords(c.rows);
+		this.reconfigure(this.store, new Ext.grid.ColumnModel(storeConf.headers));
+		this.filterStore();
 	},
 	
-	setEditorFields: function(attributes) {
-		var editorFields = [];
-		for (var i = 0; i < attributes.length; i++) {
-			var attribute = attributes[i];
-			var field = CMDBuild.Management.FieldManager.getFieldForAttr(attribute);
-			if (attribute.type == "LOOKUP" || attribute.type == "REFERENCE")
-				field.on('select', this.onComboSelect, this);
-			if (attribute.type == "REFERENCE") {
-				field.on('cmdbuild-reference-selected', function(record, combo) {
-					var rec = new Ext.data.Record({
-						'Id': record.Id,
-						'Description' : record.Description
-					});
-					this.onComboSelect(combo, rec)
-				}, this );
+	setAttributesToShow: function(attributes, headerToShow) {
+		var out = {
+			dataIndexes: [],
+			headers: []	
+		};
+		
+		for (var j = 0, l=attributes.length; j < l; j++) {
+			var attribute = attributes[j];
+			
+			for (var i = 0 ; i < headerToShow.length ; i++) {
+				if (attribute.name === headerToShow[i]) {
+					var editorField = this.setEditorField(attribute);
+					var hd = this.setHeader(attribute, editorField);
+					out.dataIndexes.push(hd.dataIndex);
+					out.headers.push(hd);
+				}
 			}
-			editorFields.push(field);
 		}
-		return editorFields;
+		return out;
 	},
 	
-	setHeaders: function(attributes, editorfields){
-		var headers = [];
-		for (var i = 0; i < attributes.length; i++) {
-			var attribute = attributes[i];			
-			var cm = this.getColumnModel();
-			var grid = this;			
-			var header = CMDBuild.Management.FieldManager.getHeaderForAttr(attribute);
-			if (attribute.type == "REFERENCE" || attribute.type == "LOOKUP") {
-				var index = header.dataIndex.lastIndexOf("_value");
-				header.dataIndex = header.dataIndex.slice(0,index);
-			}
-			if (header) {
-				header.renderer = this.setRendererCondition.createDelegate(attribute,[header.dataIndex, cm, grid],true);
-				header.hidden = false;
-				header.editor = editorfields[i];
-				headers.push(header);
-			}
-		}		
-		return headers;
-	},
-	
-	setStorefields: function(headers) {
-		var storeFields = [];
-		for (var i = 0; i < headers.length; i++) {
-			var field = headers[i];
-			storeFields[i] = field.dataIndex;
+	setEditorField: function(attribute) {
+		var field = CMDBuild.Management.FieldManager.getFieldForAttr(attribute);
+		if (attribute.type == "LOOKUP" || attribute.type == "REFERENCE") {
+			field.on('select', this.onComboSelect, this);
 		}
-		return storeFields;
+		if (attribute.type == "REFERENCE") {
+			field.on('cmdbuild-reference-selected', function(record, combo) {
+				var rec = new Ext.data.Record({
+					'Id': record.Id,
+					'Description' : record.Description
+				});
+				this.onComboSelect(combo, rec);
+			}, this );
+		}
+		return field;
 	},
 	
+	setHeader: function(attribute, editorfield){
+		var cm = this.getColumnModel();
+		var grid = this;			
+		var header = CMDBuild.Management.FieldManager.getHeaderForAttr(attribute);
+		if (attribute.type == "REFERENCE" || attribute.type == "LOOKUP") {
+			var index = header.dataIndex.lastIndexOf("_value");
+			header.dataIndex = header.dataIndex.slice(0,index);
+		}
+		if (header) {
+			header.renderer = this.setRendererCondition.createDelegate(attribute,[header.dataIndex, cm, grid],true);
+			header.hidden = false;
+			header.editor = editorfield;
+		}
+		return header;
+	},
+		
 	updateStoreRecords: function(rows) {
 		this.store.removeAll();
-		var records = [];
 		for ( var i = 0, len = rows.length ; i < len; i++ ) {
-			var record = new Ext.data.Record(rows[i]);			
-			records[i] = record;
+			this.store.add(new Ext.data.Record(rows[i]));
 		}
-		this.store.add(records);			
 	},
 	
 	filterStore: function() {		
 		this.store.clearFilter(false);
 		var nonValid = this.validFlag.getValue();
 		var query = this.searchField.getRawValue().toUpperCase();
+		
 		if (query == "") {
-			if (nonValid)
+			if (nonValid) {
 				this.store.filterBy(this.isInvalid, this);
+			}
 		} else {
-			if (nonValid)
-				this.store.filterBy(this.isInvalidAndFilterQuery, this)
-			else 
-				this.store.filterBy(this.filterQuery, this)
+			if (nonValid) {
+				this.store.filterBy(this.isInvalidAndFilterQuery, this);
+			} else {
+				this.store.filterBy(this.filterQuery, this);
+			}
 		}
 	},	
 	
@@ -152,18 +154,20 @@ CMDBuild.Management.CSVGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 		var query = this.searchField.getRawValue().toUpperCase();
 		for (var attr in record.data) {
 			var attribute = record.data[attr];
-    		if (attr == 'invalid_fields'){
+    		if (attr == 'invalid_fields') {
     			for (var invalid in attribute) {
     				var attributeInString = (attribute[invalid]+"").toUpperCase();
     				var searchIndex = attributeInString.search(query);
-    				if (searchIndex != -1)
-    	    			return true
+    				if (searchIndex != -1) {
+    	    			return true;
+    				}
     			}
     		} else {
     			var attributeInString = (attribute+"").toUpperCase();
 	    		var searchIndex = attributeInString.search(query);
-	    		if (searchIndex != -1)
-	    			return true
+	    		if (searchIndex != -1) {
+	    			return true;
+	    		}
     		}
     	}
     	return false;
@@ -171,23 +175,10 @@ CMDBuild.Management.CSVGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 	
 	isInvalid: function(record, id) {
 		var invalidFields = record.get("invalid_fields");
-		for (var i in invalidFields)
-			return true
-		return false
-	},
-	
-	setAttributesToShow: function(attributes, headerToShow) {
-		var attrToshow = []
-		for (var j = 0; j < attributes.length; j++) {
-			var attribute = attributes[j];
-			for (var i = 0 ; i < headerToShow.length ; i++) {
-				var tmpAttribute = headerToShow[i];
-				if (attribute.name === tmpAttribute) {
-					attrToshow.push(attribute);
-				}
-			}
+		for (var i in invalidFields) {
+			return true;
 		}
-		return attrToshow;
+		return false;
 	},
 	
 	setRendererCondition: function(value, metadata, record, rowindex, collindex, store, nameColumn, cm, grid) {
@@ -216,26 +207,27 @@ CMDBuild.Management.CSVGrid = Ext.extend(Ext.grid.EditorGridPanel, {
 		var sm = this.getSelectionModel();
 		var gridRecord = sm.selection.record;
 		var storeRecordIndex = this.store.find("Id", gridRecord.data["Id"]);
-		var storeRecord = this.store.getAt(storeRecordIndex);	
-		storeRecord.set(field.name, record.data.Description);	
+		var storeRecord = this.store.getAt(storeRecordIndex);
+		storeRecord.set(field.name, record.data.Description);
 		var index = field.name.lastIndexOf("_value");
-		var name = field.name.slice(0,index);	
-		storeRecord.set(name, record.data.Id);	
+		var name = field.name.slice(0,index);
+		storeRecord.set(name, record.data.Id);
 		gridRecord.set(name, record.data.Id);
 		return false;
-	},	
+	},
 	
 	getRecordToUpload: function(){
 		var records = this.store.getModifiedRecords();
 		var data = [];
-		for (var i = 0, len = records.length ; i < len ; i++) {
+		for (var i = 0, len = records.length; i < len; i++) {
 			data[i] = records[i].data;
 			var changes = records[i].getChanges();
 			var invalid_fields = data[i]['invalid_fields'];
 			
 			for (var invalid in invalid_fields) {
-				if (changes[invalid])
+				if (changes[invalid]) {
 					continue;
+				}
 				data[i][invalid] = invalid_fields[invalid];
 			}
 		}
