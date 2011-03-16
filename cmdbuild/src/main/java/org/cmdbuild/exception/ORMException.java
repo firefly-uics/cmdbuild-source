49a0f2edea9082c7b9ef1320c9909b428fd8757f
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2011-03-16 10:22:45 +0100

    Serbian and French translation update
    Contributed by Miroslav Zaric and Jean Lasson respectively

--- cmdbuild/src/main/java/org/cmdbuild/exception/ORMException.java
+++ cmdbuild/src/main/java/org/cmdbuild/exception/ORMException.java
@@ -53,7 +53,7 @@ public class ORMException extends CMDBException {
 		ORM_ICONS_UNSUPPORTED_TYPE,
 		ORM_POSTGIS_NOT_FOUND,
 		ORM_TABLE_HAS_DOMAIN,
-		ORM_TABLE_HAS_CIHLDREN;
+		ORM_TABLE_HAS_CHILDREN;
 		
 		public ORMException createException(String ... parameters){
 			return new ORMException(this, parameters);
