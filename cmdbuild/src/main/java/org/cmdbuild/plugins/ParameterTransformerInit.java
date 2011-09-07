40897cbd8b5df1872d6d69d5fd80d9d1b3114ace
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2011-09-07 15:14:31 +0200

    #219: Remove useless configuration files

--- cmdbuild/src/main/java/org/cmdbuild/plugins/ParameterTransformerInit.java
+++ cmdbuild/src/main/java/org/cmdbuild/plugins/ParameterTransformerInit.java
@@ -1,13 +1,9 @@
 package org.cmdbuild.plugins;
 
-import java.util.Map.Entry;
-
 import javax.servlet.ServletContext;
 
-import org.cmdbuild.config.DefaultProperties;
 import org.cmdbuild.logger.Log;
 import org.cmdbuild.plugins.CMDBInitListener.CmdbuildModuleLoader;
-import org.cmdbuild.services.Settings;
 import org.cmdbuild.servlets.utils.MethodParameterResolver;
 import org.cmdbuild.servlets.utils.ParameterBuilder;
 import org.cmdbuild.servlets.utils.ParameterTransformer;
@@ -15,37 +11,49 @@ import org.cmdbuild.servlets.utils.Transformer;
 
 public class ParameterTransformerInit implements CmdbuildModuleLoader {
 
+	private static Transformer<?>[] TRANSFORMERS = {
+		new org.cmdbuild.servlets.utils.transformer.FileItemTransformer(),
+		new org.cmdbuild.servlets.utils.transformer.JSONObjectTransformer(),
+		new org.cmdbuild.servlets.utils.transformer.JSONArrayTransformer()
+	};
+
+	private static ParameterBuilder<?>[] BUILDERS = {
+		new org.cmdbuild.servlets.utils.builder.ActivityIdentifierParameter(),
+		new org.cmdbuild.servlets.utils.builder.CardParameter(),
+		new org.cmdbuild.servlets.utils.builder.RelationParameter(),
+		new org.cmdbuild.servlets.utils.builder.TableParameter(),
+		new org.cmdbuild.servlets.utils.builder.BaseSchemaParameter(),
+		new org.cmdbuild.servlets.utils.builder.ProcessTypeParameter(),
+		new org.cmdbuild.servlets.utils.builder.DomainParameter(),
+		new org.cmdbuild.servlets.utils.builder.CardQueryParameter(),
+		new org.cmdbuild.servlets.utils.builder.ProcessQueryParameter(),
+		new org.cmdbuild.servlets.utils.builder.XMLDocumentBuilder(),
+		new org.cmdbuild.servlets.utils.builder.UserContextParameter(),
+		new org.cmdbuild.servlets.utils.builder.factory.TableFactoryParameter(),
+		new org.cmdbuild.servlets.utils.builder.factory.DomainFactoryParameter(),
+		new org.cmdbuild.servlets.utils.builder.factory.RelationFactoryParameter()
+	};
+
 	@SuppressWarnings("unchecked")
 	public void init(ServletContext ctxt) throws Exception {
-		DefaultProperties props = Settings.getInstance().getModule("transformers");
-		Log.OTHER.info("Initialize custom ParameterTransformers");
-		for(Entry entry : props.entrySet()) {
-			String targetClass = (String)entry.getKey();
-			String transformerClass = (String)entry.getValue();
-			Log.OTHER.info("Transformer for: " + targetClass + " : " + transformerClass );
-			try{
-				ParameterTransformer.getInstance().addTransformer( 
-						(Transformer)Class.forName(transformerClass).newInstance() );
-			} catch(Exception e){
-				e.printStackTrace();
-				Log.OTHER.error("Cannot load ParameterTransformer for '" + targetClass + "' ('" + transformerClass + "')");
+		Log.OTHER.info("Initializing ParameterTransformers");
+		for (Transformer transformer : TRANSFORMERS) {
+			try {
+				ParameterTransformer.getInstance().addTransformer(transformer);
+				Log.OTHER.info("Transformer for " + transformer.getTransformedClass().getName() + ": " + transformer.getClass().getName());
+			} catch (Exception e) {
+				Log.OTHER.error("Cannot load ParameterTransformer " + transformer.getClass().getCanonicalName());
 			}
 		}
-		
-		props = Settings.getInstance().getModule("builders");
+
 		Log.OTHER.info("Initializer custom ParameterBuilders");
-		for(Entry entry : props.entrySet()) {
-			String targetClass = (String)entry.getKey();
-			String builderClass = (String)entry.getValue();
-			Log.OTHER.info("Builder for: " + targetClass + ": " + builderClass);
-			try{
-				MethodParameterResolver.getInstance().putAutoloadParameter(
-						(ParameterBuilder)Class.forName(builderClass).newInstance());
-			} catch(Exception e){
-				e.printStackTrace();
-				Log.OTHER.error("Cannot load ParameterBuilder for '" + targetClass + "' ('" + builderClass + "')");
+		for (ParameterBuilder<?> builder : BUILDERS) {
+			try {
+				MethodParameterResolver.getInstance().putAutoloadParameter(builder);
+				Log.OTHER.info("Builder for " + builder.getBindedClass().getName() + ": " + builder.getClass().getName());
+			} catch (Exception e) {
+				Log.OTHER.error("Cannot load ParameterBuilder " + builder.getClass().getCanonicalName());
 			}
 		}
 	}
-
 }
