77df3450c4055e46389e867bf14a40c6a7d3cd5c
Davide Pavan <d.pavan@tecnoteca.com>
2011-12-14 12:18:56 +0100

    #316: Date/time/timestamp format

--- cmdbuild/src/main/java/org/cmdbuild/dao/attribute/TimeAttribute.java
+++ cmdbuild/src/main/java/org/cmdbuild/dao/attribute/TimeAttribute.java
@@ -9,12 +9,10 @@ import org.cmdbuild.exception.ORMException.ORMExceptionType;
 
 public class TimeAttribute extends AbstractDateAttribute {
 
-	public static final String JSON_DATETIME_FORMAT="HH:mm:ss";
-	public static final String SOAP_DATETIME_FORMAT="yyyy-MM-dd'T'HH:mm:ss";
-	public static final String REST_DATETIME_FORMAT="yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-	public static final String POSTGRES_DATETIME_FORMAT="HH:mm:ss";
+	public static final String JSON_DATETIME_FORMAT = "HH:mm:ss";
+	public static final String POSTGRES_DATETIME_FORMAT = "HH:mm:ss";
 
-	public TimeAttribute(BaseSchema schema, String name, Map<String, String> meta) {
+	public TimeAttribute(final BaseSchema schema, final String name, final Map<String, String> meta) {
 		super(schema, name, meta);
 	}
 
@@ -24,14 +22,12 @@ public class TimeAttribute extends AbstractDateAttribute {
 	}
 
 	@Override
-	protected Object convertValue(Object value) {
+	protected Object convertValue(final Object value) {
 		Date dateValue;
 		if (value instanceof Date) {
 			dateValue = (Date) value;
 		} else if (value instanceof String) {
-			dateValue = convertDateString((String) value,
-					JSON_DATETIME_FORMAT,
-					SOAP_DATETIME_FORMAT,
+			dateValue = convertDateString((String) value, JSON_DATETIME_FORMAT, SOAP_DATETIME_FORMAT,
 					REST_DATETIME_FORMAT);
 		} else {
 			throw ORMExceptionType.ORM_TYPE_ERROR.createException();
@@ -40,12 +36,12 @@ public class TimeAttribute extends AbstractDateAttribute {
 	}
 
 	@Override
-	public String notNullValueToDBFormat(Object value) {
-		return escapeAndQuote(new SimpleDateFormat(POSTGRES_DATETIME_FORMAT).format((Date)value));
+	public String notNullValueToDBFormat(final Object value) {
+		return escapeAndQuote(new SimpleDateFormat(POSTGRES_DATETIME_FORMAT).format((Date) value));
 	}
 
 	@Override
-	public String notNullValueToString(Object value) {
-		return new SimpleDateFormat(JSON_DATETIME_FORMAT).format((Date)value);
+	public String notNullValueToString(final Object value) {
+		return new SimpleDateFormat(JSON_DATETIME_FORMAT).format((Date) value);
 	}
 }
