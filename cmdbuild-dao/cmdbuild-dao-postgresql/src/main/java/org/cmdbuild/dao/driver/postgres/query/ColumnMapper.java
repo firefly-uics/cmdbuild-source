925a4aa3341f98bb840b99fb7ca444365264056a
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2011-12-20 13:00:09 +0100

    #188: New DAO
    Fixed toString loop, enhanced exception clarity

--- cmdbuild-dao/cmdbuild-dao-postgresql/src/main/java/org/cmdbuild/dao/driver/postgres/query/ColumnMapper.java
+++ cmdbuild-dao/cmdbuild-dao-postgresql/src/main/java/org/cmdbuild/dao/driver/postgres/query/ColumnMapper.java
@@ -43,6 +43,11 @@ public class ColumnMapper {
 			this.sqlType = sqlType;
 			this.sqlTypeString = sqlTypeString;
 		}
+
+		@Override
+		public String toString() {
+			return String.format("[%s,%s,%s]", name, alias, sqlType);
+		}
 	}
 
 	private static class AliasAttributes {
@@ -100,6 +105,11 @@ public class ColumnMapper {
 		Iterable<CMEntryType> getEntryTypes() {
 			return map.keySet();
 		}
+
+		@Override
+		public String toString() {
+			return map.toString();
+		}
 	}
 
 	private static class AliasStore {
@@ -119,6 +129,11 @@ public class ColumnMapper {
 
 		Set<Alias> getAliases() {
 			return map.keySet();
+		}
+
+		@Override
+		public String toString() {
+			return map.toString();
 		}
 	}
 
@@ -218,5 +233,10 @@ public class ColumnMapper {
 
 	public Set<Alias> getDomainAliases() {
 		return domainAliases.getAliases();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[Classes=%s,Domains=%s]", classAliases, domainAliases);
 	}
 }
