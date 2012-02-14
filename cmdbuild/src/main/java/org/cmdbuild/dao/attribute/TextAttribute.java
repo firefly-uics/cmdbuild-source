3072d20a7557418dd9184b566c91ee195d6506d4
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2012-02-14 15:23:41 +0100

    #418: [Fix] Old text attributes appear as HTML
    The previous fix was writing the editor type on string and char attributes

--- cmdbuild/src/main/java/org/cmdbuild/dao/attribute/TextAttribute.java
+++ cmdbuild/src/main/java/org/cmdbuild/dao/attribute/TextAttribute.java
@@ -2,13 +2,9 @@ package org.cmdbuild.dao.attribute;
 
 import java.util.Map;
 
-import org.cmdbuild.dao.entrytype.attributetype.CMAttributeType;
-import org.cmdbuild.dao.entrytype.attributetype.TextAttributeType;
 import org.cmdbuild.elements.interfaces.BaseSchema;
 
-public class TextAttribute extends DaoWrapperAttribute {
-
-	private static CMAttributeType<?> TEXT_TYPE = new TextAttributeType();
+public class TextAttribute extends AbstractTextAttribute {
 
 	private enum ALLOWED_TYPES {
 		PLAIN,
@@ -17,17 +13,11 @@ public class TextAttribute extends DaoWrapperAttribute {
 
 	public TextAttribute(BaseSchema schema, String name, Map<String, String> meta) {
 		super(schema, name, meta);
-		daoType = TEXT_TYPE;
 	}
 
 	@Override
-	public AttributeType getType() {
+	public final AttributeType getType() {
 		return AttributeType.TEXT;
-	}
-
-	@Override
-	public String notNullValueToDBFormat(Object value) {
-		return escapeAndQuote((String)value);
 	}
 
 	@Override
