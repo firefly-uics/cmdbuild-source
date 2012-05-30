f9a68be5f8a11148d551201c664d4fd45695fdc5
Davide Pavan <d.pavan@tecnoteca.com>
2012-05-30 11:54:19 +0200

    #455: Shark Complete Rewrite - Added transitions between activities

---
+++ shark/commons/src/main/java/org/cmdbuild/workflow/xpdl/XpdlTransition.java
@@ -0,0 +1,16 @@
+package org.cmdbuild.workflow.xpdl;
+
+import org.apache.commons.lang.Validate;
+import org.enhydra.jxpdl.elements.Transition;
+
+public class XpdlTransition {
+
+	@SuppressWarnings("unused")
+	private final Transition inner;
+
+	public XpdlTransition(final Transition transition) {
+		Validate.notNull(transition);
+		inner = transition;
+	}
+
+}
