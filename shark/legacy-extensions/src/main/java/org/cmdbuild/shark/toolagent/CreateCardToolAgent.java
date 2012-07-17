70188e2e5421f3073f93e5ef279b79f9ddc18012
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2012-07-17 12:15:15 +0200

    #455: Shark Complete Rewrite - Tool Agents and new APIs
    selectReferenceByCode selectReferenceByCustomAttribute

--- shark/legacy-extensions/src/main/java/org/cmdbuild/shark/toolagent/CreateCardToolAgent.java
+++ shark/legacy-extensions/src/main/java/org/cmdbuild/shark/toolagent/CreateCardToolAgent.java
@@ -5,14 +5,10 @@ import static java.util.Arrays.asList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.cmdbuild.common.Constants;
 import org.cmdbuild.workflow.type.ReferenceType;
 import org.enhydra.shark.api.internal.toolagent.AppParameter;
-
 public class CreateCardToolAgent extends AbstractConditionalToolAgent {
-
-	// Move these in a common place
-	private static final String CMDBUILD_CODE_ATTRIBUTE = "Code";
-	private static final String CMDBUILD_DESCRIPTION_ATTRIBUTE = "Description";
 
 	private static final String CREATE_CARD = "createCard";
 	private static final String CREATE_CARD_REF = "createCardRef";
@@ -28,7 +24,7 @@ public class CreateCardToolAgent extends AbstractConditionalToolAgent {
 		
 		final int newCardId = getWorkflowApi().createCard(classname, attributes);
 
-		final String description =  String.valueOf(attributes.get(CMDBUILD_DESCRIPTION_ATTRIBUTE));
+		final String description =  String.valueOf(attributes.get(Constants.DESCRIPTION_ATTRIBUTE));
 		for (final AppParameter parmOut : getReturnParameters()) {
 			if (parmOut.the_class == Long.class) {
 				parmOut.the_value = newCardId;
@@ -54,9 +50,9 @@ public class CreateCardToolAgent extends AbstractConditionalToolAgent {
 		final Map<String, Object> attributes = new HashMap<String, Object>();
 		if (asList(CREATE_CARD, CREATE_CARD_REF).contains(getId())) {
 			final String code = getParameterValue(CARD_CODE);
-			attributes.put(CMDBUILD_CODE_ATTRIBUTE, code);
+			attributes.put(Constants.CODE_ATTRIBUTE, code);
 			final String description = getParameterValue(CARD_DESCRIPTION);
-			attributes.put(CMDBUILD_DESCRIPTION_ATTRIBUTE, description);
+			attributes.put(Constants.DESCRIPTION_ATTRIBUTE, description);
 		} else {
 			for (final Map.Entry<String, Object> entry : getInputParameterValues().entrySet()) {
 				if (CLASS_NAME.equals(entry.getKey()))
