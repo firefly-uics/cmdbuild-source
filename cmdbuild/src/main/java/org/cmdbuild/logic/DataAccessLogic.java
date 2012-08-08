4ab34f8a039070513fa6e85c25dc10640082eaae
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2012-08-08 16:00:51 +0200

    #455: Shark Complete Rewrite
    Fixed submission of link cards widget on superclasses

--- cmdbuild/src/main/java/org/cmdbuild/logic/DataAccessLogic.java
+++ cmdbuild/src/main/java/org/cmdbuild/logic/DataAccessLogic.java
@@ -1,19 +1,17 @@
 package org.cmdbuild.logic;
 
-import static org.cmdbuild.dao.query.clause.QueryAliasAttribute.attribute;
-
-import org.cmdbuild.common.Constants;
+import org.cmdbuild.common.annotations.Legacy;
 import org.cmdbuild.dao.entry.CMCard;
-import org.cmdbuild.dao.entrytype.CMClass;
-import org.cmdbuild.dao.query.CMQueryResult;
-import org.cmdbuild.dao.query.clause.where.SimpleWhereClause.Operator;
+import org.cmdbuild.dao.legacywrappers.CardWrapper;
 import org.cmdbuild.dao.view.CMDataView;
+import org.cmdbuild.elements.interfaces.ICard;
 import org.cmdbuild.logic.LogicDTO.Card;
 import org.cmdbuild.logic.LogicDTO.DomainWithSource;
 import org.cmdbuild.logic.commands.GetRelationHistory;
 import org.cmdbuild.logic.commands.GetRelationHistory.GetRelationHistoryResponse;
 import org.cmdbuild.logic.commands.GetRelationList;
 import org.cmdbuild.logic.commands.GetRelationList.GetRelationListResponse;
+import org.cmdbuild.services.auth.UserContext;
 
 /**
  * Business Logic Layer for Data Access
@@ -34,7 +32,16 @@ public class DataAccessLogic {
 		return new GetRelationHistory(view).exec(srcCard);
 	}
 
+	@Legacy("IMPORTANT! FIX THE NEW DAO AND FIX BECAUSE IT USES THE SYSTEM USER!")
 	public CMCard getCard(final String className, final Object cardId) {
+		try {
+			int id = Integer.parseInt(cardId.toString()); // very expensive but almost never called
+			final ICard card = UserContext.systemContext().tables().get(className).cards().get(id);
+			return new CardWrapper(card);
+		} catch (Exception e) {
+			return null;
+		}
+		/* The new DAO layer does not query subclasses! ****************
 		final CMClass cardType = view.findClassByName(className);
 		final CMQueryResult result = view.select(
 				attribute(cardType, Constants.DESCRIPTION_ATTRIBUTE))
@@ -46,5 +53,6 @@ public class DataAccessLogic {
 		} else {
 			return result.iterator().next().getCard(cardType);
 		}
+		**************************************************************** */
 	}
 }
