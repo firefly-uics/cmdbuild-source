d333583b4a95d8db0e079efe836000e5ae0d97b0
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2012-08-09 14:40:55 +0200

    #517: Predefined Dashboard Functions

--- cmdbuild/src/main/webapp/WEB-INF/patches/2.0.0-04.sql
+++ cmdbuild/src/main/webapp/WEB-INF/patches/2.0.0-04.sql
@@ -1,19 +1,4 @@
---
--- Put here what the PM decides that should be added to the empty database
---
-
-
--- 
--- Predefined DMS categories
--- 
-
-INSERT INTO "LookUp" ("IdClass","Type", "Number", "Description", "IsDefault", "Status") VALUES ('"LookUp"'::regclass,'AlfrescoCategory', 1, 'Document', true, 'A');
-INSERT INTO "LookUp" ("IdClass","Type", "Number", "Description", "IsDefault", "Status") VALUES ('"LookUp"'::regclass,'AlfrescoCategory', 2, 'Image', false, 'A');
-
-
--- 
--- Custom Functions
--- 
+-- A few Dashboard Functions
 
 CREATE OR REPLACE FUNCTION _cmf_class_description(cid oid) RETURNS character varying AS $$
     SELECT _cm_read_comment(_cm_comment_for_table_id($1), 'DESCR');
