bf6d464ffc1f58ec02b9f3acc9810871e8d1ba62
Paolo Ambrosio <p.ambrosio@tecnoteca.com>
2012-08-30 18:31:45 +0200

    #455: Shark Complete Rewrite
    Set all reference output attributes with the current group reference

--- shark/legacy-extensions/src/main/java/org/cmdbuild/shark/toolagent/GetCurrentGroupReferenceToolAgent.java
+++ shark/legacy-extensions/src/main/java/org/cmdbuild/shark/toolagent/GetCurrentGroupReferenceToolAgent.java
@@ -2,15 +2,31 @@ package org.cmdbuild.shark.toolagent;
 
 import org.cmdbuild.workflow.Constants;
 import org.cmdbuild.workflow.type.ReferenceType;
+import org.enhydra.shark.api.internal.toolagent.AppParameter;
 
 public class GetCurrentGroupReferenceToolAgent extends AbstractConditionalToolAgent {
-
-	private static final String OUTPUT = "GroupRef";
 
 	@Override
 	protected void innerInvoke() throws Exception {
 		final ReferenceType groupReference = getProcessAttributeValue(Constants.CURRENT_GROUP_VARIABLE);
-		setParameterValue(OUTPUT, groupReference);
+		setAllReferenceOutputParametersTo(groupReference);
+	}
+
+	/**
+	 * Set all reference output attributes with the current group reference.
+	 * 
+	 * It should have been just GroupRef but in VERY old process definitions
+	 * the output parameter was GroupName so, what the hell, we don't care
+	 * about the parameter name as long as it is a reference. Good boy!
+	 * 
+	 * @param groupReference
+	 */
+	private void setAllReferenceOutputParametersTo(final ReferenceType groupReference) {
+		for (final AppParameter parmOut : getReturnParameters()) {
+			if (parmOut.the_class == ReferenceType.class) {
+				parmOut.the_value = groupReference;
+			}
+		}
 	}
 
 }
