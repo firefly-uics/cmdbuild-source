dace45f82d50e4998ad90c96eb7a2561094ff4fc
Stefano Forgiarini <s.forgiarini@tecnoteca.com>
2012-12-03 15:06:29 +0100

    #556: New-DAO full integration
    Integration tests are now all ok. Improve performances of AuthenticationLogicTest

--- auth/default/src/main/java/org/cmdbuild/auth/DBGroupFetcher.java
+++ auth/default/src/main/java/org/cmdbuild/auth/DBGroupFetcher.java
@@ -1,5 +1,8 @@
 package org.cmdbuild.auth;
 
+import static org.cmdbuild.dao.query.clause.AnyAttribute.anyAttribute;
+import static org.cmdbuild.dao.query.clause.alias.Alias.as;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,39 +14,24 @@ import org.cmdbuild.auth.acl.CMPrivilege;
 import org.cmdbuild.auth.acl.CMPrivilegedObject;
 import org.cmdbuild.auth.acl.DefaultPrivileges;
 import org.cmdbuild.auth.acl.GroupImpl;
-import org.cmdbuild.auth.acl.PrivilegePair;
 import org.cmdbuild.auth.acl.GroupImpl.GroupImplBuilder;
+import org.cmdbuild.auth.acl.PrivilegePair;
 import org.cmdbuild.dao.entry.CMCard;
 import org.cmdbuild.dao.entrytype.CMClass;
 import org.cmdbuild.dao.query.CMQueryResult;
 import org.cmdbuild.dao.query.CMQueryRow;
+import org.cmdbuild.dao.query.clause.AnyAttribute;
 import org.cmdbuild.dao.query.clause.alias.Alias;
 import org.cmdbuild.dao.reference.EntryTypeReference;
 import org.cmdbuild.dao.view.CMDataView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Lists;
-
-import static org.cmdbuild.dao.query.clause.AnyAttribute.anyAttribute;
-import static org.cmdbuild.dao.query.clause.QueryAliasAttribute.attribute;
-import static org.cmdbuild.dao.query.clause.alias.Alias.as;
-
 public class DBGroupFetcher implements GroupFetcher {
 
 	private final CMDataView view;
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
-	
-	private static final String GROUP_ATTRIBUTE_CODE = "Code";
-	private static final String GROUP_ATTRIBUTE_DESCRIPTION = "Description";
-	private static final String GROUP_ATTRIBUTE_EMAIL = "Email";
-	private static final String GROUP_ATTRIBUTE_DISABLED_MODULES = "DisabledModules";
-	private static final String GROUP_ATTRIBUTE_ADMINISTRATOR = "Administrator";
-	private static final String GROUP_ATTRIBUTE_STARTING_CLASS = "StartingClass";
-	private static final String GROUP_ATTRIBUTE_PRIVILEGE_TYPE = "Mode";
-	private static final String GROUP_ATTRIBUTE_PRIVILEGE_GROUP_ID = "IdRole";
-	private static final String GROUP_ATTRIBUTE_PRIVILEGE_CLASS_ID = "IdGrantedClass";
-	
+
 	private static final String DB_READ_PRIVILEGE = "r";
 	private static final String DB_WRITE_PRIVILEGE = "w";
 
@@ -51,29 +39,22 @@ public class DBGroupFetcher implements GroupFetcher {
 		Validate.notNull(view);
 		this.view = view;
 	}
-	
+
 	public Map<Long, CMGroup> fetchAllGroupIdToGroup() {
 		final Map<Long, CMGroup> groupCards = new HashMap<Long, CMGroup>();
 		final Map<Object, List<PrivilegePair>> allPrivileges = fetchAllPrivileges();
 		final Alias groupClassAlias = Alias.canonicalAlias(groupClass());
-		final CMQueryResult groupRows = view
-				.select(
-				// FIXME: anyAttribute()
-				attribute(groupClassAlias, GROUP_ATTRIBUTE_CODE),
-						attribute(groupClassAlias, GROUP_ATTRIBUTE_DESCRIPTION),
-						attribute(groupClassAlias, GROUP_ATTRIBUTE_ADMINISTRATOR),
-						attribute(groupClassAlias, GROUP_ATTRIBUTE_DISABLED_MODULES),
-						attribute(groupClassAlias, GROUP_ATTRIBUTE_STARTING_CLASS))
+		final CMQueryResult groupRows = view.select(AnyAttribute.anyAttribute(groupClass())) //
 				.from(groupClass(), as(groupClassAlias)).run();
 		for (final CMQueryRow row : groupRows) {
 			final CMCard groupCard = row.getCard(groupClassAlias);
 			final Long groupId = groupCard.getId();
-			final Object groupDescription = groupCard.get(GROUP_ATTRIBUTE_DESCRIPTION);
+			final Object groupDescription = groupCard.get(groupDescriptionAttribute());
 			final GroupImplBuilder groupBuilder = GroupImpl.newInstance().withId(groupId)
-					.withName(groupCard.get(GROUP_ATTRIBUTE_CODE).toString())
+					.withName(groupCard.get(groupNameAttribute()).toString())
 					.withDescription(groupDescription != null ? groupDescription.toString() : null);
 
-			final boolean groupIsGod = Boolean.TRUE.equals(groupCard.get(GROUP_ATTRIBUTE_ADMINISTRATOR));
+			final boolean groupIsGod = Boolean.TRUE.equals(groupCard.get(groupIsGodAttribute()));
 			if (groupIsGod) {
 				groupBuilder.withPrivilege(new PrivilegePair(DefaultPrivileges.GOD));
 			} else if (allPrivileges.containsKey(groupId)) {
@@ -83,20 +64,21 @@ public class DBGroupFetcher implements GroupFetcher {
 				}
 			}
 
-			final EntryTypeReference classReference = (EntryTypeReference) groupCard.get(GROUP_ATTRIBUTE_STARTING_CLASS);
+			final EntryTypeReference classReference = (EntryTypeReference) groupCard.get(groupStartingClassAttribute());
 			if (classReference != null) {
 				groupBuilder.withStartingClassId(classReference.getId());
 			}
-			groupBuilder.withEmail(groupCard.get(GROUP_ATTRIBUTE_EMAIL).toString());
+			Object emailAddress = groupCard.get(groupEmailAttribute());
+			groupBuilder.withEmail(emailAddress != null ? emailAddress.toString() : null);
 			groupBuilder.active(true);
 			groupBuilder.administrator(groupIsGod);
 			groupCards.put(groupId, groupBuilder.build());
 		}
 		return groupCards;
 	}
-	
+
 	private String[] getDisabledModules(final CMCard groupCard) {
-		final String groupDisabledModules = (String) groupCard.get(GROUP_ATTRIBUTE_DISABLED_MODULES);
+		final String groupDisabledModules = (String) groupCard.get(groupDisabledModulesAttribute());
 		if (groupDisabledModules != null) {
 			final int start = groupDisabledModules.lastIndexOf("{"), end = groupDisabledModules.indexOf("{");
 			if (start == 0 && end == (groupDisabledModules.length() - 1)) {
@@ -118,12 +100,12 @@ public class DBGroupFetcher implements GroupFetcher {
 			final CMCard privilegeCard = row.getCard(privilegeClassAlias);
 			final CMPrivilegedObject privObject = extractPrivilegeId(privilegeCard);
 			final CMPrivilege privilege = extractPrivilegeType(privilegeCard);
-			final Object groupId = privilegeCard.get(GROUP_ATTRIBUTE_PRIVILEGE_GROUP_ID);
+			final Object groupId = privilegeCard.get(privilegeGroupIdAttribute());
 			if (privObject == null || privilege == null || groupId == null) {
 				logger.warn(
 						"Skipping privilege pair (%s,%s) for group %s",
-						new Object[] { privilegeCard.get(GROUP_ATTRIBUTE_PRIVILEGE_CLASS_ID),
-								privilegeCard.get(GROUP_ATTRIBUTE_PRIVILEGE_TYPE), groupId });
+						new Object[] { privilegeCard.get(privilegeClassIdAttribute()),
+								privilegeCard.get(privilegeTypeAttribute()), groupId });
 			} else {
 				List<PrivilegePair> privList = allPrivileges.get(groupId);
 				if (privList == null) {
@@ -135,14 +117,14 @@ public class DBGroupFetcher implements GroupFetcher {
 		}
 		return allPrivileges;
 	}
-	
+
 	private CMPrivilegedObject extractPrivilegeId(final CMCard privilegeCard) {
-		final EntryTypeReference etr = (EntryTypeReference) privilegeCard.get(GROUP_ATTRIBUTE_PRIVILEGE_CLASS_ID);
+		final EntryTypeReference etr = (EntryTypeReference) privilegeCard.get(privilegeClassIdAttribute());
 		return view.findClassById(etr.getId());
 	}
 
 	private CMPrivilege extractPrivilegeType(final CMCard privilegeCard) {
-		final Object type = privilegeCard.get(GROUP_ATTRIBUTE_PRIVILEGE_TYPE);
+		final Object type = privilegeCard.get(privilegeTypeAttribute());
 		if (DB_READ_PRIVILEGE.equals(type)) {
 			return DefaultPrivileges.READ;
 		} else if (DB_WRITE_PRIVILEGE.equals(type)) {
@@ -150,18 +132,55 @@ public class DBGroupFetcher implements GroupFetcher {
 		}
 		return null;
 	}
-	
+
 	private CMClass privilegeClass() {
 		return view.findClassByName("Grant");
 	}
-	
+
 	private CMClass groupClass() {
 		return view.findClassByName("Role");
 	}
-	
+
 	public Iterable<CMGroup> fetchAllGroups() {
 		Map<Long, CMGroup> groupIdToGroup = fetchAllGroupIdToGroup();
 		return groupIdToGroup.values();
 	}
 
+	private String groupNameAttribute() {
+		return "Code";
+	}
+
+	private String groupEmailAttribute() {
+		return "Email";
+	}
+
+	private String groupDescriptionAttribute() {
+		return "Description";
+	}
+
+	private String groupIsGodAttribute() {
+		return "Administrator";
+	}
+
+	private String groupDisabledModulesAttribute() {
+		return "DisabledModules";
+	}
+
+	private String groupStartingClassAttribute() {
+		return "startingClass";
+	}
+
+	private String privilegeGroupIdAttribute() {
+		return "IdRole";
+	}
+
+	private String privilegeClassIdAttribute() {
+		return "IdGrantedClass";
+	}
+
+	private String privilegeTypeAttribute() {
+		return "Mode";
+	}
+
 }
+
