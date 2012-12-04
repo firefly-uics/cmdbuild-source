a330421c2b475773d329c9c7c06dd82a497daddc
Alberto Borsetta <a.borsetta@tecnoteca.com>
2012-12-04 15:31:52 +0100

    #558: [Fix] SelectAttributeToolAgent returns integer values instead of string

--- shark/legacy-extensions/src/main/java/org/cmdbuild/shark/toolagent/SelectAttributeToolAgent.java
+++ shark/legacy-extensions/src/main/java/org/cmdbuild/shark/toolagent/SelectAttributeToolAgent.java
@@ -1,5 +1,8 @@
 package org.cmdbuild.shark.toolagent;
 
+import org.cmdbuild.workflow.type.LookupType;
+import org.cmdbuild.workflow.type.ReferenceType;
+
 public class SelectAttributeToolAgent extends ManageAttributeToolAgent {
 
 	private static final String ATTRIBUTE_VALUE = "AttributeValue";
@@ -11,7 +14,14 @@ public class SelectAttributeToolAgent extends ManageAttributeToolAgent {
 	
 	@Override
 	protected Object attributeValue() {
-		return super.attributeValue().toString();
+		final Object value = super.attributeValue();
+		if (value.getClass().equals(LookupType.class)) {
+			return ((Integer) ((LookupType) value).getId()).toString();
+		} else if (value.getClass().equals(ReferenceType.class)) {
+			return ((Integer) ((ReferenceType) value).getId()).toString();
+		} else {
+			return value.toString();
+		}
 	}
 
 }
