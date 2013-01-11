c7275ce1c8000ae7e907e985b96eb606f8ecdc00
Stefano Forgiarini <s.forgiarini@tecnoteca.com>
2013-01-11 17:28:05 +0100

    #556: New-DAO full integration
    Added new attribute to table _Filters

--- cmdbuild/src/main/webapp/WEB-INF/patches/2.1.0-04.sql
+++ cmdbuild/src/main/webapp/WEB-INF/patches/2.1.0-04.sql
@@ -5,5 +5,9 @@ SELECT cm_create_class_attribute('_Filters', 'Master', 'int', null, false, false
 SELECT cm_modify_class_attribute('_Filters', 'Code', 'varchar', null, true, false, 'MODE: write|DESCR: Name|INDEX: 2|STATUS: active');
 SELECT cm_modify_class_attribute('_Filters', 'Description', 'varchar', null, false, false, 'MODE: write|DESCR: Description|INDEX: 3|STATUS: active');
 SELECT cm_create_class_attribute('_Filters', 'Filter', 'varchar', null, false, false, 'MODE: write|DESCR: Filter|INDEX: 4|STATUS: active');
+SELECT cm_create_class_attribute('_Filters', 'TableId', 'regclass', null, true, false, 'MODE: write|DESCR: Class Reference|INDEX: 5|STATUS: active');
+
+ALTER TABLE "_Filters" ADD CONSTRAINT filter_name_table_unique UNIQUE ("Code", "Master", "TableId");
+
 
 
