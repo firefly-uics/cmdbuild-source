e717e23d835928b20c81350720192142d043b4f5
Stefano Forgiarini <s.forgiarini@tecnoteca.com>
2013-02-22 09:29:06 +0100

    #556: New-DAO full integration
    Setup and Configuration are integrated with new dao

--- cmdbuild/src/main/java/org/cmdbuild/servlets/json/schema/Setup.java
+++ cmdbuild/src/main/java/org/cmdbuild/servlets/json/schema/Setup.java
@@ -3,12 +3,10 @@ package org.cmdbuild.servlets.json.schema;
 import java.io.IOException;
 import java.util.Map;
 
-import org.cmdbuild.common.annotations.OldDao;
 import org.cmdbuild.config.DefaultProperties;
 import org.cmdbuild.exception.AuthException;
-import org.cmdbuild.services.SessionVars;
+import org.cmdbuild.logic.TemporaryObjectsBeforeSpringDI;
 import org.cmdbuild.services.Settings;
-import org.cmdbuild.services.auth.UserContext;
 import org.cmdbuild.servlets.json.JSONBase;
 import org.cmdbuild.servlets.json.JSONBase.Admin.AdminAccess;
 import org.cmdbuild.servlets.utils.Parameter;
@@ -17,19 +15,16 @@ import org.json.JSONObject;
 
 public class Setup extends JSONBase {
 
-	@OldDao
 	@JSONExported
 	@Unauthorized
-	public JSONObject getConfiguration(
-			@Parameter("name") String nameOfConfigFile,
-			JSONObject serializer,
-			UserContext userCtx
-		) throws JSONException, AuthException {
-		DefaultProperties module = Settings.getInstance().getModule(nameOfConfigFile);
-		boolean userIsAdmin = (userCtx != null && userCtx.privileges().isAdmin()); //FIXME with new dao
-		JSONObject data = new JSONObject();
-		for(Object keyObject : module.keySet()) {
-			String key = keyObject.toString();
+	public JSONObject getConfiguration(@Parameter("name") final String nameOfConfigFile, //
+			final JSONObject serializer //
+	) throws JSONException, AuthException {
+		final DefaultProperties module = Settings.getInstance().getModule(nameOfConfigFile);
+		final boolean userIsAdmin = TemporaryObjectsBeforeSpringDI.getOperationUser().hasAdministratorPrivileges();
+		final JSONObject data = new JSONObject();
+		for (final Object keyObject : module.keySet()) {
+			final String key = keyObject.toString();
 			if (userIsAdmin || !key.endsWith("password"))
 				data.put(key, module.get(key));
 		}
@@ -39,21 +34,19 @@ public class Setup extends JSONBase {
 
 	@JSONExported
 	@Admin(AdminAccess.DEMOSAFE)
-	public void saveConfiguration (
-			@Parameter("name") String nameOfConfigFile,
-			Map<String,String> requestParams
-		) throws IOException {
-	    DefaultProperties module = Settings.getInstance().getModule(nameOfConfigFile);
-	    for (Object keyObject : module.keySet()) {
-			String key = keyObject.toString();
+	public void saveConfiguration(@Parameter("name") final String nameOfConfigFile,
+			final Map<String, String> requestParams) throws IOException {
+		final DefaultProperties module = Settings.getInstance().getModule(nameOfConfigFile);
+		for (final Object keyObject : module.keySet()) {
+			final String key = keyObject.toString();
 			if (requestParams.containsKey(key)) {
-			    String value = requestParams.get(key);
-			    if (value == null) {
-			    	value = "";
-			    }
-			    module.setProperty(key, value);
+				String value = requestParams.get(key);
+				if (value == null) {
+					value = "";
+				}
+				module.setProperty(key, value);
 			}
-	    }
-	    module.store();
+		}
+		module.store();
 	}
 }
