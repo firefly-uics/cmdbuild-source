4c2dc94fa8d77ed4e516823af367f74aee097af4
Davide Pavan <d.pavan@tecnoteca.com>
2013-03-07 17:43:55 +0100

    #556: New-DAO full integration
    Cosmetic changes.

--- cmdbuild/src/main/java/org/cmdbuild/model/data/Card.java
+++ cmdbuild/src/main/java/org/cmdbuild/model/data/Card.java
@@ -5,6 +5,8 @@ import static org.apache.commons.lang.StringUtils.isNotBlank;
 import java.util.Map;
 
 import org.apache.commons.lang.Validate;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 import org.cmdbuild.common.Builder;
 import org.cmdbuild.services.store.Store.Storable;
 import org.joda.time.DateTime;
@@ -13,7 +15,7 @@ import com.google.common.collect.Maps;
 
 public class Card implements Storable {
 
-	public static class CardDTOBuilder implements Builder<Card> {
+	public static class CardBuilder implements Builder<Card> {
 
 		private Long id;
 		private String className;
@@ -23,7 +25,7 @@ public class Card implements Storable {
 		private String user;
 		private Map<String, Object> attributes = Maps.newHashMap();
 
-		public CardDTOBuilder clone(final Card card) {
+		public CardBuilder clone(final Card card) {
 			this.id = card.id;
 			this.className = card.className;
 			this.classId = card.classId;
@@ -34,47 +36,47 @@ public class Card implements Storable {
 			return this;
 		}
 
-		public CardDTOBuilder withId(final Long value) {
+		public CardBuilder withId(final Long value) {
 			this.id = value;
 			return this;
 		}
 
-		public CardDTOBuilder withClassName(final String value) {
+		public CardBuilder withClassName(final String value) {
 			this.className = value;
 			return this;
 		}
 
-		public CardDTOBuilder withClassId(final Long value) {
+		public CardBuilder withClassId(final Long value) {
 			this.classId = value;
 			return this;
 		}
 
-		public CardDTOBuilder withBeginDate(final DateTime value) {
+		public CardBuilder withBeginDate(final DateTime value) {
 			this.begin = value;
 			return this;
 		}
 
-		public CardDTOBuilder withEndDate(final DateTime value) {
+		public CardBuilder withEndDate(final DateTime value) {
 			this.end = value;
 			return this;
 		}
 
-		public CardDTOBuilder withUser(final String value) {
+		public CardBuilder withUser(final String value) {
 			this.user = value;
 			return this;
 		}
 
-		public CardDTOBuilder withAttribute(final String key, final Object value) {
+		public CardBuilder withAttribute(final String key, final Object value) {
 			this.attributes.put(key, value);
 			return this;
 		}
 
-		public CardDTOBuilder withAllAttributes(final Map<String, Object> values) {
+		public CardBuilder withAllAttributes(final Map<String, Object> values) {
 			this.attributes.putAll(values);
 			return this;
 		}
 
-		public CardDTOBuilder withAllAttributes(final Iterable<Map.Entry<String, Object>> values) {
+		public CardBuilder withAllAttributes(final Iterable<Map.Entry<String, Object>> values) {
 			for (final Map.Entry<String, Object> entry : values) {
 				this.attributes.put(entry.getKey(), entry.getValue());
 			}
@@ -89,8 +91,8 @@ public class Card implements Storable {
 
 	}
 
-	public static CardDTOBuilder newInstance() {
-		return new CardDTOBuilder();
+	public static CardBuilder newInstance() {
+		return new CardBuilder();
 	}
 
 	private final Long id;
@@ -101,7 +103,9 @@ public class Card implements Storable {
 	private final String user;
 	private final Map<String, Object> attributes;
 
-	public Card(final CardDTOBuilder builder) {
+	private final transient String toString;
+
+	public Card(final CardBuilder builder) {
 		this.id = builder.id;
 		this.className = builder.className;
 		this.classId = builder.classId;
@@ -109,11 +113,17 @@ public class Card implements Storable {
 		this.end = builder.end;
 		this.user = builder.user;
 		this.attributes = builder.attributes;
+		this.toString = ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
 	}
 
 	@Override
 	public String getIdentifier() {
 		return getId().toString();
+	}
+
+	@Override
+	public String toString() {
+		return toString;
 	}
 
 	public Long getId() {
