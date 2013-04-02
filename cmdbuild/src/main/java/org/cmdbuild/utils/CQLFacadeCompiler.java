7a795a66df18e8cdef654c61879a0306aa1e4d11
Davide Pavan <d.pavan@tecnoteca.com>
2013-04-02 11:38:41 +0200

    #556: New-DAO full integration
    Removed unused methods.

--- cmdbuild/src/main/java/org/cmdbuild/utils/CQLFacadeCompiler.java
+++ cmdbuild/src/main/java/org/cmdbuild/utils/CQLFacadeCompiler.java
@@ -11,122 +11,69 @@ import org.cmdbuild.cql.compiler.impl.QueryImpl;
 import org.cmdbuild.cql.sqlbuilder.NaiveCmdbuildSQLBuilder;
 import org.cmdbuild.elements.interfaces.CardQuery;
 import org.cmdbuild.exception.CMDBWorkflowException.WorkflowExceptionType;
-import org.cmdbuild.exception.ORMException.ORMExceptionType;
 import org.cmdbuild.logger.Log;
 import org.cmdbuild.services.auth.UserContext;
 
 public class CQLFacadeCompiler {
 
-	public static QueryImpl compileWithTemplateParams(String cqlQueryTemplate) throws Exception {
-		String compilableCqlQuery = substituteCqlVariableNames(cqlQueryTemplate);
+	public static QueryImpl compileWithTemplateParams(final String cqlQueryTemplate) throws Exception {
+		final String compilableCqlQuery = substituteCqlVariableNames(cqlQueryTemplate);
 		return compile(compilableCqlQuery);
 	}
 
 	/*
-	 * {ns:varname} is not parsable, so we need to substitute
-	 * them with fake ones to parse the CQL query string 
+	 * {ns:varname} is not parsable, so we need to substitute them with fake
+	 * ones to parse the CQL query string
 	 */
-	private static String substituteCqlVariableNames(String cqlQuery) {
-		Pattern r = Pattern.compile("\\{[^\\{\\}]+\\}");
-	    Matcher m = r.matcher(cqlQuery);
-	    return m.replaceAll("{fake}");
+	private static String substituteCqlVariableNames(final String cqlQuery) {
+		final Pattern r = Pattern.compile("\\{[^\\{\\}]+\\}");
+		final Matcher m = r.matcher(cqlQuery);
+		return m.replaceAll("{fake}");
 	}
 
-	public static QueryImpl compile(String query) throws Exception {
-		CQLCompiler comp = new CQLCompiler();
-		CQLCompilerListener l = new CQLCompilerListener();
+	private static QueryImpl compile(final String query) throws Exception {
+		final CQLCompiler comp = new CQLCompiler();
+		final CQLCompilerListener l = new CQLCompilerListener();
 		l.setFactory(new FactoryImpl());
 		FactoryImpl.CmdbuildCheck = true;
-		
+
 		comp.compile(query, l);
-		
-		QueryImpl compiled = (QueryImpl)l.getRootQuery();
+
+		final QueryImpl compiled = (QueryImpl) l.getRootQuery();
 		compiled.check();
 		return compiled;
 	}
 
-	public static void naiveCmbuildCompile(
-			CardQuery mngr,
-			String query,
-			int limit, int offset,
-			Map<String,Object> context,
-			UserContext userCtx) {
+	private static CardQuery naiveCmbuildCompile(final String query, final int limit, final int offset,
+			final Map<String, Object> context, final UserContext userCtx) {
 		try {
-			CQLCompiler comp = new CQLCompiler();
-			CQLCompilerListener l = new CQLCompilerListener();
+			final CQLCompiler comp = new CQLCompiler();
+			final CQLCompilerListener l = new CQLCompilerListener();
 			l.setFactory(new FactoryImpl());
 			FactoryImpl.CmdbuildCheck = true;
-			
-			comp.compile(query, l);
-			
-			QueryImpl compiled = (QueryImpl)l.getRootQuery();
-			compiled.check();
-			
-			if(limit > 0) {
-				compiled.setLimit(limit);
-			}
-			if(offset > 0) {
-				compiled.setOffset(offset);
-			}
-			
-			NaiveCmdbuildSQLBuilder sqlbuilder = new NaiveCmdbuildSQLBuilder();
-			sqlbuilder.build(compiled, context, mngr, userCtx);
-		} catch (Exception e) {
-			throw ORMExceptionType.ORM_CQL_COMPILATION_FAILED.createException();
-		}
-	}
 
-	public static CardQuery naiveCmbuildCompile(
-			String query,
-			int limit, int offset,
-			Map<String,Object> context,
-			UserContext userCtx) {
-		try {
-			CQLCompiler comp = new CQLCompiler();
-			CQLCompilerListener l = new CQLCompilerListener();
-			l.setFactory(new FactoryImpl());
-			FactoryImpl.CmdbuildCheck = true;
-			
 			comp.compile(query, l);
-			
-			QueryImpl compiled = (QueryImpl)l.getRootQuery();
+
+			final QueryImpl compiled = (QueryImpl) l.getRootQuery();
 			compiled.check();
-			
-			if(limit > 0) {
+
+			if (limit > 0) {
 				compiled.setLimit(limit);
 			}
-			if(offset > 0) {
+			if (offset > 0) {
 				compiled.setOffset(offset);
 			}
-			
-			NaiveCmdbuildSQLBuilder sqlbuilder = new NaiveCmdbuildSQLBuilder();
+
+			final NaiveCmdbuildSQLBuilder sqlbuilder = new NaiveCmdbuildSQLBuilder();
 			return sqlbuilder.build(compiled, context, userCtx);
-		} catch(Exception e) {
+		} catch (final Exception e) {
 			Log.OTHER.error("CQL compilation failed", e);
 			throw WorkflowExceptionType.CQL_COMPILATION_FAILED.createException();
 		}
 	}
 
-	public static CardQuery naiveCmbuildCompileSystemUser(
-			String query,
-			Map<String,Object> context) {
-		return naiveCmbuildCompile(query, 0, 0, context,
-				UserContext.systemContext());
+	public static CardQuery naiveCmbuildCompileSystemUser(final String query, final Map<String, Object> context) {
+		return naiveCmbuildCompile(query, 0, 0, context, UserContext.systemContext());
 	}
 
-	public static void naiveCmbuildCompileSystemUser(
-			CardQuery mngr,
-			String query,
-			int limit, int offset,
-			Map<String,Object> context) {
-		naiveCmbuildCompile(mngr, query, limit, offset, context,
-				UserContext.systemContext());
-	}
-
-	public static CardQuery naiveCmbuildCompile(
-			String query,
-			Map<String,Object> context,
-			UserContext userCtx) {
-		return naiveCmbuildCompile(query, 0, 0, context, userCtx);
-	}
 }
