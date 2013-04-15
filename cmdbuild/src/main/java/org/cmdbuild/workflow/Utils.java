b8ca643f08f085a7604c6d8b8ad15535f07def9f
Davide Pavan <d.pavan@tecnoteca.com>
2013-04-15 09:38:59 +0200

    #556: New-DAO full integration
    Workflow management has no more dependencies with old DAO.

--- cmdbuild/src/main/java/org/cmdbuild/workflow/Utils.java
+++ cmdbuild/src/main/java/org/cmdbuild/workflow/Utils.java
@@ -3,8 +3,6 @@ package org.cmdbuild.workflow;
 import java.util.Map;
 
 import org.cmdbuild.common.annotations.OldDao;
-import org.cmdbuild.dao.backend.CMBackend;
-import org.cmdbuild.elements.Lookup;
 import org.cmdbuild.workflow.service.WSProcessInstanceState;
 import org.enhydra.shark.api.common.SharkConstants;
 
@@ -36,37 +34,6 @@ class Utils {
 		}
 		final WSProcessInstanceState state = stateCodeToEnumMap.get(flowStatusLookupCode);
 		return (state == null) ? WSProcessInstanceState.UNSUPPORTED : state;
-	}
-
-	/*
-	 * From the Proterozoic Eon
-	 */
-	public static Lookup lookupForFlowStatus(final WSProcessInstanceState state) {
-		final String flowStatusLookupCode;
-		switch (state) {
-		case OPEN:
-			flowStatusLookupCode = SharkConstants.STATE_OPEN_RUNNING;
-			break;
-		case SUSPENDED:
-			flowStatusLookupCode = SharkConstants.STATE_OPEN_NOT_RUNNING_SUSPENDED;
-			break;
-		case COMPLETED:
-			flowStatusLookupCode = SharkConstants.STATE_CLOSED_COMPLETED;
-			break;
-		case TERMINATED:
-			flowStatusLookupCode = SharkConstants.STATE_CLOSED_TERMINATED;
-			break;
-		case ABORTED:
-			flowStatusLookupCode = SharkConstants.STATE_CLOSED_ABORTED;
-			break;
-		default:
-			flowStatusLookupCode = null;
-		}
-		return lookupForFlowStatusCode(flowStatusLookupCode);
-	}
-
-	public static Lookup lookupForFlowStatusCode(final String flowStatusLookupCode) {
-		return CMBackend.INSTANCE.getFirstLookupByCode(FLOW_STATUS_LOOKUP, flowStatusLookupCode);
 	}
 
 }
