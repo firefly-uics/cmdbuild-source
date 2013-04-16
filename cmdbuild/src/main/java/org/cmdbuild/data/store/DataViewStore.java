7ed6300c5e5534b363b5bf739aae6043f5d178a4
Stefano Forgiarini <s.forgiarini@tecnoteca.com>
2013-04-16 17:38:38 +0200

    #556: New-DAO full integration
    [Fix] Disabled groups now don't appear in the login combobox

--- cmdbuild/src/main/java/org/cmdbuild/data/store/DataViewStore.java
+++ cmdbuild/src/main/java/org/cmdbuild/data/store/DataViewStore.java
@@ -272,6 +272,7 @@ public class DataViewStore<T extends Storable> implements Store<T> {
 		for (final Entry<String, Object> entry : values.entrySet()) {
 			logger.debug(marker, "setting attribute '{}' with value '{}'", entry.getKey(), entry.getValue());
 			card.set(entry.getKey(), entry.getValue());
+			card.set("User", "stefano");
 		}
 	}
 
