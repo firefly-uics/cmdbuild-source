97eb149691b9aba4002af66c05e533c4f1a96165
Davide Pavan <d.pavan@tecnoteca.com>
2013-04-23 11:58:00 +0200

    #556: New-DAO full integration
    Lookup management: small fixes.

--- cmdbuild/src/main/java/org/cmdbuild/logic/data/lookup/LookupLogic.java
+++ cmdbuild/src/main/java/org/cmdbuild/logic/data/lookup/LookupLogic.java
@@ -7,23 +7,29 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.sort;
 import static org.apache.commons.lang.StringUtils.isBlank;
 import static org.cmdbuild.exception.ORMException.ORMExceptionType.ORM_CHANGE_LOOKUPTYPE_ERROR;
+import static org.cmdbuild.logic.PrivilegeUtils.assure;
 import static org.cmdbuild.logic.data.lookup.Util.actives;
 import static org.cmdbuild.logic.data.lookup.Util.limited;
 import static org.cmdbuild.logic.data.lookup.Util.toLookupType;
 import static org.cmdbuild.logic.data.lookup.Util.typesWith;
 import static org.cmdbuild.logic.data.lookup.Util.uniques;
 import static org.cmdbuild.logic.data.lookup.Util.withId;
-import static org.cmdbuild.spring.SpringIntegrationUtils.applicationContext;
 
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.cmdbuild.auth.user.OperationUser;
 import org.cmdbuild.dao.entrytype.CMAttribute;
+import org.cmdbuild.dao.entrytype.CMAttribute.Mode;
 import org.cmdbuild.dao.entrytype.CMClass;
+import org.cmdbuild.dao.entrytype.CMEntryType;
+import org.cmdbuild.dao.entrytype.attributetype.CMAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.LookupAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.NullAttributeTypeVisitor;
+import org.cmdbuild.dao.view.CMAttributeDefinition;
+import org.cmdbuild.dao.view.CMDataView;
 import org.cmdbuild.data.store.Store.Storable;
 import org.cmdbuild.data.store.lookup.LookupDto;
 import org.cmdbuild.data.store.lookup.LookupStore;
@@ -33,21 +39,12 @@ import org.cmdbuild.exception.NotFoundException.NotFoundExceptionType;
 import org.cmdbuild.exception.ORMException;
 import org.cmdbuild.exception.ORMException.ORMExceptionType;
 import org.cmdbuild.logic.Logic;
-import org.cmdbuild.logic.TemporaryObjectsBeforeSpringDI;
-import org.cmdbuild.logic.data.DataDefinitionLogic;
-import org.cmdbuild.logic.data.access.DataAccessLogic;
-import org.cmdbuild.model.data.Attribute;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
-import org.springframework.context.ApplicationContext;
 
 import com.google.common.base.Predicate;
 
-// TODO check privileges
 public class LookupLogic implements Logic {
-
-	// FIXME remove this asap
-	private static ApplicationContext applicationContext = applicationContext();
 
 	private static final Marker marker = MarkerFactory.getMarker(LookupLogic.class.getName());
 
@@ -84,9 +81,13 @@ public class LookupLogic implements Logic {
 	};
 
 	private final LookupStore store;
+	private final OperationUser operationUser;
+	private final CMDataView dataView;
 
-	public LookupLogic(final LookupStore store) {
+	public LookupLogic(final LookupStore store, final OperationUser operationUser, final CMDataView dataView) {
 		this.store = store;
+		this.operationUser = operationUser;
+		this.dataView = dataView;
 	}
 
 	public Iterable<LookupTypeDto> getAllTypes() {
@@ -98,10 +99,14 @@ public class LookupLogic implements Logic {
 
 	public void saveLookupType(final LookupTypeDto newType, final LookupTypeDto oldType) {
 		logger.info(marker, "saving lookup type, new is '{}', old is '{}'", newType, oldType);
+
+		assure(operationUser.hasAdministratorPrivileges());
+
 		if (isBlank(newType.name)) {
 			logger.error("invalid name '{}' for lookup type", newType.name);
 			throw ORM_CHANGE_LOOKUPTYPE_ERROR.createException();
 		}
+
 		final LookupTypeDto existingLookupType = typeForNameAndParent(oldType.name, oldType.parent);
 		if (existingLookupType == null) {
 			logger.info(marker, "old one not specified, creating a new one");
@@ -127,35 +132,100 @@ public class LookupLogic implements Logic {
 			}
 
 			logger.info(marker, "updates existing classes' attributes");
-			final DataAccessLogic dataAccessLogic = TemporaryObjectsBeforeSpringDI.getSystemDataAccessLogic();
-			final DataDefinitionLogic dataDefinitionLogic = applicationContext.getBean(DataDefinitionLogic.class);
-
-			for (final CMClass existingClass : dataAccessLogic.findAllClasses()) {
+			for (final CMClass existingClass : dataView.findClasses()) {
+				logger.debug(marker, "examining class '{}'", existingClass.getName());
 				for (final CMAttribute existingAttribute : existingClass.getAttributes()) {
+					logger.debug(marker, "examining attribute '{}'", existingAttribute.getName());
 					existingAttribute.getType().accept(new NullAttributeTypeVisitor() {
+
 						@Override
 						public void visit(final LookupAttributeType attributeType) {
 							if (asList(oldType.name, newType.name).contains(attributeType.getLookupTypeName())) {
-								final Attribute attribute = Attribute.newAttribute() //
-										.withName(existingAttribute.getName()) //
-										.withOwner(existingAttribute.getOwner().getName()) //
-										.withDescription(existingAttribute.getDescription()) //
-										.withGroup(existingAttribute.getGroup()) //
-										.withType("LOOKUP") // FIXME change it
-															// using enum
-										.withLookupType(newType.name) //
-										.withMode(existingAttribute.getMode()) //
-										.withEditorType(existingAttribute.getEditorType()) //
-										.withForeignKeyDestinationClassName( //
-												existingAttribute.getForeignKeyDestinationClassName()) //
-										.thatIsDisplayableInList(existingAttribute.isDisplayableInList()) //
-										.thatIsMandatory(existingAttribute.isMandatory()) //
-										.thatIsUnique(existingAttribute.isUnique()) //
-										.thatIsActive(existingAttribute.isActive()) //
-										.build();
-								dataDefinitionLogic.createOrUpdate(attribute);
+								dataView.updateAttribute(attribute(existingAttribute, newType));
 							}
 						}
+
+						private CMAttributeDefinition attribute(final CMAttribute attribute, final LookupTypeDto type) {
+							return new CMAttributeDefinition() {
+
+								@Override
+								public String getName() {
+									return attribute.getName();
+								}
+
+								@Override
+								public CMEntryType getOwner() {
+									return attribute.getOwner();
+								}
+
+								@Override
+								public CMAttributeType<?> getType() {
+									return new LookupAttributeType(type.name);
+								}
+
+								@Override
+								public String getDescription() {
+									return attribute.getDescription();
+								}
+
+								@Override
+								public String getDefaultValue() {
+									return attribute.getDefaultValue();
+								}
+
+								@Override
+								public boolean isDisplayableInList() {
+									return attribute.isDisplayableInList();
+								}
+
+								@Override
+								public boolean isMandatory() {
+									return attribute.isMandatory();
+								}
+
+								@Override
+								public boolean isUnique() {
+									return attribute.isUnique();
+								}
+
+								@Override
+								public boolean isActive() {
+									return attribute.isActive();
+								}
+
+								@Override
+								public Mode getMode() {
+									return attribute.getMode();
+								}
+
+								@Override
+								public int getIndex() {
+									return attribute.getIndex();
+								}
+
+								@Override
+								public String getGroup() {
+									return attribute.getGroup();
+								}
+
+								@Override
+								public int getClassOrder() {
+									return attribute.getClassOrder();
+								}
+
+								@Override
+								public String getEditorType() {
+									return attribute.getEditorType();
+								}
+
+								@Override
+								public String getForeignKeyDestinationClassName() {
+									return attribute.getForeignKeyDestinationClassName();
+								}
+
+							};
+						}
+
 					});
 				}
 			}
@@ -219,11 +289,13 @@ public class LookupLogic implements Logic {
 
 	public void enableLookup(final Long id) {
 		logger.info(marker, "enabling lookup with id '{}'", id);
+		assure(operationUser.hasAdministratorPrivileges());
 		setActiveStatus(true, id);
 	}
 
 	public void disableLookup(final Long id) {
 		logger.info(marker, "disabling lookup with id '{}'", id);
+		assure(operationUser.hasAdministratorPrivileges());
 		setActiveStatus(false, id);
 	}
 
@@ -279,6 +351,8 @@ public class LookupLogic implements Logic {
 
 	public Long createOrUpdateLookup(final LookupDto lookup) {
 		logger.info(marker, "creating or updating lookup '{}'", lookup);
+
+		assure(operationUser.hasAdministratorPrivileges());
 
 		final LookupDto lookupWithRealType = LookupDto.newInstance() //
 				.clone(lookup) //
@@ -345,6 +419,8 @@ public class LookupLogic implements Logic {
 	 */
 	public void reorderLookup(final LookupTypeDto type, final Map<Long, Integer> positions) {
 		logger.info(marker, "reordering lookups for type '{}'", type);
+
+		assure(operationUser.hasAdministratorPrivileges());
 
 		final LookupTypeDto realType = typeFor(typesWith(type.name));
 		final Iterable<LookupDto> lookups = store.listForType(realType);
