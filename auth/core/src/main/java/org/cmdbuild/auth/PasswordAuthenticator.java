0527597bfb6074cd4ee6849fc7d5c1ce64c7c233
Davide Pavan <d.pavan@tecnoteca.com>
2013-04-24 14:54:21 +0200

    #556: New-DAO full integration
    Authentication: restored LDAP authenticator.

--- auth/core/src/main/java/org/cmdbuild/auth/PasswordAuthenticator.java
+++ auth/core/src/main/java/org/cmdbuild/auth/PasswordAuthenticator.java
@@ -6,31 +6,35 @@ public interface PasswordAuthenticator extends CMAuthenticator {
 
 		/**
 		 * Change user password
-		 *
-		 * @param oldPassword old password
-		 * @param newPassword new password
+		 * 
+		 * @param oldPassword
+		 *            old password
+		 * @param newPassword
+		 *            new password
 		 * @return if password change was successful
 		 */
 		boolean changePassword(String oldPassword, String newPassword);
 	}
 
 	/**
-	 *
-	 * @param login login informations
-	 * @param password unencrypted password
-	 * @return if the password mached
+	 * 
+	 * @param login
+	 *            login informations
+	 * @param password
+	 *            unencrypted password
+	 * @return if the password matches
 	 */
 	boolean checkPassword(Login login, String password);
 
 	/**
-	 *
+	 * 
 	 * @param login
 	 * @return the unencrypted password or null
 	 */
 	String fetchUnencryptedPassword(Login login);
 
 	/**
-	 *
+	 * 
 	 * @param login
 	 * @return password changer or null if no change possible
 	 */
