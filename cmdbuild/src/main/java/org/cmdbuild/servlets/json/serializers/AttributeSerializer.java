924fe96e587e182c2a766ce5b04c9ab1e64bede3
Martino Valenti <m.valenti@tecnoteca.com>
2013-04-26 10:01:22 +0200

    #556: New-DAO full integration
    [Fix] Multi level lookup serialization

--- cmdbuild/src/main/java/org/cmdbuild/servlets/json/serializers/AttributeSerializer.java
+++ cmdbuild/src/main/java/org/cmdbuild/servlets/json/serializers/AttributeSerializer.java
@@ -46,8 +46,11 @@ import org.cmdbuild.dao.entrytype.attributetype.StringAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.TextAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.TimeAttributeType;
 import org.cmdbuild.dao.view.CMDataView;
+import org.cmdbuild.data.store.lookup.LookupType;
 import org.cmdbuild.exception.NotFoundException.NotFoundExceptionType;
+import org.cmdbuild.logic.data.lookup.LookupLogic;
 import org.cmdbuild.model.data.Metadata;
+import org.cmdbuild.spring.SpringIntegrationUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -131,165 +134,8 @@ public class AttributeSerializer extends Serializer {
 	}
 
 	public JSONObject toClient(final CMAttribute attribute, final Iterable<Metadata> metadata) throws JSONException {
-		final Map<String, Object> attributes = new CMAttributeTypeVisitor() {
-
-			private final Map<String, Object> attributes = Maps.newHashMap();
-
-			@Override
-			public void visit(final BooleanAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final CharAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final EntryTypeAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final DateTimeAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final DateAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final DecimalAttributeType attributeType) {
-				attributes.put(PRECISION, attributeType.precision);
-				attributes.put(SCALE, attributeType.scale);
-			}
-
-			@Override
-			public void visit(final DoubleAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final ForeignKeyAttributeType attributeType) {
-				attributes.put("fkDestination", attributeType.getForeignKeyDestinationClassName());
-			}
-
-			@Override
-			public void visit(final IntegerAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final IpAddressAttributeType attributeType) {
-			}
-
-			@Override
-			public void visit(final LookupAttributeType attributeType) {
-				// Temporary solution to have single level lookup
-				final String lookupTypeName = attributeType.getLookupTypeName();
-				final JSONArray lookupChain = new JSONArray();
-				lookupChain.put(lookupTypeName);
-				attributes.put("lookupchain", lookupChain);
-				attributes.put(LOOKUP, lookupTypeName);
-				// // NdPaolo: PLEASE, LET ME REFACTOR THE LOOKUPS
-				// LookupType lt = attribute.getLookupType();
-				// JSONArray lookupChain = new JSONArray();
-				// while (lt != null) {
-				// if (lookupChain.length() == 0) {
-				// jattr.put("lookup", lt.getType());
-				// }
-				// lookupChain.put(lt.getType());
-				// lt = lt.getParentType();
-				// }
-				// jattr.put("lookupchain", lookupChain);
-			}
-
-			@Override
-			public void visit(final ReferenceAttributeType attributeType) {
-				// ITable reftable = attribute.getReferenceTarget();
-				// jattr.put("referencedClassName", reftable.getName());
-				// jattr.put("referencedIdClass", reftable.getId());
-				// jattr.put("fieldFilter", attribute.getFilter());
-				// jattr.put("domainDirection", attribute.isReferenceDirect());
-
-				final String domainName = attributeType.getDomainName();
-				final CMDomain domain = view.findDomain(domainName);
-				if (domain == null) {
-					throw NotFoundExceptionType.DOMAIN_NOTFOUND.createException(domainName);
-				}
-				final CMEntryType owner = attribute.getOwner();
-				final CMClass target = domain.getClass1().getIdentifier().getLocalName()
-						.equals(owner.getIdentifier().getLocalName()) ? domain.getClass2() : domain.getClass1();
-
-				attributes.put("idClass", target.getId());
-				attributes.put("referencedClassName", target.getIdentifier().getLocalName());
-				attributes.put("domainName", domain.getIdentifier().getLocalName());
-				attributes.put("idDomain", domain.getId());
-			}
-
-			@Override
-			public void visit(final StringAttributeType attributeType) {
-				attributes.put(LENGTH, attributeType.length);
-			}
-
-			@Override
-			public void visit(final TextAttributeType attributeType) {
-				attributes.put(EDITOR_TYPE, attribute.getEditorType());
-			}
-
-			@Override
-			public void visit(final TimeAttributeType attributeType) {
-			}
-
-			public Map<String, Object> fill(final CMAttribute attribute) {
-				/*
-				 * type specific
-				 */
-				attribute.getType().accept(this);
-
-				/*
-				 * common
-				 */
-				attributes.put(NAME, attribute.getName());
-				attributes.put(DESCRIPTION, attribute.getDescription());
-				attributes.put(TYPE,
-						new JsonDashboardDTO.JsonDataSourceParameter.TypeConverter(attribute.getType()).getTypeName());
-				attributes.put(SHOW_IN_GRID, attribute.isDisplayableInList());
-				attributes.put(UNIQUE, attribute.isUnique());
-				attributes.put(NOT_NULL, attribute.isMandatory());
-				attributes.put(INHERITED, attribute.isInherited());
-				attributes.put(ACTIVE, attribute.isActive());
-				attributes.put(FIELD_MODE, JsonModeMapper.textFrom(attribute.getMode()));
-				attributes.put("index", attribute.getIndex()); // TODO: constant
-				attributes.put(DEFAULT_VALUE, attribute.getDefaultValue());
-				attributes.put(GROUP, attribute.getGroup() == null ? "" : attribute.getGroup());
-
-				final Map<String, String> metadataMap = Maps.newHashMap();
-				for (final Metadata element : metadata) {
-					metadataMap.put(element.name, element.value);
-				}
-				attributes.put("meta", metadata);
-
-				int absoluteClassOrder = attribute.getClassOrder();
-				int classOrderSign;
-				if (absoluteClassOrder == 0) {
-					classOrderSign = 0;
-					// to manage the sorting in the AttributeGridForSorting
-					absoluteClassOrder = 10000;
-				} else if (absoluteClassOrder > 0) {
-					classOrderSign = 1;
-				} else {
-					classOrderSign = -1;
-					absoluteClassOrder *= -1;
-				}
-				attributes.put("classOrderSign", classOrderSign); // TODO
-																	// constant
-				attributes.put("absoluteClassOrder", absoluteClassOrder); // TODO
-																			// constant
-				return attributes;
-			}
-
-			@Override
-			public void visit(final StringArrayAttributeType attributeType) {
-			}
-
-		}.fill(attribute);
-		return attributesToJsonObject(attributes);
+		final Map<String, Object> serializedAttribute = new SerializerAttributeVisitor(attribute, metadata).serialize();
+		return attributesToJsonObject(serializedAttribute);
 	}
 
 	private static JSONObject attributesToJsonObject(final Map<String, Object> attributes) throws JSONException {
@@ -434,10 +280,172 @@ public class AttributeSerializer extends Serializer {
 				}
 
 			}.jsonOf(type);
+
 			out.put(jsonType);
 		}
 
 		return out;
 	}
 
+	private class SerializerAttributeVisitor implements CMAttributeTypeVisitor {
+
+		private final CMAttribute attribute;
+		private final Iterable<Metadata> metadata;
+		private final Map<String, Object> serialization = Maps.newHashMap();
+
+		private final LookupLogic lookupLogic = SpringIntegrationUtils.applicationContext().getBean(LookupLogic.class);
+
+		private SerializerAttributeVisitor(final CMAttribute attribute, final Iterable<Metadata> metadata) {
+			this.attribute = attribute;
+			this.metadata = metadata;
+		}
+
+		@Override
+		public void visit(final BooleanAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final CharAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final EntryTypeAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final DateTimeAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final DateAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final DecimalAttributeType attributeType) {
+			serialization.put(PRECISION, attributeType.precision);
+			serialization.put(SCALE, attributeType.scale);
+		}
+
+		@Override
+		public void visit(final DoubleAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final ForeignKeyAttributeType attributeType) {
+			serialization.put("fkDestination", attributeType.getForeignKeyDestinationClassName());
+		}
+
+		@Override
+		public void visit(final IntegerAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final IpAddressAttributeType attributeType) {
+		}
+
+		@Override
+		public void visit(final LookupAttributeType attributeType) {
+			final String lookupTypeName = attributeType.getLookupTypeName();
+			serialization.put(LOOKUP, lookupTypeName);
+
+			final JSONArray lookupChain = new JSONArray();
+			LookupType lookupType = lookupLogic.typeFor(lookupTypeName);
+			while (lookupType != null) {
+				lookupChain.put(lookupType.name);
+				lookupType = lookupLogic.typeFor(lookupType.parent);
+			}
+
+			serialization.put("lookupchain", lookupChain);
+		}
+
+		@Override
+		public void visit(final ReferenceAttributeType attributeType) {
+			// ITable reftable = attribute.getReferenceTarget();
+			// jattr.put("referencedClassName", reftable.getName());
+			// jattr.put("referencedIdClass", reftable.getId());
+			// jattr.put("fieldFilter", attribute.getFilter());
+			// jattr.put("domainDirection", attribute.isReferenceDirect());
+
+			final String domainName = attributeType.getDomainName();
+			final CMDomain domain = view.findDomain(domainName);
+			if (domain == null) {
+				throw NotFoundExceptionType.DOMAIN_NOTFOUND.createException(domainName);
+			}
+			final CMEntryType owner = attribute.getOwner();
+			final CMClass target = domain.getClass1().getIdentifier().getLocalName()
+					.equals(owner.getIdentifier().getLocalName()) ? domain.getClass2() : domain.getClass1();
+
+			serialization.put("idClass", target.getId());
+			serialization.put("referencedClassName", target.getIdentifier().getLocalName());
+			serialization.put("domainName", domain.getIdentifier().getLocalName());
+			serialization.put("idDomain", domain.getId());
+		}
+
+		@Override
+		public void visit(final StringAttributeType attributeType) {
+			serialization.put(LENGTH, attributeType.length);
+		}
+
+		@Override
+		public void visit(final TextAttributeType attributeType) {
+			serialization.put(EDITOR_TYPE, attribute.getEditorType());
+		}
+
+		@Override
+		public void visit(final TimeAttributeType attributeType) {
+		}
+
+		public Map<String, Object> serialize() {
+
+			/*
+			 * type specific
+			 */
+			attribute.getType().accept(this);
+
+			/*
+			 * common
+			 */
+			serialization.put(NAME, attribute.getName());
+			serialization.put(DESCRIPTION, attribute.getDescription());
+			serialization.put(TYPE,
+					new JsonDashboardDTO.JsonDataSourceParameter.TypeConverter(attribute.getType()).getTypeName());
+			serialization.put(SHOW_IN_GRID, attribute.isDisplayableInList());
+			serialization.put(UNIQUE, attribute.isUnique());
+			serialization.put(NOT_NULL, attribute.isMandatory());
+			serialization.put(INHERITED, attribute.isInherited());
+			serialization.put(ACTIVE, attribute.isActive());
+			serialization.put(FIELD_MODE, JsonModeMapper.textFrom(attribute.getMode()));
+			serialization.put("index", attribute.getIndex()); // TODO: constant
+			serialization.put(DEFAULT_VALUE, attribute.getDefaultValue());
+			serialization.put(GROUP, attribute.getGroup() == null ? "" : attribute.getGroup());
+
+			final Map<String, String> metadataMap = Maps.newHashMap();
+			for (final Metadata element : metadata) {
+				metadataMap.put(element.name, element.value);
+			}
+			serialization.put("meta", metadata);
+
+			int absoluteClassOrder = attribute.getClassOrder();
+			int classOrderSign;
+			if (absoluteClassOrder == 0) {
+				classOrderSign = 0;
+				// to manage the sorting in the AttributeGridForSorting
+				absoluteClassOrder = 10000;
+			} else if (absoluteClassOrder > 0) {
+				classOrderSign = 1;
+			} else {
+				classOrderSign = -1;
+				absoluteClassOrder *= -1;
+			}
+			serialization.put("classOrderSign", classOrderSign); // TODO
+																// constant
+			serialization.put("absoluteClassOrder", absoluteClassOrder); // TODO
+																		// constant
+			return serialization;
+		}
+
+		@Override
+		public void visit(final StringArrayAttributeType attributeType) {
+		}
+	}
 }
