c57021bed58ac7233c661a0efa13365c7cde82da
Stefano Forgiarini <s.forgiarini@tecnoteca.com>
2013-05-02 09:57:56 +0200

    #556: New-DAO full integration
    Unit tests fixed

--- cmdbuild/src/test/java/unit/privileges/fetchers/PrivilegeFetcherFactoriesTest.java
+++ cmdbuild/src/test/java/unit/privileges/fetchers/PrivilegeFetcherFactoriesTest.java
@@ -1,40 +1,17 @@
 package unit.privileges.fetchers;
 
-import static org.cmdbuild.spring.SpringIntegrationUtils.*;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
-import java.util.List;
-
 import org.cmdbuild.dao.view.DBDataView;
-import org.cmdbuild.logic.TemporaryObjectsBeforeSpringDI;
 import org.cmdbuild.privileges.fetchers.CMClassPrivilegeFetcher;
 import org.cmdbuild.privileges.fetchers.PrivilegeFetcher;
 import org.cmdbuild.privileges.fetchers.ViewPrivilegeFetcher;
 import org.cmdbuild.privileges.fetchers.factories.CMClassPrivilegeFetcherFactory;
-import org.cmdbuild.privileges.fetchers.factories.FilterPrivilegeFetcherFactory;
-import org.cmdbuild.privileges.fetchers.factories.PrivilegeFetcherFactory;
 import org.cmdbuild.privileges.fetchers.factories.ViewPrivilegeFetcherFactory;
 import org.junit.Test;
 
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
-
 public class PrivilegeFetcherFactoriesTest {
-
-	
-	
-	@Test
-	public void shouldReturnTheCorrectNumberOfPrivilegeFetcherFactories() {
-		// when
-		Iterable<PrivilegeFetcherFactory> factories ;
-		factories= applicationContext().getBean("privilegeFetcherFactories", List.class);
-		final int factoriesSize = Iterables.size(factories);
-
-		// then
-		assertEquals(factoriesSize, 3);
-	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldThrowExceptionIfGroupIsNotSetForCMClass() {
