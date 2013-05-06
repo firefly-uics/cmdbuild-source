b7f474c487f614c595ab5bdba5ae80224b98b40a
Davide Pavan <d.pavan@tecnoteca.com>
2013-05-06 14:50:25 +0200

    #556: New-DAO full integration
    Fixed queries over domains where the target class is a simple class.

--- dao/postgresql/src/main/java/org/cmdbuild/dao/driver/postgres/quote/AliasQuoter.java
+++ dao/postgresql/src/main/java/org/cmdbuild/dao/driver/postgres/quote/AliasQuoter.java
@@ -1,14 +1,11 @@
 package org.cmdbuild.dao.driver.postgres.quote;
 
-import org.cmdbuild.dao.entrytype.CMIdentifier;
 import org.cmdbuild.dao.query.clause.alias.Alias;
 import org.cmdbuild.dao.query.clause.alias.AliasVisitor;
 import org.cmdbuild.dao.query.clause.alias.EntryTypeAlias;
 import org.cmdbuild.dao.query.clause.alias.NameAlias;
 
 public class AliasQuoter implements Quoter {
-
-	private static final String NAMESPACE_LOCALNAME_SEPARATOR = "_";
 
 	public static String quote(final Alias alias) {
 		return new AliasQuoter(alias).quote();
@@ -22,26 +19,20 @@ public class AliasQuoter implements Quoter {
 
 	@Override
 	public String quote() {
-		final StringBuilder toQuote = new StringBuilder();
+		final StringBuilder quoted = new StringBuilder();
 		alias.accept(new AliasVisitor() {
 
 			@Override
 			public void visit(final EntryTypeAlias alias) {
-				final CMIdentifier identifier = alias.getEntryType().getIdentifier();
-				if (identifier.getNameSpace() != CMIdentifier.DEFAULT_NAMESPACE) {
-					toQuote.append(identifier.getNameSpace());
-					toQuote.append(NAMESPACE_LOCALNAME_SEPARATOR);
-				}
-				toQuote.append(identifier.getLocalName());
+				quoted.append(EntryTypeQuoter.quote(alias.getEntryType()));
 			}
 
 			@Override
 			public void visit(final NameAlias alias) {
-				toQuote.append(alias.getName());
+				quoted.append(IdentQuoter.quote(alias.getName()));
 			}
 		});
-
-		return IdentQuoter.quote(toQuote.toString());
+		return quoted.toString();
 	}
 
 }
