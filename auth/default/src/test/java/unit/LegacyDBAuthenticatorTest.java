0bda10cfc7d460cf2dd726b8b4e1f343676c1dda
Davide Pavan <d.pavan@tecnoteca.com>
2013-07-17 11:49:40 +0200

    #693: [Fix] Users should not be able to use e-mail for login

--- auth/default/src/test/java/unit/LegacyDBAuthenticatorTest.java
+++ auth/default/src/test/java/unit/LegacyDBAuthenticatorTest.java
@@ -2,23 +2,24 @@ package unit;
 
 import static org.mockito.Mockito.mock;
 
+import org.cmdbuild.auth.DatabaseAuthenticator;
 import org.cmdbuild.auth.LegacyDBAuthenticator;
 import org.cmdbuild.dao.view.CMDataView;
 import org.junit.Test;
 
-public class DBAuthenticatorTest {
+public class LegacyDBAuthenticatorTest {
 
 	private final CMDataView view = mock(CMDataView.class);
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void viewCannotBeNull() {
 		@SuppressWarnings("unused")
-		final LegacyDBAuthenticator authenticator = new LegacyDBAuthenticator(null);
+		final DatabaseAuthenticator authenticator = new LegacyDBAuthenticator(null);
 	}
 
 	@Test(expected = java.lang.IllegalArgumentException.class)
 	public void passwordHandlerIfProvidedCannotBeNull() {
 		@SuppressWarnings("unused")
-		final LegacyDBAuthenticator authenticator = new LegacyDBAuthenticator(view, null);
+		final DatabaseAuthenticator authenticator = new LegacyDBAuthenticator(view, null);
 	}
 }
