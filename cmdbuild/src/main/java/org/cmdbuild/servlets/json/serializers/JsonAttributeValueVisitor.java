1655d3e0e7eb86b8a20fbe09c7fffb19c22d315f
Davide Pavan <d.pavan@tecnoteca.com>
2013-07-18 15:56:54 +0200

    [Fix] fix-fulltextquery - Error with serialization of process' attributes

--- cmdbuild/src/main/java/org/cmdbuild/servlets/json/serializers/JsonAttributeValueVisitor.java
+++ cmdbuild/src/main/java/org/cmdbuild/servlets/json/serializers/JsonAttributeValueVisitor.java
@@ -1,10 +1,17 @@
 package org.cmdbuild.servlets.json.serializers;
 
+import static org.cmdbuild.servlets.json.ComunicationConstants.DESCRIPTION;
+import static org.cmdbuild.servlets.json.ComunicationConstants.ID;
+
+import java.util.Map;
+
 import org.cmdbuild.dao.entry.CardReference;
 import org.cmdbuild.dao.entrytype.attributetype.CMAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.EntryTypeAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.LookupAttributeType;
 import org.cmdbuild.dao.entrytype.attributetype.ReferenceAttributeType;
+
+import com.google.common.collect.Maps;
 
 public class JsonAttributeValueVisitor extends AbstractAttributeValueVisitor {
 
@@ -21,7 +28,7 @@ public class JsonAttributeValueVisitor extends AbstractAttributeValueVisitor {
 	public void visit(final LookupAttributeType attributeType) {
 		if (value instanceof CardReference) {
 			final CardReference cardReference = CardReference.class.cast(value);
-			convertedValue = cardReference.getDescription();
+			convertedValue = mapOf(cardReference);
 		} else {
 			convertedValue = value;
 		}
@@ -31,10 +38,17 @@ public class JsonAttributeValueVisitor extends AbstractAttributeValueVisitor {
 	public void visit(final ReferenceAttributeType attributeType) {
 		if (value instanceof CardReference) {
 			final CardReference cardReference = CardReference.class.cast(value);
-			convertedValue = cardReference.getDescription();
+			convertedValue = mapOf(cardReference);
 		} else {
 			convertedValue = value;
 		}
+	}
+
+	private Object mapOf(CardReference cardReference) {
+		final Map<String, Object> map = Maps.newHashMap();
+		map.put(ID, cardReference.getId());
+		map.put(DESCRIPTION, cardReference.getDescription());
+		return map;
 	}
 
 }
