0ac163570c8462a4ce0e5813ccdd61347dddea23
Stefano Forgiarini <s.forgiarini@tecnoteca.com>
2013-07-25 17:51:37 +0200

    [Fix] Process instances list does not work when ALL processes are requested

--- dao/postgresql/src/main/java/org/cmdbuild/dao/driver/postgres/query/WherePartCreator.java
+++ dao/postgresql/src/main/java/org/cmdbuild/dao/driver/postgres/query/WherePartCreator.java
@@ -17,6 +17,7 @@ import org.cmdbuild.dao.CardStatus;
 import org.cmdbuild.dao.driver.postgres.Const.SystemAttributes;
 import org.cmdbuild.dao.driver.postgres.SqlType;
 import org.cmdbuild.dao.driver.postgres.Utils;
+import org.cmdbuild.dao.entry.CardReference;
 import org.cmdbuild.dao.entrytype.CMAttribute;
 import org.cmdbuild.dao.entrytype.CMClass;
 import org.cmdbuild.dao.entrytype.CMDomain;
@@ -293,6 +294,9 @@ public class WherePartCreator extends PartCreator implements WhereClauseVisitor 
 	}
 
 	private Object sqlValueOf(final QueryAliasAttribute attribute, final Object value) {
+		if (value instanceof CardReference) {
+			return CardReference.class.cast(value).getId();
+		}
 		return sqlTypeOf(attribute).javaToSqlValue(value);
 	}
 
