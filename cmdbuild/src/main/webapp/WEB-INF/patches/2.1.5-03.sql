fc2cd28a2ae3b1fae73df486e93cd43b57fb1e4a
Davide Pavan <d.pavan@tecnoteca.com>
2013-10-29 16:19:42 +0100

    #967: [Fix] User truncated in card's history

--- cmdbuild/src/main/webapp/WEB-INF/patches/2.1.5-03.sql
+++ cmdbuild/src/main/webapp/WEB-INF/patches/2.1.5-03.sql
@@ -1,4 +1,4 @@
--- Update User column size
+﻿-- Update User column size
 
 CREATE OR REPLACE FUNCTION patch_215_03() RETURNS void AS $$
 DECLARE
@@ -6,8 +6,8 @@ DECLARE
 	query varchar;
 	NEW_SIZE integer := 100;
 BEGIN
-	DROP VIEW system_relationlist_history;
-	DROP VIEW system_privilegescatalog;
+	DROP IF EXISTS VIEW system_relationlist_history;
+	DROP IF EXISTS VIEW system_privilegescatalog;
 
 	EXECUTE 'ALTER TABLE "Class" ALTER COLUMN "User" TYPE character varying(' || NEW_SIZE || ');';
 	EXECUTE 'ALTER TABLE "Report" ALTER COLUMN "User" TYPE character varying(' || NEW_SIZE || ');';
@@ -31,8 +31,6 @@ BEGIN
 			LEFT JOIN "Class" ON "Class"."IdClass"::oid = "Map"."IdClass1"::oid AND "Class"."Id" = "Map"."IdObj1", pg_class pg_class0, pg_description pg_description0, pg_class pg_class1, pg_description pg_description1, pg_class pg_class2, pg_description pg_description2
 			WHERE "Map"."Status" = 'U'::bpchar AND pg_class1.oid = "Map"."IdClass1"::oid AND pg_class2.oid = "Map"."IdClass2"::oid AND pg_class0.oid = "Map"."IdDomain"::oid AND pg_description0.objoid = pg_class0.oid AND pg_description0.objsubid = 0 AND pg_description1.objoid = pg_class1.oid AND pg_description1.objsubid = 0 AND pg_description2.objoid = pg_class2.oid AND pg_description2.objsubid = 0 AND _cm_is_domain_comment(pg_description0.description) AND _cm_is_active_comment(pg_description0.description);
 
-	ALTER TABLE system_relationlist_history OWNER TO postgres;
-
 	CREATE OR REPLACE VIEW system_privilegescatalog AS
 		SELECT DISTINCT ON (permission."IdClass", permission."Code", permission."Description", permission."Status", permission."User", permission."Notes", permission."IdRole", permission."IdGrantedClass") permission."Id", permission."IdClass", permission."Code", permission."Description", permission."Status", permission."User", permission."BeginDate", permission."Notes", permission."IdRole", permission."IdGrantedClass", permission."Mode"
 			FROM (
@@ -44,8 +42,6 @@ BEGIN
 					WHERE system_classcatalog.classid::regclass::oid <> '"Class"'::regclass::oid AND NOT ("Role"."Id"::text || system_classcatalog.classid::integer::text IN (SELECT "Grant"."IdRole"::text || "Grant"."IdGrantedClass"::oid::integer::text FROM "Grant"))) permission
 				JOIN system_classcatalog ON permission."IdGrantedClass"::oid = system_classcatalog.classid AND (_cm_legacy_read_comment(system_classcatalog.classcomment::character varying::text, 'MODE'::character varying::text)::text = ANY (ARRAY['write'::character varying::text, 'read'::character varying::text]))
 				ORDER BY permission."IdClass", permission."Code", permission."Description", permission."Status", permission."User", permission."Notes", permission."IdRole", permission."IdGrantedClass";
-
-	ALTER TABLE system_privilegescatalog OWNER TO postgres;
 END
 $$ LANGUAGE PLPGSQL;
 
