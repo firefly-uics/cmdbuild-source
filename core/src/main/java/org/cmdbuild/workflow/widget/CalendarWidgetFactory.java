61c974962befb570a03b6161b2e79ba7002257e9
Martino Valenti <m.valenti@tecnoteca.com>
2013-12-03 12:48:27 +0100

    #976: [Fix] Calendar widget does not work

--- core/src/main/java/org/cmdbuild/workflow/widget/CalendarWidgetFactory.java
+++ core/src/main/java/org/cmdbuild/workflow/widget/CalendarWidgetFactory.java
@@ -12,6 +12,7 @@ public class CalendarWidgetFactory extends ValuePairWidgetFactory {
 	private static final String WIDGET_NAME = "calendar";
 
 	public static final String TARGET_CLASS = "ClassName";
+	public static final String EVENT_CLASS = "EventClass";
 	public static final String CQL_FILTER = "Filter";
 	public static final String TITLE = "EventTitle";
 	public static final String START_DATE = "EventStartDate";
@@ -34,9 +35,9 @@ public class CalendarWidgetFactory extends ValuePairWidgetFactory {
 		final String filter = readString(valueMap.get(CQL_FILTER));
 		if (filter != null) {
 			widget.setFilter(filter);
-			widget.setTargetClass(readClassNameFromCQLFilter(filter));
+			widget.setEventClass(readClassNameFromCQLFilter(filter));
 		} else {
-			widget.setTargetClass(readString(valueMap.get(TARGET_CLASS)));
+			widget.setEventClass(readString(valueMap.get(EVENT_CLASS)));
 		}
 
 		widget.setEventTitle(readString(valueMap.get(TITLE)));
