1e60c80327c3596769a8953bfedd9eb03f6e3d3a
Davide Pavan <d.pavan@tecnoteca.com>
2014-01-14 10:05:54 +0100

    #1040: RESTful API implementation - Attribute list

--- services/rest/core/src/main/java/org/cmdbuild/service/rest/dto/Constants.java
+++ services/rest/core/src/main/java/org/cmdbuild/service/rest/dto/Constants.java
@@ -4,6 +4,24 @@ public class Constants {
 
 	public static final String NAMESPACE = "http://rest.services.cmdbuild.org";
 
+	public static final String ATTRIBUTE_DETAIL = "attributeDetail";
+	public static final String ATTRIBUTE_DETAIL_RESPONSE = "attributeDetailResponse";
+	public static final String CLASS_DETAIL = "classDetail";
+	public static final String CLASS_DETAIL_RESPONSE = "classDetailResponse";
+
+	public static final String ACTIVE = "active";
+	public static final String DATA = "data";
+	public static final String DEFAULT_VALUE = "defaultValue";
+	public static final String DESCRIPTION = "description";
+	public static final String DISPLAYABLE_IN_LIST = "displayableInList";
+	public static final String GROUP = "group";
+	public static final String INDEX = "index";
+	public static final String INHERITED = "inherited";
+	public static final String MANDATORY = "mandatory";
+	public static final String NAME = "name";
+	public static final String TOTAL = "total";
+	public static final String UNIQUE = "unique";
+
 	private Constants() {
 		// prevents instantiation
 	}
--- services/rest/core/src/test/java/integration/rest/SchemaTest.java
+++ services/rest/core/src/test/java/integration/rest/SchemaTest.java
@@ -3,12 +3,15 @@ package integration.rest;
 import static java.util.Arrays.asList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.cmdbuild.service.rest.Schema;
+import org.cmdbuild.service.rest.dto.AttributeDetail;
+import org.cmdbuild.service.rest.dto.AttributeDetailResponse;
 import org.cmdbuild.service.rest.dto.ClassDetail;
 import org.cmdbuild.service.rest.dto.ClassDetailResponse;
 import org.junit.Before;
@@ -60,11 +63,36 @@ public class SchemaTest {
 								.build())) //
 				.withTotal(2) //
 				.build();
-		when(service.getClasses()) //
+		when(service.getClasses(anyBoolean())) //
 				.thenReturn(expectedResponse);
 
 		// when
 		final GetMethod get = new GetMethod("http://localhost:8080/schema/classes/");
+		final int result = httpclient.executeMethod(get);
+
+		// then
+		assertThat(result, equalTo(200));
+		assertThat(json.from(get.getResponseBodyAsString()), equalTo(json.from(expectedResponse)));
+	}
+
+	@Test
+	public void getAttributes() throws Exception {
+		// given
+		final AttributeDetailResponse expectedResponse = AttributeDetailResponse.newInstance() //
+				.withDetails(asList( //
+						AttributeDetail.newInstance() //
+								.withName("bar") //
+								.build(), //
+						AttributeDetail.newInstance() //
+								.withName("baz") //
+								.build())) //
+				.withTotal(2) //
+				.build();
+		when(service.getAttributes(eq("foo"), anyBoolean())) //
+				.thenReturn(expectedResponse);
+
+		// when
+		final GetMethod get = new GetMethod("http://localhost:8080/schema/classes/foo/attributes/");
 		final int result = httpclient.executeMethod(get);
 
 		// then
