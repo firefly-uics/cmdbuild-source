d8ae86c422925448ddc68426be5dc27c3dff50c8
Anna Pascoletti <Anna Pascoletti@localhost>
2014-01-16 12:29:01 +0100

    #1042: [Fix] Cmdbuild does never try to reconnect to BimServer

--- bim/bimserver/src/main/java/org/cmdbuild/bim/service/bimserver/BimserverClientHolder.java
+++ bim/bimserver/src/main/java/org/cmdbuild/bim/service/bimserver/BimserverClientHolder.java
@@ -4,7 +4,6 @@ import org.bimserver.client.BimServerClient;
 import org.bimserver.client.BimServerClientFactory;
 import org.bimserver.client.soap.SoapBimServerClientFactory;
 import org.bimserver.shared.UsernamePasswordAuthenticationInfo;
-import org.cmdbuild.bim.service.BimError;
 import org.cmdbuild.bim.service.bimserver.BimserverConfiguration.ChangeListener;
 import org.cmdbuild.common.Holder;
 
@@ -12,7 +11,7 @@ public class BimserverClientHolder implements Holder<BimServerClient>, ChangeLis
 
 	private BimServerClient client;
 	private final BimserverConfiguration configuration;
-	private boolean clientValid;
+	private boolean connectionValid;
 
 	public BimserverClientHolder(final BimserverConfiguration configuration) {
 		this.configuration = configuration;
@@ -22,12 +21,33 @@ public class BimserverClientHolder implements Holder<BimServerClient>, ChangeLis
 
 	@Override
 	public BimServerClient get() {
-		return clientValid ? client : null;
+		return ping() ? client : null;
 	}
 
 	@Override
 	public void configurationChanged() {
 		buildClient();
+	}
+
+	private boolean ping() {
+		boolean pingSuccess = false;
+		if (connectionValid) {
+			try {
+				pingSuccess = client.getBimsie1AuthInterface().isLoggedIn();
+			} catch (Throwable t1) {
+				buildClient(); // try to reconnect
+				if (connectionValid) {
+					pingSuccess = true;
+				} else {
+				}
+			}
+		} else {
+			buildClient(); // try to reconnect
+			if (connectionValid) {
+				pingSuccess = true;
+			}
+		}
+		return pingSuccess;
 	}
 
 	private void buildClient() {
@@ -36,21 +56,16 @@ public class BimserverClientHolder implements Holder<BimServerClient>, ChangeLis
 			try {
 				client = factory.create(new UsernamePasswordAuthenticationInfo(configuration.getUsername(),
 						configuration.getPassword()));
-				clientValid = true;
+				connectionValid = true;
 			} catch (final Throwable t) {
-				/*
-				 * Business rule: the first implementation disabled
-				 * the conf if some error occurs.
-				 * 
-				 * Now (05/12/2013) this behavior is no longer desired
-				 */
-				//configuration.disable();
-
-//				throw new BimError("Unable to connect to '" + configuration.getUrl()
-//						+ "'. Please check configuration parameters.", t);
+				connectionValid = false;
+				System.out.println("Connection to BimServer failed");
 			}
 		} else {
-			clientValid = false;
+			connectionValid = false;
 		}
+		if (connectionValid) {
+			System.out.println("Connection to BimServer established");
+		} 
 	}
 }
