52b2ed9e99997a763f73c62fd3c0443e89b460c6
Davide Pavan <d.pavan@tecnoteca.com>
2014-01-27 12:31:48 +0100

    #836: Start a process as a result of receiving an e-mail - User interface for multiple email account management
    Server side implementation.

--- core/src/main/java/org/cmdbuild/logic/email/DefaultEmailAccountLogic.java
+++ core/src/main/java/org/cmdbuild/logic/email/DefaultEmailAccountLogic.java
@@ -1,6 +1,7 @@
 package org.cmdbuild.logic.email;
 
 import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.collect.Iterables.isEmpty;
 import static org.cmdbuild.data.store.StorableUtils.storableById;
 
 import java.util.List;
@@ -13,6 +14,113 @@ import org.cmdbuild.data.store.email.EmailAccount;
 import com.google.common.base.Function;
 
 public class DefaultEmailAccountLogic implements EmailAccountLogic {
+
+	private static class ForwardingAccount implements Account {
+
+		private final Account delegate;
+
+		public ForwardingAccount(final Account delegate) {
+			this.delegate = delegate;
+		}
+
+		@Override
+		public Long getId() {
+			return delegate.getId();
+		}
+
+		@Override
+		public String getName() {
+			return delegate.getName();
+		}
+
+		@Override
+		public boolean isDefault() {
+			return delegate.isDefault();
+		}
+
+		@Override
+		public String getUsername() {
+			return delegate.getUsername();
+		}
+
+		@Override
+		public String getPassword() {
+			return delegate.getPassword();
+		}
+
+		@Override
+		public String getAddress() {
+			return delegate.getAddress();
+		}
+
+		@Override
+		public String getSmtpServer() {
+			return delegate.getSmtpServer();
+		}
+
+		@Override
+		public Integer getSmtpPort() {
+			return delegate.getSmtpPort();
+		}
+
+		@Override
+		public boolean isSmtpSsl() {
+			return delegate.isSmtpSsl();
+		}
+
+		@Override
+		public String getImapServer() {
+			return delegate.getImapServer();
+		}
+
+		@Override
+		public Integer getImapPort() {
+			return delegate.getImapPort();
+		}
+
+		@Override
+		public boolean isImapSsl() {
+			return delegate.isImapSsl();
+		}
+
+		@Override
+		public String getInputFolder() {
+			return delegate.getInputFolder();
+		}
+
+		@Override
+		public String getProcessedFolder() {
+			return delegate.getProcessedFolder();
+		}
+
+		@Override
+		public String getRejectedFolder() {
+			return delegate.getRejectedFolder();
+		}
+
+		@Override
+		public boolean isRejectNotMatching() {
+			return delegate.isRejectNotMatching();
+		}
+
+	}
+
+	private static class AlwaysDefault extends ForwardingAccount {
+
+		public static AlwaysDefault of(final Account account) {
+			return new AlwaysDefault(account);
+		}
+
+		public AlwaysDefault(final Account delegate) {
+			super(delegate);
+		}
+
+		@Override
+		public boolean isDefault() {
+			return true;
+		}
+
+	}
 
 	private static class AccountWrapper implements Account {
 
@@ -149,24 +257,33 @@ public class DefaultEmailAccountLogic implements EmailAccountLogic {
 
 	@Override
 	public Account createAccount(final Account account) {
-		final EmailAccount emailAccount = ACCOUNT_TO_EMAIL_ACCOUNT.apply(account);
+		Validate.isTrue(account.getId() == null, "cannot create an account with an id");
+		for (final EmailAccount element : store.list()) {
+			Validate.isTrue(!element.getName().equals(account.getName()), "duplicate name");
+		}
+		final Account readyAccount = isEmpty(store.list()) ? AlwaysDefault.of(account) : account;
 
-		Validate.isTrue(emailAccount.getId() == null, "cannot create an account with an id");
-		validateForDuplicateName(emailAccount);
+		// TODO there must be one default account only
 
+		final EmailAccount emailAccount = ACCOUNT_TO_EMAIL_ACCOUNT.apply(readyAccount);
 		final Storable created = store.create(emailAccount);
 		final EmailAccount createdEmailAccount = store.read(created);
+
 		return EMAIL_ACCOUNT_TO_ACCOUNT.apply(createdEmailAccount);
 	}
 
 	@Override
 	public Account updateAccount(final Account account) {
+		Validate.isTrue(account.getId() != null, "cannot update an account with a missing id");
+		for (final EmailAccount element : store.list()) {
+			Validate.isTrue(!element.getName().equals(account.getName()), "duplicate name");
+		}
+
+		// TODO there must be one default account only
+
 		final EmailAccount emailAccount = ACCOUNT_TO_EMAIL_ACCOUNT.apply(account);
-
-		Validate.isTrue(emailAccount.getId() != null, "cannot update an account with a missing id");
-		validateForDuplicateName(emailAccount);
-
 		store.update(emailAccount);
+
 		return account;
 	}
 
@@ -190,12 +307,6 @@ public class DefaultEmailAccountLogic implements EmailAccountLogic {
 	public void deleteAccount(final Long id) {
 		final Storable storable = storableById(id);
 		store.delete(storable);
-	}
-
-	private void validateForDuplicateName(final EmailAccount emailAccount) {
-		for (final EmailAccount element : store.list()) {
-			Validate.isTrue(!element.getName().equals(emailAccount.getName()), "duplicate name");
-		}
 	}
 
 }
