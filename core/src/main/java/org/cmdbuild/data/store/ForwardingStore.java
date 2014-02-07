29067bc1c079d4c01232f55251cfd23ea6fc399c
Marco Cepparo <m.cepparo@tecnoteca.com>
2014-02-07 17:23:23 +0100

    #1077: [Fix] Cannot disable views accordion from Administration

---
+++ core/src/main/java/org/cmdbuild/data/store/ForwardingStore.java
@@ -0,0 +1,41 @@
+package org.cmdbuild.data.store;
+
+import java.util.List;
+
+import org.cmdbuild.data.store.Store.Storable;
+
+public abstract class ForwardingStore<T extends Storable> implements Store<T> {
+
+	private final Store<T> delegate;
+
+	protected ForwardingStore(final Store<T> delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public Storable create(final T storable) {
+		return delegate.create(storable);
+	}
+
+	@Override
+	public T read(final Storable storable) {
+		return delegate.read(storable);
+	}
+
+	@Override
+	public void update(final T storable) {
+		delegate.update(storable);
+	}
+
+	@Override
+	public void delete(final Storable storable) {
+		delegate.delete(storable);
+
+	}
+
+	@Override
+	public List<T> list() {
+		return delegate.list();
+	}
+
+}
