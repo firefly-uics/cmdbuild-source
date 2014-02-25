adf5ca67f3688f6c4e68038091d3d6b04a4e677e
Anna Pascoletti <Anna Pascoletti@localhost>
2014-02-25 16:27:20 +0100

    #821: CRUD operation on BIMProjects
    Refactoring.

---
+++ core/src/main/java/org/cmdbuild/logic/bim/BimLogic.java
@@ -0,0 +1,84 @@
+package org.cmdbuild.logic.bim;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.activation.DataHandler;
+
+import org.cmdbuild.logic.Logic;
+import org.cmdbuild.model.bim.BimLayer;
+import org.cmdbuild.services.bim.connector.DefaultBimDataView.BimObjectCard;
+import org.joda.time.DateTime;
+
+public interface BimLogic extends Logic {
+
+	public static interface Project {
+	
+		String getProjectId();
+	
+		String getName();
+	
+		String getDescription();
+	
+		boolean isActive();
+	
+		boolean isSynch();
+	
+		String getImportMapping();
+	
+		String getExportMapping();
+	
+		DateTime getLastCheckin();
+		
+		Iterable<String> getCardBinding();
+
+		File getFile();
+	
+	}
+	
+	Project createProject(Project project);
+	
+	Iterable<Project> readAllProjects();
+	
+	void updateProject(Project project);
+	
+	DataHandler download(String projectId);
+	
+	void enableProject(Project project);
+	
+	void disableProject(Project project);
+
+	
+
+	
+	
+	
+	List<BimLayer> readBimLayer();
+
+	void updateBimLayer(String className, String attributeName, String value);
+
+	void bindProjectToCards(String projectId, ArrayList<String> cardsId);
+
+	String getPoidForCardId(Long cardId);
+
+	String getLastRevisionIdFromCmCardId(Long cardId);
+
+	Iterable<String> readCardsBindedToProject(String projectId, String className);
+
+	void importIfc(String projectId);
+
+	void exportIfc(String sourceProjectId);
+
+
+	BimLayer getRootLayer();
+
+	BimObjectCard fetchCardDataFromObjectId(String objectId, String revisionId);
+
+	String fetchJsonForBimViewer(String revisionId);
+
+	boolean getActiveForClassname(String classname);
+
+	Project read(Long cardId);
+
+}
