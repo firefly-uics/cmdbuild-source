cbdc16d405b004bf028be6612956beefc2b02600
Marco Cepparo <m.cepparo@tecnoteca.com>
2014-02-26 16:38:47 +0100

    #980: Integration of 3D-viewer Read Binding Card in Bim administration view Refactoring on row selection

--- core/src/main/java/org/cmdbuild/services/bim/DefaultBimPersistence.java
+++ core/src/main/java/org/cmdbuild/services/bim/DefaultBimPersistence.java
@@ -42,7 +42,7 @@ public class DefaultBimPersistence implements BimPersistence {
 		ProjectRelations relations = relationPersistenceManager.readRelations(storableProject.getCardId(), findRoot()
 				.getClassName());
 		CmProject cmProject = from(storableProject, relations);
-		cmProject = setRelations(cmProject, relations);
+		//cmProject = setRelations(cmProject, relations);
 		return cmProject;
 	}
 
