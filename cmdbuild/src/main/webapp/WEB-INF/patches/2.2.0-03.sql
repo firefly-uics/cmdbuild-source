1dafff8da8622796efa3adab0ca0b5a89c6706f6
Anna Pascoletti <Anna Pascoletti@localhost>
2014-03-05 16:15:25 +0100

    #985: Configure BImLayers from administration module Added column for storing the name of the reference toward the bim-root class

--- cmdbuild/src/main/webapp/WEB-INF/patches/2.2.0-03.sql
+++ cmdbuild/src/main/webapp/WEB-INF/patches/2.2.0-03.sql
@@ -1,10 +1,11 @@
--- Add column to table _BimProject
+-- Add columns to bim tables
 
 CREATE OR REPLACE FUNCTION patch_220_03() RETURNS VOID AS $$
 
 BEGIN
 	RAISE INFO 'add column to _BimProject';
 	PERFORM cm_create_class_attribute('_BimProject', 'ExportProjectId', 'varchar', null, false, false, 'MODE: write|DESCR: ExportProjectId|INDEX: 9|STATUS: active');
+	PERFORM cm_create_class_attribute('_BimLayer', 'RootReference', 'varchar', null, false, false, 'MODE: write|DESCR: RootReference|INDEX: 6|STATUS: active');
 END
 
 $$ LANGUAGE PLPGSQL;
