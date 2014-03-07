1740d344a9599935641d63bd0a83d8dcab99a7b1
Davide Pavan <d.pavan@tecnoteca.com>
2014-03-07 10:38:06 +0100

    #1071: [Fix] Card's details report shows all relations regardless of privileges

--- core/src/main/java/org/cmdbuild/report/ReportFactoryTemplateDetailSubreport.java
+++ core/src/main/java/org/cmdbuild/report/ReportFactoryTemplateDetailSubreport.java
@@ -1,5 +1,16 @@
 package org.cmdbuild.report;
 
+import static java.lang.String.format;
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.cmdbuild.dao.driver.postgres.Const.ID_ATTRIBUTE;
+import static org.cmdbuild.dao.query.clause.AnyClass.anyClass;
+import static org.cmdbuild.dao.query.clause.AnyDomain.anyDomain;
+import static org.cmdbuild.dao.query.clause.QueryAliasAttribute.attribute;
+import static org.cmdbuild.dao.query.clause.alias.Utils.as;
+import static org.cmdbuild.dao.query.clause.join.Over.over;
+import static org.cmdbuild.dao.query.clause.where.EqualsOperatorAndValue.eq;
+import static org.cmdbuild.dao.query.clause.where.SimpleWhereClause.condition;
+
 import java.awt.Color;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -22,13 +33,17 @@ import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 import org.cmdbuild.config.CmdbuildConfiguration;
+import org.cmdbuild.dao.driver.postgres.query.QueryCreator;
 import org.cmdbuild.dao.entrytype.CMAttribute;
 import org.cmdbuild.dao.entrytype.CMClass;
-import org.cmdbuild.dao.entrytype.CMDomain;
+import org.cmdbuild.dao.query.QuerySpecs;
+import org.cmdbuild.dao.query.clause.OrderByClause.Direction;
+import org.cmdbuild.dao.query.clause.alias.Alias;
+import org.cmdbuild.dao.query.clause.alias.NameAlias;
+import org.cmdbuild.dao.query.clause.where.WhereClause;
 import org.cmdbuild.dao.view.CMDataView;
 import org.cmdbuild.logger.Log;
 import org.cmdbuild.model.data.Card;
-import org.cmdbuild.report.query.RelationsReportQuery;
 import org.cmdbuild.services.localization.Localization;
 
 public class ReportFactoryTemplateDetailSubreport extends ReportFactoryTemplate {
@@ -65,6 +80,12 @@ public class ReportFactoryTemplateDetailSubreport extends ReportFactoryTemplate 
 
 	private final static String REPORT = "CMDBuild_card_detail_subreport.jrxml";
 
+	public static final String CLASS_DESCRIPTION = "TargetDescription";
+	public static final String DOMAIN_DESCRIPTION = "DomainDescription";
+	public static final String BEGIN_DATE = "BeginDate";
+	public static final String CODE = "Code";
+	public static final String DESCRIPTION = "Description";
+
 	private final JasperDesign jasperDesign;
 	private final ReportExtension reportExtension;
 	private final List<SubreportAttribute> attributes;
@@ -90,42 +111,72 @@ public class ReportFactoryTemplateDetailSubreport extends ReportFactoryTemplate 
 		this.attributes = new LinkedList<SubreportAttribute>();
 		this.localization = localization;
 
-		String query = "";
-
+		final String query;
 		switch (subreportType) {
-
 		case RELATIONS:
 
 			designTitle = getTranslation("management.modcard.tabs.relations");
 			attributes.add( //
 					new SubreportAttribute( //
-							RelationsReportQuery.DOMAIN_DESCRIPTION, //
+							DOMAIN_DESCRIPTION, //
 							getTranslation("management.modcard.relation_columns.domain"), //
-							ReportParameterConverter.of(new RPFake(RelationsReportQuery.DESCRIPTION)).toCMAttribute()));
+							ReportParameterConverter.of(new RPFake(DESCRIPTION)).toCMAttribute()));
 			attributes.add( //
 					new SubreportAttribute( //
-							RelationsReportQuery.CLASS_DESCRIPTION, //
+							CLASS_DESCRIPTION, //
 							getTranslation("management.modcard.relation_columns.destclass"), //
-							ReportParameterConverter.of(new RPFake(RelationsReportQuery.DESCRIPTION)).toCMAttribute()));
+							ReportParameterConverter.of(new RPFake(DESCRIPTION)).toCMAttribute()));
 			attributes.add( //
 					new SubreportAttribute( //
 							"begindate", //
 							getTranslation("management.modcard.relation_columns.begin_date"), //
-							ReportParameterConverter.of(new RPFake(RelationsReportQuery.BEGIN_DATE)).toCMAttribute()));
+							ReportParameterConverter.of(new RPFake(BEGIN_DATE)).toCMAttribute()));
 			attributes.add( //
 					new SubreportAttribute( //
-							RelationsReportQuery.CODE, //
+							CODE, //
 							getTranslation("management.modcard.relation_columns.code"), //
-							ReportParameterConverter.of(new RPFake(RelationsReportQuery.CODE)).toCMAttribute()));
+							ReportParameterConverter.of(new RPFake(CODE)).toCMAttribute()));
 			attributes.add( //
 					new SubreportAttribute( //
-							RelationsReportQuery.DESCRIPTION, //
+							DESCRIPTION, //
 							getTranslation("management.modcard.relation_columns.description"), //
-							ReportParameterConverter.of(new RPFake(RelationsReportQuery.DESCRIPTION)).toCMAttribute()));
+							ReportParameterConverter.of(new RPFake(DESCRIPTION)).toCMAttribute()));
 
-			final Iterable<? extends CMDomain> domains = dataView.findDomainsFor(table);
-			query = new RelationsReportQuery(card, domains).toString();
+			final Alias DOM_ALIAS = NameAlias.as("DOM");
+			final Alias DST_ALIAS = NameAlias.as("DST");
+			final CMClass srcCardType = dataView.findClass(card.getClassName());
+			final WhereClause clause = condition(attribute(srcCardType, ID_ATTRIBUTE), eq(card.getId()));
 
+			final QuerySpecs querySpecs = dataView
+					.select(attribute(DST_ALIAS, CODE), attribute(DST_ALIAS, DESCRIPTION)) //
+					.from(srcCardType) //
+					.join(anyClass(), as(DST_ALIAS), over(anyDomain(), as(DOM_ALIAS))) //
+					.where(clause) //
+					.orderBy(attribute(DST_ALIAS, DESCRIPTION), Direction.ASC).build();
+			final QueryCreator queryCreator = new QueryCreator(querySpecs);
+			query = format("" //
+					+ "SELECT"
+					+ " _cm_read_comment(_cm_comment_for_table_id(\"_DST_IdClass\"), 'DESCR') AS \"%s\","
+					+ " _cm_read_comment(_cm_comment_for_table_id(\"_DOM_IdDomain\")," //
+					+ " CASE WHEN \"_DOM__Src\" = '_1' THEN 'DESCRDIR' ELSE 'DESCRINV' END" //
+					+ " ) AS \"%s\"," //
+					+ " \"_DST_BeginDate\" AS \"%s\"," //
+					+ " \"DST#Code\" AS \"%s\"," //
+					+ " \"DST#Description\" AS \"%s\"" //
+					+ " FROM (%s) AS main" //
+					+ " ORDER BY \"%s\", \"%s\"", //
+					CLASS_DESCRIPTION, //
+					DOMAIN_DESCRIPTION, //
+					BEGIN_DATE, //
+					CODE, //
+					DESCRIPTION, //
+					getQueryString(queryCreator), //
+					DOMAIN_DESCRIPTION, //
+					DESCRIPTION);
+			break;
+
+		default:
+			query = EMPTY;
 			break;
 		}
 
