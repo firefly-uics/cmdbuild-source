a16bac33e7d5c3d5a7b34a339dafa62a66f1c3c6
Marco Cepparo <m.cepparo@tecnoteca.com>
2014-03-10 09:09:55 +0100

    #1105: Translation of the attributes of the entities defined in administration First step: inserted translations in administration Added a new window for manage TextArea's translations

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/CMTranslationsWindow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/CMTranslationsWindow.js
@@ -1,26 +1,4 @@
 (function() {
-	Ext.define("CMDBuild.view.common.CMTranslationsWindow.CMTranslatableForm", {
-		extend: "Ext.form.Panel",
-		border: 0,
-		bodyCls: 'cmgraypanel',
-		height: "100%",
-		buildWindowItem: function() {
-			var activeTranslations = _CMCache.getActiveTranslations();
-			for (var i = 0; i < activeTranslations.length; i++) {
-				var at = activeTranslations[i];
-				var item = Ext.create("CMDBuild.view.common.CMTranslationsWindow.CMTranslatableText", {
-					name: at.name,
-					image: at.image,
-					language: at.language
-				});
-				this.add(item);
-			}
-		},
-		initComponent : function() {
-			this.callParent(arguments);
-			this.buildWindowItem();
-		}
-	});
 	Ext.define("CMDBuild.view.common.CMTranslationsWindow.CMTranslatableText", {
 			extend: "Ext.container.Container",
 		layout: "hbox",
@@ -36,14 +14,7 @@
 		},
 		initComponent : function() {
 			var me = this;
-			this.text = new Ext.form.field.Text( {
-				padding: "0 0 0 5",
-				fieldLabel : me.language,
-				labelWidth: CMDBuild.LABEL_WIDTH,
-				flex: 1,
-				name : me.name,
-				allowBlank : false,
-			});
+			this.text = this.buildTextField();
 			this.width += 22;
 			this.translationsButton = new Ext.form.field.Display( {
 				iconCls: me.image,
@@ -54,6 +25,31 @@
 			});
 			this.items = [this.translationsButton, this.text];
 			this.callParent(arguments);
+		},
+		buildTextField: function() {
+			var text = new Ext.form.field.Text( {
+				padding: "0 0 0 5",
+				fieldLabel : this.language,
+				labelWidth: CMDBuild.LABEL_WIDTH,
+				flex: 1,
+				name : this.name,
+				allowBlank : false,
+			});
+			return text;
+		}
+	});
+	Ext.define("CMDBuild.view.common.CMTranslationsWindow.CMTranslatableTextArea", {
+		extend: "CMDBuild.view.common.CMTranslationsWindow.CMTranslatableText",
+		buildTextField: function() {
+			var text = new Ext.form.field.TextArea( {
+				padding: "0 0 0 5",
+				fieldLabel : this.language,
+				labelWidth: CMDBuild.LABEL_WIDTH,
+				flex: 1,
+				name : this.name,
+				allowBlank : false,
+			});
+			return text;
 		}
 	});
 	Ext.define('CMDBuild.view.common.CMTranslationsWindowDelegate', {
@@ -84,6 +80,32 @@
 		},
 
 	});
+	Ext.define("CMDBuild.view.common.CMTranslationsWindow.CMTranslatableForm", {
+		extend: "Ext.form.Panel",
+		border: 0,
+		bodyCls: 'cmgraypanel',
+		height: "100%",
+		textArea: false,
+		buildWindowItem: function() {
+			var activeTranslations = _CMCache.getActiveTranslations();
+			var componentType = (! this.textArea) ?
+					"CMDBuild.view.common.CMTranslationsWindow.CMTranslatableText" :
+					"CMDBuild.view.common.CMTranslationsWindow.CMTranslatableTextArea";
+			for (var i = 0; i < activeTranslations.length; i++) {
+				var at = activeTranslations[i];
+				var item = Ext.create(componentType, {
+					name: at.name,
+					image: at.image,
+					language: at.language
+				});
+				this.add(item);
+			}
+		},
+		initComponent : function() {
+			this.callParent(arguments);
+			this.buildWindowItem();
+		}
+	});
 	Ext.define('CMDBuild.view.common.CMTranslationsWindow', {
 		extend: 'Ext.window.Window',
 
@@ -101,6 +123,7 @@
 		translationsKeyName: "",
 		translationsKeySubName: "",
 		translationsKeyField: "",
+		textArea: false,
 
 		initComponent: function() {
 			var me = this;
@@ -110,7 +133,7 @@
 			this.form = Ext.create('CMDBuild.view.common.CMTranslationsWindow.CMTranslatableForm', {
 				delegate: me.delegate,
 				configFileName: me.configFileName,
-				
+				textArea: this.textArea
 			});
 			this.contentComponent = Ext.create('Ext.panel.Panel', {
 				layout: {
