f0c2be161b200958efc9024f524294471d71c74b
Davide Pavan <d.pavan@tecnoteca.com>
2014-03-21 13:09:24 +0100

    #1040: RESTful API implementation
    "attributes" is now a resource (also).

---
+++ services/rest/cxf/src/main/java/org/cmdbuild/service/rest/cxf/CxfAttributes.java
@@ -0,0 +1,65 @@
+package org.cmdbuild.service.rest.cxf;
+
+import static com.google.common.collect.FluentIterable.from;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.cmdbuild.common.utils.PagedElements;
+import org.cmdbuild.dao.entrytype.CMAttribute;
+import org.cmdbuild.dao.entrytype.CMClass;
+import org.cmdbuild.logic.data.access.DataAccessLogic.AttributesQuery;
+import org.cmdbuild.service.rest.Attributes;
+import org.cmdbuild.service.rest.dto.AttributeDetail;
+import org.cmdbuild.service.rest.dto.AttributeDetailResponse;
+import org.cmdbuild.service.rest.dto.DetailResponseMetadata;
+import org.cmdbuild.service.rest.serialization.AttributeTypeResolver;
+import org.cmdbuild.service.rest.serialization.ToAttributeDetail;
+
+public class CxfAttributes extends CxfService implements Attributes {
+
+	private static final AttributeTypeResolver ATTRIBUTE_TYPE_RESOLVER = new AttributeTypeResolver();
+
+	@Override
+	public AttributeDetailResponse getAttributes(final String type, final String name, final boolean activeOnly,
+			final Integer limit, final Integer offset) {
+		final CMClass target = userDataAccessLogic().findClass(name);
+		if (target == null) {
+			throw new WebApplicationException(Response.status(Status.NOT_FOUND) //
+					.entity(name) //
+					.build());
+		}
+		final PagedElements<CMAttribute> filteredAttributes = userDataAccessLogic().getAttributes( //
+				name, //
+				activeOnly, //
+				new AttributesQuery() {
+
+					@Override
+					public Integer limit() {
+						return limit;
+					}
+
+					@Override
+					public Integer offset() {
+						return offset;
+					}
+
+				});
+
+		final ToAttributeDetail toAttributeDetails = ToAttributeDetail.newInstance() //
+				.withAttributeTypeResolver(ATTRIBUTE_TYPE_RESOLVER) //
+				.withDataView(systemDataView()) //
+				.withErrorHandler(errorHandler()) //
+				.build();
+		final Iterable<AttributeDetail> elements = from(filteredAttributes) //
+				.transform(toAttributeDetails);
+		return AttributeDetailResponse.newInstance() //
+				.withElements(elements) //
+				.withMetadata(DetailResponseMetadata.newInstance() //
+						.withTotal(filteredAttributes.totalSize()) //
+						.build()) //
+				.build();
+	}
+
+}
