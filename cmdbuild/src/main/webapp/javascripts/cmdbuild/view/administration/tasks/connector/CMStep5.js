3f950ad8df9dce34f79c3173037c7037f29125eb
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-05-05 17:58:49 +0200

    #1050: Task Manager - User Interface

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/administration/tasks/connector/CMStep5.js
@@ -0,0 +1,460 @@
+(function() {
+
+	var tr = CMDBuild.Translation.administration.tasks.taskConnector;
+
+	Ext.define('CMDBuild.view.administration.tasks.connector.CMStep5Delegate', {
+		extend: 'CMDBuild.controller.CMBasePanelController',
+
+		parentDelegate: undefined,
+		view: undefined,
+
+		/**
+		 * Gatherer function to catch events
+		 *
+		 * @param (String) name
+		 * @param (Object) param
+		 * @param (Function) callback
+		 */
+		// overwrite
+		cmOn: function(name, param, callBack) {
+			switch (name) {
+				case 'onBeforeEdit':
+					return this.onBeforeEdit(param.fieldName, param.rowData);
+
+				case 'onStepEdit':
+					return this.onStepEdit();
+
+				default: {
+					if (this.parentDelegate)
+						return this.parentDelegate.cmOn(name, param, callBack);
+				}
+			}
+		},
+
+		getData: function() {
+			var data = [];
+			var isKeySelection = null;
+
+			if (this.view.gridSelectionModel.hasSelection())
+				isKeySelection = this.view.gridSelectionModel.getSelection();
+
+			// To validate and filter grid rows and creating isKey attributes
+			this.view.attributeLevelMappingGrid.getStore().each(function(record) {
+				if (
+					!Ext.isEmpty(record.get(CMDBuild.ServiceProxy.parameter.CLASS_NAME))
+					&& !Ext.isEmpty(record.get(CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE))
+					&& !Ext.isEmpty(record.get(CMDBuild.ServiceProxy.parameter.VIEW_NAME))
+					&& !Ext.isEmpty(record.get(CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE))
+				) {
+					var buffer = [];
+
+					buffer[CMDBuild.ServiceProxy.parameter.CLASS_NAME] = record.get(CMDBuild.ServiceProxy.parameter.CLASS_NAME);
+					buffer[CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE] = record.get(CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE);
+					buffer[CMDBuild.ServiceProxy.parameter.VIEW_NAME] = record.get(CMDBuild.ServiceProxy.parameter.VIEW_NAME);
+					buffer[CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE] = record.get(CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE);
+					buffer[CMDBuild.ServiceProxy.parameter.IS_KEY] = false;
+
+					// Check to setup isKey parameter
+					for (key in isKeySelection) {
+						if (
+							buffer[CMDBuild.ServiceProxy.parameter.CLASS_NAME] == isKeySelection[key].get(CMDBuild.ServiceProxy.parameter.CLASS_NAME)
+							&& buffer[CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE] == isKeySelection[key].get(CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE)
+							&& buffer[CMDBuild.ServiceProxy.parameter.VIEW_NAME] == isKeySelection[key].get(CMDBuild.ServiceProxy.parameter.VIEW_NAME)
+							&& buffer[CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE] == isKeySelection[key].get(CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE)
+						) {
+							buffer[CMDBuild.ServiceProxy.parameter.IS_KEY] = true;
+							break;
+						}
+					}
+
+					data.push(buffer);
+				}
+			});
+
+			return data;
+		},
+
+		isEmptyMappingGrid: function() {
+			if (CMDBuild.Utils.isEmpty(this.getData()))
+				return true;
+
+			return false;
+		},
+
+		buildClassCombo: function() {
+			var me = this;
+
+			this.view.attributeLevelMappingGrid.columns[2].setEditor({
+				xtype: 'combo',
+				displayField: CMDBuild.ServiceProxy.parameter.DESCRIPTION,
+				valueField: CMDBuild.ServiceProxy.parameter.NAME,
+				forceSelection: true,
+				editable: false,
+				allowBlank: false,
+				store: this.parentDelegate.getFilteredClassStore(),
+				queryMode: 'local',
+
+				// To make sure the filter in the store is not cleared
+				triggerAction: 'all',
+				lastQuery: '',
+
+				listeners: {
+					select: function(combo, records, eOpts) {
+						me.buildClassAttributesCombo(records[0].get(CMDBuild.ServiceProxy.parameter.NAME));
+					}
+				}
+			});
+		},
+
+		/**
+		 * To setup class attribute combo editor
+		 *
+		 * @param (String) className
+		 * @param (Boolean) onStepEditExecute
+		 */
+		buildClassAttributesCombo: function(className, onStepEditExecute) {
+			if (!Ext.isEmpty(className)) {
+				var me = this;
+				var attributesListStore = [];
+
+				if (typeof onStepEditExecute == 'undefined')
+					var onStepEditExecute = true;
+
+				for (var key in _CMCache.getClasses()) {
+					if (key == _CMCache.getEntryTypeByName(className).get(CMDBuild.ServiceProxy.parameter.ID))
+						attributesListStore.push(this.view.classesAttributesMap[key]);
+				}
+
+				this.view.attributeLevelMappingGrid.columns[3].setEditor({
+					xtype: 'combo',
+					displayField: CMDBuild.ServiceProxy.parameter.DESCRIPTION,
+					valueField: CMDBuild.ServiceProxy.parameter.NAME,
+					forceSelection: true,
+					editable: false,
+					allowBlank: false,
+
+					store: Ext.create('Ext.data.Store', {
+						autoLoad: true,
+						fields: [CMDBuild.ServiceProxy.parameter.NAME, CMDBuild.ServiceProxy.parameter.DESCRIPTION],
+						data: attributesListStore[0]
+					}),
+
+					listeners: {
+						select: function(combo, records, eOpts) {
+							me.cmOn('onStepEdit');
+						}
+					}
+				});
+
+				if (onStepEditExecute)
+					this.onStepEdit();
+			}
+		},
+
+		buildViewCombo: function() {
+			var me = this;
+
+			this.view.attributeLevelMappingGrid.columns[0].setEditor({
+				xtype: 'combo',
+				displayField: CMDBuild.ServiceProxy.parameter.DESCRIPTION,
+				valueField: CMDBuild.ServiceProxy.parameter.NAME,
+				forceSelection: true,
+				editable: false,
+				allowBlank: false,
+				store: this.parentDelegate.getFilteredViewStore(),
+
+				// To make sure the filter in the store is not cleared
+				triggerAction: 'all',
+				lastQuery: '',
+
+				listeners: {
+					select: function(combo, records, eOpts) {
+						me.buildViewAttributesCombo(records[0].get(CMDBuild.ServiceProxy.parameter.VALUE));
+					}
+				}
+			});
+		},
+
+		/**
+		 * To setup view attribute combo editor
+		 *
+		 * @param (String) viewName
+		 * @param (Boolean) onStepEditExecute
+		 */
+		buildViewAttributesCombo: function(viewName, onStepEditExecute) {
+			if (!Ext.isEmpty(viewName)) {
+				var me = this;
+
+				var attributesListStore = [
+					{ 'name': 'Function1', 'description': 'Function 1' },
+					{ 'name': 'Function2', 'description': 'Function 2' },
+					{ 'name': 'Function3', 'description': 'Function 3' }
+				];
+
+				if (typeof onStepEditExecute == 'undefined')
+					var onStepEditExecute = true;
+
+// TODO: to finish implementation when stores will be ready
+//				for (var key in _CMCache.getClasses()) {
+//					if (key == classId)
+//						attributesListStore.push(this.view.classesAttributesMap[key]);
+//				}
+
+				this.view.attributeLevelMappingGrid.columns[1].setEditor({
+					xtype: 'combo',
+					displayField: CMDBuild.ServiceProxy.parameter.DESCRIPTION,
+					valueField: CMDBuild.ServiceProxy.parameter.NAME,
+					forceSelection: true,
+					editable: false,
+					allowBlank: false,
+
+					store: Ext.create('Ext.data.Store', {
+						autoLoad: true,
+						fields: [CMDBuild.ServiceProxy.parameter.NAME, CMDBuild.ServiceProxy.parameter.DESCRIPTION],
+						data: attributesListStore
+					}),
+
+					listeners: {
+						select: function(combo, records, eOpts) {
+							me.cmOn('onStepEdit');
+						}
+					}
+				});
+
+				if (onStepEditExecute)
+					this.onStepEdit();
+			}
+		},
+
+		/**
+		 * Function to update rows stores/editors on beforeEdit event
+		 *
+		 * @param (String) fieldName
+		 * @param (Object) rowData
+		 */
+		onBeforeEdit: function(fieldName, rowData) {
+			switch (fieldName) {
+				case CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE: {
+					if (
+						(typeof rowData[CMDBuild.ServiceProxy.parameter.CLASS_NAME] != 'undefined')
+						&& !Ext.isEmpty(rowData[CMDBuild.ServiceProxy.parameter.CLASS_NAME])
+					) {
+						this.buildClassAttributesCombo(rowData[CMDBuild.ServiceProxy.parameter.CLASS_NAME], false);
+					} else {
+						var columnModel = this.view.attributeLevelMappingGrid.columns[3];
+						var columnEditor = columnModel.getEditor();
+
+						if (!columnEditor.disabled)
+							columnModel.setEditor({
+								xtype: 'combo',
+								disabled: true
+							});
+					}
+				} break;
+
+				case CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE: {
+					if (
+						(typeof rowData[CMDBuild.ServiceProxy.parameter.VIEW_NAME] != 'undefined')
+						&& !Ext.isEmpty(rowData[CMDBuild.ServiceProxy.parameter.VIEW_NAME])
+					) {
+						this.buildViewAttributesCombo(rowData[CMDBuild.ServiceProxy.parameter.VIEW_NAME], false);
+					} else {
+						var columnModel = this.view.attributeLevelMappingGrid.columns[1];
+						var columnEditor = columnModel.getEditor();
+
+						if (!columnEditor.disabled)
+							columnModel.setEditor({
+								xtype: 'combo',
+								disabled: true
+							});
+					}
+				} break;
+			}
+		},
+
+		/**
+		 * Step validation (at least one class/view association and main view check)
+		 */
+		onStepEdit: function() {
+			this.view.gridEditorPlugin.completeEdit();
+
+			if (!this.isEmptyMappingGrid()) {
+				this.setDisabledButtonNext(false);
+			} else {
+				this.setDisabledButtonNext(true);
+			}
+		},
+
+		setDisabledButtonNext: function(state) {
+			this.parentDelegate.setDisabledButtonNext(state);
+		}
+	});
+
+	Ext.define('CMDBuild.view.administration.tasks.connector.CMStep5', {
+		extend: 'Ext.panel.Panel',
+
+		delegate: undefined,
+
+		border: false,
+		height: '100%',
+		overflowY: 'auto',
+
+		initComponent: function() {
+			var me = this;
+
+			this.classesAttributesMap = _CMCache.getAllAttributesList();
+			this.delegate = Ext.create('CMDBuild.view.administration.tasks.connector.CMStep5Delegate', this);
+
+			this.gridSelectionModel = Ext.create('Ext.selection.CheckboxModel', {
+				mode: 'multi',
+				showHeaderCheckbox: false,
+				headerText: 'tr.isKey',
+				headerWidth: 50,
+				dataIndex: CMDBuild.ServiceProxy.parameter.IS_KEY,
+				checkOnly: true,
+				selectByPosition: Ext.emptyFn, // FIX: to avoid checkbox selection on cellediting (workaround)
+				injectCheckbox: 4,
+
+				listeners: {
+					selectionchange: function(model, record, index, eOpts) {
+						me.delegate.cmOn('onStepEdit');
+					}
+				}
+			});
+
+			this.gridEditorPlugin = Ext.create('Ext.grid.plugin.CellEditing', {
+				clicksToEdit: 1,
+
+				listeners: {
+					beforeedit: function(editor, e, eOpts) {
+						me.delegate.cmOn('onBeforeEdit', {
+							fieldName: e.field,
+							rowData: e.record.data
+						});
+					}
+				}
+			});
+
+			this.attributeLevelMappingGrid = Ext.create('Ext.grid.Panel', {
+				title: 'tr.attributeLevelMapping',
+				considerAsFieldToDisable: true,
+				margin: '0 0 5 0',
+
+				selModel: this.gridSelectionModel,
+				plugins: this.gridEditorPlugin,
+
+				columns: [
+					{
+						header: 'tr.view',
+						dataIndex: CMDBuild.ServiceProxy.parameter.VIEW_NAME,
+						editor: {
+							xtype: 'combo',
+							disabled: true
+						},
+						flex: 1
+					},
+					{
+						header: 'tr.viewAttribute',
+						dataIndex: CMDBuild.ServiceProxy.parameter.VIEW_ATTRIBUTE,
+						editor: {
+							xtype: 'combo',
+							disabled: true
+						},
+						flex: 1
+					},
+					{
+						header: 'tr.className',
+						dataIndex: CMDBuild.ServiceProxy.parameter.CLASS_NAME,
+						editor: {
+							xtype: 'combo',
+							disabled: true
+						},
+						flex: 1
+					},
+					{
+						header: 'tr.classAttribute',
+						dataIndex: CMDBuild.ServiceProxy.parameter.CLASS_ATTRIBUTE,
+						editor: {
+							xtype: 'combo',
+							disabled: true
+						},
+						flex: 1
+					},
+// TODO: Future implementation
+//					{
+//						header: 'tr.function',
+//						dataIndex: 'CMDBuild.ServiceProxy.parameter.FUNCTION',
+//						editor: {
+//							xtype: 'combo',
+//							valueField: CMDBuild.ServiceProxy.parameter.VALUE,
+//							displayField: CMDBuild.ServiceProxy.parameter.NAME,
+//							forceSelection: true,
+//							editable: false,
+//							allowBlank: false,
+//							store: CMDBuild.core.proxy.CMProxyTasks.getFunctionStore()
+//						},
+//						flex: 1
+//					},
+					{
+						xtype: 'actioncolumn',
+						width: 30,
+						align: 'center',
+						sortable: false,
+						hideable: false,
+						menuDisabled: true,
+						fixed: true,
+
+						items: [
+							{
+								icon: 'images/icons/cross.png',
+								tooltip: CMDBuild.Translation.administration.modClass.attributeProperties.meta.remove,
+								handler: function(grid, rowIndex, colIndex, node, e, record, rowNode) {
+									me.attributeLevelMappingGrid.store.remove(record);
+								}
+							}
+						]
+					}
+				],
+
+				store: Ext.create('Ext.data.Store', {
+					model: 'CMDBuild.model.CMModelTasks.connector.attributeLevel',
+					data: []
+				}),
+
+				tbar: [
+					{
+						text: CMDBuild.Translation.common.buttons.add,
+						iconCls: 'add',
+						handler: function() {
+							me.attributeLevelMappingGrid.store.insert(0, Ext.create('CMDBuild.model.CMModelTasks.connector.attributeLevel'));
+						}
+					}
+				]
+			});
+
+			Ext.apply(this, {
+				items: [this.attributeLevelMappingGrid]
+			});
+
+			this.callParent(arguments);
+		},
+
+		listeners: {
+			/**
+			 * Disable next button only if grid haven't selected class and setup class and view combo editors
+			 */
+			show: function(view, eOpts) {
+				var me = this;
+
+				this.delegate.buildViewCombo();
+				this.delegate.buildClassCombo();
+
+				Ext.Function.createDelayed(function() { // HACK: to fix problem witch fires show event before changeTab() function
+					if (me.delegate.isEmptyMappingGrid())
+						me.delegate.setDisabledButtonNext(true);
+				}, 1)();
+			}
+		}
+	});
+
+})();
---
+++ core/src/main/java/org/cmdbuild/logic/taskmanager/DefaultObserverFactory.java
@@ -0,0 +1,333 @@
+package org.cmdbuild.logic.taskmanager;
+
+import static com.google.common.collect.Iterables.contains;
+import static com.google.common.collect.Iterables.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.cmdbuild.common.template.engine.Engines.emptyStringOnNull;
+import static org.cmdbuild.common.template.engine.Engines.nullOnError;
+import static org.cmdbuild.services.email.Predicates.named;
+import static org.cmdbuild.services.event.Commands.safe;
+import static org.cmdbuild.services.template.engine.EngineNames.GROUP_PREFIX;
+import static org.cmdbuild.services.template.engine.EngineNames.GROUP_USERS_PREFIX;
+import static org.cmdbuild.services.template.engine.EngineNames.USER_PREFIX;
+
+import org.apache.commons.lang3.Validate;
+import org.cmdbuild.api.fluent.FluentApi;
+import org.cmdbuild.auth.UserStore;
+import org.cmdbuild.common.template.TemplateResolver;
+import org.cmdbuild.common.template.engine.EngineBasedTemplateResolver;
+import org.cmdbuild.dao.entry.CMCard;
+import org.cmdbuild.dao.view.CMDataView;
+import org.cmdbuild.data.store.Store;
+import org.cmdbuild.data.store.email.StorableEmailAccount;
+import org.cmdbuild.logic.email.EmailTemplateLogic;
+import org.cmdbuild.logic.email.EmailTemplateLogic.Template;
+import org.cmdbuild.logic.email.SendTemplateEmail;
+import org.cmdbuild.logic.taskmanager.DefaultLogicAndObserverConverter.ObserverFactory;
+import org.cmdbuild.logic.workflow.StartProcess;
+import org.cmdbuild.logic.workflow.WorkflowLogic;
+import org.cmdbuild.model.email.EmailConstants;
+import org.cmdbuild.services.email.EmailAccount;
+import org.cmdbuild.services.email.EmailServiceFactory;
+import org.cmdbuild.services.email.PredicateEmailAccountSupplier;
+import org.cmdbuild.services.event.Command;
+import org.cmdbuild.services.event.Context;
+import org.cmdbuild.services.event.ContextVisitor;
+import org.cmdbuild.services.event.Contexts.AfterCreate;
+import org.cmdbuild.services.event.Contexts.AfterUpdate;
+import org.cmdbuild.services.event.Contexts.BeforeDelete;
+import org.cmdbuild.services.event.Contexts.BeforeUpdate;
+import org.cmdbuild.services.event.DefaultObserver;
+import org.cmdbuild.services.event.DefaultObserver.Builder;
+import org.cmdbuild.services.event.FilteredObserver;
+import org.cmdbuild.services.event.Observer;
+import org.cmdbuild.services.event.ScriptCommand;
+import org.cmdbuild.services.template.engine.CardEngine;
+import org.cmdbuild.services.template.engine.GroupEmailEngine;
+import org.cmdbuild.services.template.engine.GroupUsersEmailEngine;
+import org.cmdbuild.services.template.engine.UserEmailEngine;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Supplier;
+
+public class DefaultObserverFactory implements ObserverFactory {
+
+	private static class SynchronousEventTaskPredicate implements Predicate<CMCard> {
+
+		public static class Builder implements org.apache.commons.lang3.builder.Builder<SynchronousEventTaskPredicate> {
+
+			private SynchronousEventTask task;
+			private UserStore userStore;
+
+			private Builder() {
+				// use factory method
+			}
+
+			@Override
+			public SynchronousEventTaskPredicate build() {
+				validate();
+				return new SynchronousEventTaskPredicate(this);
+			}
+
+			private void validate() {
+				Validate.notNull(task, "invalid task");
+				Validate.notNull(userStore, "invalid user store");
+			}
+
+			public Builder withTask(final SynchronousEventTask task) {
+				this.task = task;
+				return this;
+			}
+
+			public Builder withUserStore(final UserStore userStore) {
+				this.userStore = userStore;
+				return this;
+			}
+
+		}
+
+		public static Builder newInstance() {
+			return new Builder();
+		}
+
+		private final SynchronousEventTask task;
+		private final UserStore userStore;
+
+		private SynchronousEventTaskPredicate(final Builder builder) {
+			this.task = builder.task;
+			this.userStore = builder.userStore;
+		}
+
+		@Override
+		public boolean apply(final CMCard input) {
+			return matchesGroup() && matchesClass(input);
+		}
+
+		private boolean matchesGroup() {
+			final String current = userStore.getUser().getPreferredGroup().getName();
+			return isEmpty(task.getGroups()) || contains(task.getGroups(), current);
+		}
+
+		private boolean matchesClass(final CMCard input) {
+			return isBlank(task.getTargetClassname()) || input.getType().getName().equals(task.getTargetClassname());
+		}
+
+	}
+
+	private static final String CURRENT_CARD_PREFIX = "current";
+	private static final String NEXT_CARD_PREFIX = "next";
+	private static final String PREVIOUS_CARD_PREFIX = "previous";
+
+	private final UserStore userStore;
+	private final FluentApi fluentApi;
+	private final WorkflowLogic workflowLogic;
+	private final Store<StorableEmailAccount> emailAccountStore;
+	private final EmailServiceFactory emailServiceFactory;
+	private final EmailTemplateLogic emailTemplateLogic;
+	private final CMDataView dataView;
+
+	public DefaultObserverFactory( //
+			final UserStore userStore, //
+			final FluentApi fluentApi, //
+			final WorkflowLogic workflowLogic, //
+			final Store<StorableEmailAccount> emailAccountStore, //
+			final EmailServiceFactory emailServiceFactory, //
+			final EmailTemplateLogic emailTemplateLogic, //
+			final CMDataView dataView //
+	) {
+		this.userStore = userStore;
+		this.fluentApi = fluentApi;
+		this.workflowLogic = workflowLogic;
+		this.emailAccountStore = emailAccountStore;
+		this.emailServiceFactory = emailServiceFactory;
+		this.emailTemplateLogic = emailTemplateLogic;
+		this.dataView = dataView;
+	}
+
+	@Override
+	public Observer create(final SynchronousEventTask task) {
+		final Builder builder = DefaultObserver.newInstance();
+		final DefaultObserver.Phase phase = phaseOf(task);
+		if (task.isWorkflowEnabled()) {
+			builder.add(workflowOf(task), phase);
+		}
+		if (task.isEmailEnabled()) {
+			builder.add(emailOf(task), phase);
+		}
+		if (task.isScriptingEnabled()) {
+			builder.add(scriptingOf(task), phase);
+		}
+		final DefaultObserver base = builder.build();
+		return FilteredObserver.newInstance() //
+				.withDelegate(base) //
+				.withFilter(filterOf(task)) //
+				.build();
+	}
+
+	private DefaultObserver.Phase phaseOf(final SynchronousEventTask task) {
+		return new SynchronousEventTask.PhaseIdentifier() {
+
+			private DefaultObserver.Phase converted;
+
+			public org.cmdbuild.services.event.DefaultObserver.Phase toObserverPhase() {
+				task.getPhase().identify(this);
+				Validate.notNull(converted, "conversion error");
+				return converted;
+			}
+
+			@Override
+			public void afterCreate() {
+				converted = DefaultObserver.Phase.AFTER_CREATE;
+			}
+
+			@Override
+			public void beforeUpdate() {
+				converted = DefaultObserver.Phase.BEFORE_UPDATE;
+			}
+
+			@Override
+			public void afterUpdate() {
+				converted = DefaultObserver.Phase.AFTER_UPDATE;
+			}
+
+			@Override
+			public void beforeDelete() {
+				converted = DefaultObserver.Phase.BEFORE_DELETE;
+			}
+
+		}.toObserverPhase();
+	}
+
+	private Command workflowOf(final SynchronousEventTask task) {
+		return safe(new Command() {
+
+			@Override
+			public void execute(final Context context) {
+				StartProcess.newInstance() //
+						.withWorkflowLogic(workflowLogic) //
+						.withClassName(task.getWorkflowClassName()) //
+						.withAttributes(task.getWorkflowAttributes()) //
+						.withAdvanceStatus(task.isWorkflowAdvanceable()) //
+						.withTemplateResolver(templateResolverOf(context)) //
+						.build() //
+						.execute();
+			}
+
+			private TemplateResolver templateResolverOf(final Context context) {
+				final EngineBasedTemplateResolver.Builder builder = EngineBasedTemplateResolver.newInstance();
+				context.accept(new ContextVisitor() {
+					@Override
+					public void visit(final AfterCreate context) {
+						builder.withEngine(//
+								CardEngine.newInstance() //
+										.withCard(context.card) //
+										.build(), //
+								CURRENT_CARD_PREFIX);
+					}
+
+					@Override
+					public void visit(final BeforeUpdate context) {
+						builder.withEngine(//
+								CardEngine.newInstance() //
+										.withCard(context.actual) //
+										.build(), //
+								CURRENT_CARD_PREFIX);
+						builder.withEngine(//
+								CardEngine.newInstance() //
+										.withCard(context.next) //
+										.build(), //
+								NEXT_CARD_PREFIX);
+					}
+
+					@Override
+					public void visit(final AfterUpdate context) {
+						builder.withEngine(//
+								CardEngine.newInstance() //
+										.withCard(context.previous) //
+										.build(), //
+								PREVIOUS_CARD_PREFIX);
+						builder.withEngine(//
+								CardEngine.newInstance() //
+										.withCard(context.actual) //
+										.build(), //
+								CURRENT_CARD_PREFIX);
+					}
+
+					@Override
+					public void visit(final BeforeDelete context) {
+						builder.withEngine(//
+								CardEngine.newInstance() //
+										.withCard(context.card) //
+										.build(), //
+								CURRENT_CARD_PREFIX);
+					}
+
+				});
+				return builder.build();
+			}
+
+		});
+	}
+
+	private Command emailOf(final SynchronousEventTask task) {
+		return safe(new Command() {
+
+			@Override
+			public void execute(final Context context) {
+				final Supplier<EmailAccount> emailAccountSupplier = PredicateEmailAccountSupplier.of(emailAccountStore,
+						named(task.getEmailAccount()));
+				SendTemplateEmail.newInstance() //
+						.withEmailAccountSupplier(emailAccountSupplier) //
+						.withEmailServiceFactory(emailServiceFactory) //
+						.withEmailTemplateSupplier(new Supplier<Template>() {
+
+							@Override
+							public Template get() {
+								final String name = task.getEmailTemplate();
+								return emailTemplateLogic.read(name);
+							}
+
+						}) //
+						.withTemplateResolver(EngineBasedTemplateResolver.newInstance() //
+								.withEngine(emptyStringOnNull(nullOnError( //
+										UserEmailEngine.newInstance() //
+												.withDataView(dataView) //
+												.build())), //
+										USER_PREFIX) //
+								.withEngine(emptyStringOnNull(nullOnError( //
+										GroupEmailEngine.newInstance() //
+												.withDataView(dataView) //
+												.build())), //
+										GROUP_PREFIX) //
+								.withEngine(emptyStringOnNull(nullOnError( //
+										GroupUsersEmailEngine.newInstance() //
+												.withDataView(dataView) //
+												.withSeparator(EmailConstants.ADDRESSES_SEPARATOR) //
+												.build() //
+										)), //
+										GROUP_USERS_PREFIX) //
+								.build()) //
+						.build() //
+						.execute();
+			}
+
+		});
+	}
+
+	private Command scriptingOf(final SynchronousEventTask task) {
+		final Command command = ScriptCommand.newInstance() //
+				.withEngine(task.getScriptingEngine()) //
+				.withScript(task.getScriptingScript()) //
+				.withFluentApi(fluentApi) //
+				.build();
+		return task.isScriptingSafe() ? safe(command) : command;
+	}
+
+	private Predicate<CMCard> filterOf(final SynchronousEventTask task) {
+		return SynchronousEventTaskPredicate.newInstance() //
+				.withTask(task) //
+				.withUserStore(userStore) //
+				.build();
+	}
+
+}
