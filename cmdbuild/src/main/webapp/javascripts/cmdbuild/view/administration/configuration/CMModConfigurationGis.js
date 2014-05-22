4016d29c4cf6792b278d25411fb1d00f0323e0e3
Marco Cepparo <m.cepparo@tecnoteca.com>
2014-05-22 14:54:04 +0200

    #1177: Change of the Gis Navigation management
    Added the tree combo on the setup page of the Gis
    (does not work)

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/administration/configuration/CMModConfigurationGis.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/administration/configuration/CMModConfigurationGis.js
@@ -8,6 +8,14 @@
 		configFileName: 'gis',
 		
 		constructor: function() {
+			var navigationTreesStore = buildNavigationTreesStore();
+			this.navigationTreeName = new Ext.form.field.ComboBox({
+				name: "navigationTreeName",
+				fieldLabel: CMDBuild.Translation.tree_navigation, 
+				valueField: 'name',
+				displayField: 'description',
+				store: navigationTreesStore
+			});
 			this.items = [{
 				xtype: 'xcheckbox',
 				name: 'enabled',
@@ -28,11 +36,18 @@
 				fieldLabel: tr.initial_zoom,
 				minValue : 0,
 				maxValue : 25
-			}]
+			},
+			this.navigationTreeName
+		];
 
 			this.callParent(arguments);
 		},
 		
+		//override
+		populateForm: function(configurationOptions) {
+			this.callParent(arguments);
+		},
+
 		afterSubmit: function(conf) {
 			CMDBuild.Config.gis = Ext.apply(CMDBuild.Config.gis, conf);
 			CMDBuild.Config.gis.enabled = ('true' == CMDBuild.Config.gis.enabled);
@@ -44,4 +59,32 @@
 			}
 		}
 	});
+	function buildNavigationTreesStore() {
+		_CMCache.listNavigationTrees({
+			success: function() {
+				var navigationTrees = _CMCache.getNavigationTrees();
+				var data = [];
+				
+				for (var i = 0; i < navigationTrees.data.length; i++) {
+					var obj = navigationTrees.data[i];
+					data.push({
+						name: obj.name,
+						description: obj.description
+					});
+				}
+				var navigationTrees = Ext.create('Ext.data.Store', {
+					fields: ['name', 'description'],
+					data : data,
+					autoLoad: true,
+					sorters: [{
+						sorterFn: function(o1, o2){
+							return o1.get('description') <  o2.get('description') ? -1 : 1;
+						}
+					}],
+				});
+				return navigationTrees;
+			}
+		});
+		
+	}
 })();
