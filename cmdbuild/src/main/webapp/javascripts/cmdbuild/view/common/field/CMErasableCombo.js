65db7a3950141169cb5e4904773f1a66d520b605
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-05-28 12:47:09 +0200

    #679: Combo Box init with 0
    Administration > User And Groups
    The combo "Starting page at" is initialized with 0
    Administration > User And Groups > Users
    The combo "Default group" is initialized with 0

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/field/CMErasableCombo.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/field/CMErasableCombo.js
@@ -1,10 +1,10 @@
 (function() {
 
 	Ext.define('CMDBuild.view.common.field.CMErasableCombo', {
-		extend: 'CMDBuild.field.CMBaseCombo',
 		alternateClassName: 'CMDBuild.field.ErasableCombo', // Legacy class name
-
+		extend: 'CMDBuild.field.CMBaseCombo',
 		alias: 'cmerasablecombo',
+
 		trigger1cls: Ext.form.field.ComboBox.triggerCls,
 		trigger2Cls: Ext.baseCSSPrefix + 'form-clear-trigger',
 		hideTrigger1: false,
