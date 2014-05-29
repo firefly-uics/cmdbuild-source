cd701312d727b9d5211aa54866be96d33b80d38a
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-05-29 11:42:33 +0200

    #668: [Fix] Wrong card info when the grid is empty
    If apply a filter to a class, and the filter has no matches,
    the card's panel below the grid still display the last card
    infos

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/classes/CMModCardController.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/classes/CMModCardController.js
@@ -78,7 +78,7 @@
 		callForSubControllers: function(fnName, params) {
 			for (var i=0, l = this.subControllers.length, ct=null; i<l; ++i) {
 				ct = this.subControllers[i];
-				if (typeof fnName == "string" 
+				if (typeof fnName == "string"
 					&& typeof ct[fnName] == "function") {
 
 					params = Ext.isArray(params) ? params : [params];
@@ -135,11 +135,11 @@
 		},
 
 		onGridVisible: function onCardGridVisible(visible, selection) {
-			if (visible 
+			if (visible
 					&& this.entryType
 					&& this.card) {
 
-				if (selection 
+				if (selection
 					&& selection[0] && selection[0].get("Id") != this.card.get("Id")) {
 						this.gridController.openCard({
 							IdClass: this.entryType.get("id"),
@@ -149,12 +149,17 @@
 			}
 		},
 
+		/**
+		 * To clear view if there are no loaded records
+		 *
+		 * @param (Object) args
+		 * @param (Array) args[1] - loaded records array
+		 */
 		onGridLoad: function(args) {
-			// TODO notify to sub-controllers ?
-			// args[1] is the array with the loaded records
-			// so, if there are no records clear the view
-			if (args[1] && args[1].length == 0) {
+			// TODO notify to sub-controllers?
+			if (Ext.isEmpty(args[1])) {
 				this.view.getCardPanel().displayMode();
+				this.view.getCardPanel().reset();
 			}
 		}
 	});
