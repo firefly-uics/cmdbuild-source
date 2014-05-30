0ad43f91d44d9c9a06ba4b2059f7c7777e0e0bd1
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-05-30 16:11:34 +0200

    #1105: Translation of the attributes of the entities defined in administration

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/administration/menu/CMMenuPanel.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/administration/menu/CMMenuPanel.js
@@ -1,169 +1,224 @@
 (function() {
+
 	var tr = CMDBuild.Translation.administration.modmenu;
 
-	Ext.define("MenuStore", {
-		extend: "Ext.data.TreeStore",
+	Ext.define('MenuStore', {
+		extend: 'Ext.data.TreeStore',
+
 		fields: [
-			{name: "type", type: "string"},
-			{name: "text", type: "string"},
-			{name: "index", type: "ingeter"},
-			{name: "referencedClassName", type: "string"},
-			{name: "referencedElementId", type: "string"},
-			{name: "folderType", type: "string"} // used to retrieve the folder of the available items
+			{ name: 'type', type: 'string' },
+			{ name: 'text', type: 'string' },
+			{ name: 'index', type: 'int' },
+			{ name: 'referencedClassName', type: 'string' },
+			{ name: 'referencedElementId', type: 'string' },
+			{ name: 'folderType', type: 'string' } // used to retrieve the folder of the available items
 		],
-		root : {
-			text: "",
-			expanded : true,
-			children : []
+		root: {
+			text: '',
+			expanded: true,
+			children: []
 		}
 	});
 
-Ext.define("CMDBuild.Administration.MenuPanel", {
-	extend: "Ext.panel.Panel",
-	initComponent : function() {
-		this.groupId = -1,
+	Ext.define('CMDBuild.Administration.MenuPanel', {
+		extend: 'Ext.panel.Panel',
 
-		this.deleteButton = new Ext.button.Button({
-			iconCls : 'delete',
-			text : tr.delete_menu,
-			handler : this.askConfirmToDeleteMenu,
-			scope : this
-		}),
+		initComponent: function() {
+			this.groupId = -1,
 
-		this.saveButton = new Ext.button.Button({
-			text: CMDBuild.Translation.common.buttons.save,
-			id: 'saveMenuButton',
-			name: 'saveMenuButton',
-			scope: this,
-			handler: this.onSave
-		}),
+			this.deleteButton = new Ext.button.Button({
+				iconCls: 'delete',
+				text: tr.delete_menu,
+				handler: this.askConfirmToDeleteMenu,
+				scope: this
+			}),
 
-		this.abortButton = new Ext.button.Button({
-			text : CMDBuild.Translation.common.buttons.abort,
-			id : 'abortMenuButton',
-			name : 'abortMenuButton',
-			scope : this,
-			handler : this.onAbort
-		});
+			this.saveButton = new Ext.button.Button({
+				text: CMDBuild.Translation.common.buttons.save,
+				id: 'saveMenuButton',
+				name: 'saveMenuButton',
+				scope: this,
+				handler: this.onSave
+			}),
 
-		this.treePanel = Ext.create("Ext.tree.Panel", {
-			title: tr.custom_menu,
-			store: new MenuStore(),
-			border: true,
-			rootVisible: true,
-			hideHeaders: true,
-			columns: [{
-				dataIndex: "text",
-				editor: {
-					allowBlank: false,
-					xtype: "textfield"
-				},
-				flex: 1,
-				xtype: "treecolumn"
-			}],
-			plugins : {
-				ptype: "treeediting",
-				clicksToEdit: 2
-			},
-			viewConfig : {
-				plugins : {
-					ptype: "treeviewdragdrop"
-				}
-			},
-			flex: 9
-		});
+			this.abortButton = new Ext.button.Button({
+				text: CMDBuild.Translation.common.buttons.abort,
+				id: 'abortMenuButton',
+				name: 'abortMenuButton',
+				scope: this,
+				handler: this.onAbort
+			});
 
-		this.availabletreePanel = Ext.create("Ext.tree.Panel", {
-			title: tr.available_elements,
-			store: new MenuStore(),
-			border: true,
-			rootVisible: false,
-			viewConfig : {
-				plugins : {
-					ptype : 'treeviewdragdrop',
-					enableDrop: false
-				}
-			},
-			flex: 9
-		});
-
-		this.removeFromMeneButton = new Ext.button.Button({
-			iconCls : "arrow_right",
-			handler: this.onRemoveItem,
-			scope: this
-		});
-
-		this.addFolderField = new Ext.form.TriggerField({
-			allowBlank: true,
-			fieldLabel : tr.new_folder,
-			name : 'addfield_value',
-			onTriggerClick: function() {
-				this.fireEvent("cm-add-folder-click", this.getValue());
-			},
-			triggerCls: 'trigger-add',
-			treePanel: this.treePanel
-		});
-
-		Ext.apply(this, {
-			border : false,
-			frame : false,
-			cls: "x-panel-body-default-framed",
-			bodyCls: 'cmgraypanel',
-			tbar : [this.addFolderField, this.deleteButton],
-			buttonAlign: "center",
-			buttons: [this.saveButton, this.abortButton],
-			layout: {
-				type: 'hbox',
-				align:'stretch'
-			},
-			items: [
-				this.treePanel,
+			// Tree columns configuration
+			var treeColumns = [
 				{
-					xtype: "panel",
-					frame: false,
-					bodyCls: "x-panel-body-default-framed",
-					layout: {
-						type:'vbox',
-						pack:'center',
-						align:'center'
+					dataIndex: 'text',
+					editor: {
+						allowBlank: false,
+						xtype: 'textfield'
 					},
-					border: false,
-					items: [this.removeFromMeneButton],
-					margin: "3"
+					flex: 1,
+					xtype: 'treecolumn'
+				}
+			];
+
+			if (true) // TODO: To enable only if translation edit button must be displayed
+				treeColumns.push({
+					xtype: 'actioncolumn',
+					width: 30,
+					align: 'center',
+					sortable: false,
+					hideable: false,
+					menuDisabled: true,
+					fixed: true,
+
+					items: [
+						{
+							icon: 'images/icons/translate.png',
+							tooltip: CMDBuild.Translation.remove,
+							getClass: function(value, meta, record, rowIx, colIx, store) {
+								return 'x-hide-display'; // Hide the action icon
+							},
+							handler: function(grid, rowIndex, colIndex, node, e, record, rowNode) {
+								// TODO: action to do on icon click
+							}
+						}
+					]
+				});
+			// END: Tree columns configuration
+
+			this.treePanel = Ext.create('Ext.tree.Panel', {
+				title: tr.custom_menu,
+				store: new MenuStore(),
+				border: true,
+				rootVisible: true,
+				hideHeaders: true,
+				columns: treeColumns,
+				plugins: [
+					Ext.create('Ext.grid.plugin.CellEditing', {
+						clicksToEdit: 2,
+
+						listeners: {
+							beforeedit: function(editor, e, eOpts) {_debug(e.row.lastElementChild.children);
+								// show the icon
+								var cmp = Ext.select('#' + e.row.id + ' [class~="x-action-col-icon"]');
+								cmp.removeCls('x-hide-display');
+								cmp.addCls('x-grid-center-icon');
+							},
+							canceledit: function(editor, e, eOpts) {
+								// hide the icon
+								var cmp = Ext.select('#' + e.row.id + ' [class~="x-action-col-icon"]');
+								cmp.removeCls('x-grid-center-icon');
+								cmp.addCls('x-hide-display');
+							},
+							validateedit: function(editor, e, eOpts) {
+								// hide the icon
+								var cmp = Ext.select('#' + e.row.id + ' [class~="x-action-col-icon"]');
+								cmp.removeCls('x-grid-center-icon');
+								cmp.addCls('x-hide-display');
+							}
+						}
+					})
+				],
+				viewConfig: {
+					plugins: {
+						ptype: 'treeviewdragdrop'
+					}
 				},
-				this.availabletreePanel
-			]
-		});
-		
-		this.callParent(arguments);
-	},
+				flex: 9
+			});
 
-	onRemoveItem: function() {
-		var tree = this.treePanel;
-		var sm = tree.getSelectionModel();
-		var node = sm.getSelection()[0];
+			this.availabletreePanel = Ext.create('Ext.tree.Panel', {
+				title: tr.available_elements,
+				store: new MenuStore(),
+				border: true,
+				rootVisible: false,
+				viewConfig: {
+					plugins: {
+						ptype: 'treeviewdragdrop',
+						enableDrop: false
+					}
+				},
+				flex: 9
+			});
 
-		if (node && node.get("type")) {
-			this.removeTreeBranch(node);
-		}
-	},
+			this.removeFromMeneButton = new Ext.button.Button({
+				iconCls: 'arrow_right',
+				handler: this.onRemoveItem,
+				scope: this
+			});
 
-	removeTreeBranch : function(node) {
-		while (node.hasChildNodes()) {
-			this.removeTreeBranch(node.childNodes[0]);
+			this.addFolderField = new Ext.form.TriggerField({
+				allowBlank: true,
+				fieldLabel: tr.new_folder,
+				name: 'addfield_value',
+				onTriggerClick: function() {
+					this.fireEvent('cm-add-folder-click', this.getValue());
+				},
+				triggerCls: 'trigger-add',
+				treePanel: this.treePanel
+			});
+
+			Ext.apply(this, {
+				border: false,
+				frame: false,
+				cls: 'x-panel-body-default-framed',
+				bodyCls: 'cmgraypanel',
+				tbar: [this.addFolderField, this.deleteButton],
+				buttonAlign: 'center',
+				buttons: [this.saveButton, this.abortButton],
+				layout: {
+					type: 'hbox',
+					align: 'stretch'
+				},
+				items: [
+					this.treePanel, {
+						xtype: 'panel',
+						frame: false,
+						bodyCls: 'x-panel-body-default-framed',
+						layout: {
+							type: 'vbox',
+							pack: 'center',
+							align: 'center'
+						},
+						border: false,
+						items: [this.removeFromMeneButton],
+						margin: '3'
+					},
+					this.availabletreePanel
+				]
+			});
+
+			this.callParent(arguments);
+		},
+
+		onRemoveItem: function() {
+			var tree = this.treePanel;
+			var sm = tree.getSelectionModel();
+			var node = sm.getSelection()[0];
+
+			if (node && node.get('type')) {
+				this.removeTreeBranch(node);
+			}
+		},
+
+		removeTreeBranch: function(node) {
+			while (node.hasChildNodes()) {
+				this.removeTreeBranch(node.childNodes[0]);
+			}
+			var nodeType = node.get('type');
+			if (nodeType.match('report')) {
+				nodeType = 'report';
+			}
+			var availableTreeRoot = this.availabletreePanel.getRootNode();
+			var originalFolderOfTheLeaf = availableTreeRoot.findChild('folderType', nodeType);
+			// remove the node before adding it to the original tree
+			node.remove();
+			if (originalFolderOfTheLeaf) {
+				originalFolderOfTheLeaf.expand();
+				originalFolderOfTheLeaf.appendChild(node);
+			}
 		}
-		var nodeType = node.get("type");
-		if (nodeType.match("report")) {
-			nodeType = "report";
-		}
-		var availableTreeRoot = this.availabletreePanel.getRootNode();
-		var originalFolderOfTheLeaf = availableTreeRoot.findChild("folderType", nodeType);
-		//remove the node before adding it to the original tree
-		node.remove();
-		if (originalFolderOfTheLeaf) {
-			originalFolderOfTheLeaf.expand();
-			originalFolderOfTheLeaf.appendChild(node);
-		}
-	}
-});
+	});
+
 })();
