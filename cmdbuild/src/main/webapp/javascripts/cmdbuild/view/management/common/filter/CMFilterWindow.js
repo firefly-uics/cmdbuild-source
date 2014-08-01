7bd5d692d90f3ff5e4aa26936e90c4ed905ed7bd
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-08-01 10:15:39 +0200

    #1216: Search filters with SQL function do not work
    Code optimization.

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/common/filter/CMFilterWindow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/common/filter/CMFilterWindow.js
@@ -1,39 +1,47 @@
 (function() {
 
-	var titleTemplate = "{0} - {1} - {2}";
+	var titleTemplate = '{0} - {1} - {2}';
 
-	Ext.define("CMDBuild.view.management.common.filter.CMFilterWindowDelegate", {
+	Ext.define('CMDBuild.view.management.common.filter.CMFilterWindowDelegate', {
 		/**
-		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 * The filter window that call the delegate
+		 *
+		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 */
 		onCMFilterWindowApplyButtonClick: Ext.emptyFn,
 
 		/**
-		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 * The filter window that call the delegate
+		 *
+		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 */
 		onCMFilterWindowSaveButtonClick: Ext.emptyFn,
 
 		/**
-		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 * The filter window that call the delegate
+		 *
+		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 */
 		onCMFilterWindowSaveAndApplyButtonClick: Ext.emptyFn,
 
 		/**
-		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 * The filter window that call the delegate
+		 *
+		 * @params {CMDBuild.view.management.common.filter.CMFilterWindow} filterWindow
 		 */
 		onCMFilterWindowAbortButtonClick: Ext.emptyFn
 	});
 
-	Ext.define("CMDBuild.view.management.common.filter.CMFilterWindow", {
-		extend : "CMDBuild.PopupWindow",
+	Ext.define('CMDBuild.view.management.common.filter.CMFilterWindow', {
+		extend: 'CMDBuild.PopupWindow',
 
 		mixins: {
-			delegable: "CMDBuild.core.CMDelegable"
+			delegable: 'CMDBuild.core.CMDelegable'
 		},
+
+		buttonAlign: 'center',
+		items: [],
+		layout: 'accordion',
 
 		// Configuration
 		attributes: {},
@@ -44,18 +52,12 @@
 			relationTab: true,
 			functionTab: true
 		},
-		/*
-		 * In some subclass the relations
-		 * panel is used in a tab panel,
-		 * so the event to listen for detect the
-		 * first time that is shown is different (activate)
-		 */
-		firstShowDetectEvent: "expand",
+		firstShowDetectEvent: 'expand', // In some subclass the relations panel is used in a tab panel, so the event to listen for
+										// detect the first time that is shown is different (activate)
 		// END: Configuration
 
 		constructor: function() {
-			this.mixins.delegable.constructor.call(this,
-				"CMDBuild.view.management.common.filter.CMFilterWindowDelegate");
+			this.mixins.delegable.constructor.call(this, 'CMDBuild.view.management.common.filter.CMFilterWindowDelegate');
 
 			this.callParent(arguments);
 		},
@@ -63,17 +65,13 @@
 		initComponent : function() {
 			var me = this;
 
-			this.layout = "accordion";
-			this.buttonAlign = "center";
-			this.items = [];
-
 			this.setWindowTitle();
 			this.buildItems();
 			this.buildButtons();
 
 			this.callParent(arguments);
 
-			me.mon(this, "show", me.onFirstShow, this, {single: true});
+			this.mon(this, 'show', me.onFirstShow, this, { single: true });
 		},
 
 		getFilter: function() {
@@ -81,60 +79,73 @@
 				this.filter.setDirty();
 				this.filter.setLocal(true);
 				this.filter.setAttributeConfiguration(this.filterAttributesPanel.getData());
-				if (!this.filterRelationNeverExpansed) { // the panel was expanded at least once
+
+				if (!this.filterRelationNeverExpansed) // The panel was expanded at least once
 					this.filter.setRelationConfiguration(this.filterRelationsPanel.getData());
-				}
+
 				this.filter.setFunctionConfiguration(this.filterFunctionsPanel.getData());
 			}
 
 			return this.filter;
 		},
 
-		// protected
+		/**
+		 * @protected
+		 */
 		onFirstShow: function() {
 			this.filterAttributesPanel.setData(this.filter.getAttributeConfiguration());
-			// defer the setting of relations data
-			// to the moment in which the panel is expanded
+			// Defer the setting of relations data to the moment in which the panel is expanded
 			// If never expanded take the data from the filter
 			this.filterRelationNeverExpansed = true;
+
 			this.mon(this.filterRelationsPanel, this.firstShowDetectEvent, function() {
 				this.filterRelationsPanel.setData(this.filter.getRelationConfiguration());
 				this.filterRelationNeverExpansed = false;
-			}, this, {single: true});
+			}, this, { single: true });
+
 			this.filterFunctionsPanel.setData(this.filter.getFunctionConfiguration());
 		},
 
-		// protected
+		/**
+		 * @protected
+		 */
 		setWindowTitle: function() {
 			var prefix = CMDBuild.Translation.management.findfilter.window_title;
 			var et = _CMCache.getEntryTypeByName(this.className);
+
 			this.title = Ext.String.format(titleTemplate, prefix, this.filter.getName(), et.getDescription());
 		},
 
-		// protected
+		/**
+		 * @protected
+		 */
 		buildButtons: function() {
 			var me = this;
 
-			this.buttons = [{
-				text: CMDBuild.Translation.management.findfilter.apply,
-				handler: function() {
-					me.callDelegates("onCMFilterWindowApplyButtonClick", [me, me.getFilter()]);
+			this.buttons = [
+				{
+					text: CMDBuild.Translation.management.findfilter.apply,
+					handler: function() {
+						me.callDelegates('onCMFilterWindowApplyButtonClick', [me, me.getFilter()]);
+					}
+				},
+				{
+					text: CMDBuild.Translation.management.findfilter.saveandapply,
+					handler: function() {
+						me.callDelegates('onCMFilterWindowSaveAndApplyButtonClick', [me, me.getFilter()]);
+					}
+				},
+				{
+					text: CMDBuild.Translation.common.buttons.abort,
+					handler: function() {
+						me.callDelegates('onCMFilterWindowAbortButtonClick', [me]);
+					}
 				}
-			},{
-				text: CMDBuild.Translation.management.findfilter.saveandapply,
-				handler: function() {
-					me.callDelegates("onCMFilterWindowSaveAndApplyButtonClick", [me, me.getFilter()]);
-				}
-			},{
-				text: CMDBuild.Translation.common.buttons.abort,
-				handler: function() {
-					me.callDelegates("onCMFilterWindowAbortButtonClick", [me]);
-				}
-			}];
+			];
 		},
 
 		buildFilterAttributePanel: function() {
-			return new CMDBuild.view.management.common.filter.CMFilterAttributes({
+			return Ext.create('CMDBuild.view.management.common.filter.CMFilterAttributes', {
 				attributes: this.attributes,
 				className: this.className
 			});
@@ -143,16 +154,16 @@
 		// protected
 		buildItems: function() {
 			this.filterAttributesPanel = this.buildFilterAttributePanel();
-			this.filterRelationsPanel = new CMDBuild.view.management.common.filter.CMRelations({
+			this.filterRelationsPanel = Ext.create('CMDBuild.view.management.common.filter.CMRelations', {
 				attributes: this.attributes,
 				className: this.className
 			});
-			this.filterFunctionsPanel = new CMDBuild.view.management.common.filter.CMFunctions({
+			this.filterFunctionsPanel = Ext.create('CMDBuild.view.management.common.filter.CMFunctions', {
 				attributes: this.attributes,
 				className: this.className
 			});
-//			this.items = [this.filterAttributesPanel, this.filterRelationsPanel, this.filterFunctionsPanel];
 
+			// Filter tabs
 			if (this.filterTabToEnable.attributeTab)
 				this.items.push(this.filterAttributesPanel);
 
@@ -165,7 +176,8 @@
 	});
 
 	function theFilterIsDirty(me) {
-		var currentFilter = new CMDBuild.model.CMFilterModel();
+		var currentFilter = Ext.create('CMDBuild.model.CMFilterModel');
+
 		currentFilter.setAttributeConfiguration(me.filterAttributesPanel.getData());
 
 		if (me.filterRelationNeverExpansed) {
@@ -173,6 +185,7 @@
 		} else {
 			currentFilter.setRelationConfiguration(me.filterRelationsPanel.getData());
 		}
+
 		currentFilter.setFunctionConfiguration(me.filterFunctionsPanel.getData());
 
 		// The string are not equals because serialize the fields of the object not in the same
@@ -182,25 +195,26 @@
 		return true;
 	}
 
-	Ext.define("CMDBuild.view.management.common.filter.CMSaveFilterWindowDelegate", {
+	Ext.define('CMDBuild.view.management.common.filter.CMSaveFilterWindowDelegate', {
 		/**
-		 * @param {CMDBuild.view.management.common.filter.CMSaveFilterWindow} window
-		 * the window that calls the delegate
-		 * @param {CMDBuild.model.CMFilterModel} filter
-		 * the filter to save
-		 * @param {String} name
-		 * the name set in the form
+		 * @param {CMDBuild.view.management.common.filter.CMSaveFilterWindow} window - the window that calls the delegate
+		 * @param {CMDBuild.model.CMFilterModel} filter - the filter to save
+		 * @param {String} name - the name set in the form
 		 * @param {String} the description set in the form
 		 */
 		onSaveFilterWindowConfirm: Ext.emptyFn
 	});
 
-	Ext.define("CMDBuild.view.management.common.filter.CMSaveFilterWindow", {
-		extend: "Ext.window.Window",
+	Ext.define('CMDBuild.view.management.common.filter.CMSaveFilterWindow', {
+		extend: 'Ext.window.Window',
 
 		mixins: {
-			delegable: "CMDBuild.core.CMDelegable"
+			delegable: 'CMDBuild.core.CMDelegable'
 		},
+
+		bodyPadding: '5 5 1 5',
+		buttonAlign: 'center',
+		modal: true,
 
 		// configuration
 		filter: undefined, // a CMDBuild.model.CMFilterModel,
@@ -208,54 +222,54 @@
 		// configuration
 
 		constructor: function() {
-			this.mixins.delegable.constructor.call(this,
-				"CMDBuild.view.management.common.filter.CMSaveFilterWindowDelegate");
+			this.mixins.delegable.constructor.call(this, 'CMDBuild.view.management.common.filter.CMSaveFilterWindowDelegate');
 
 			this.callParent(arguments);
 		},
 
 		initComponent: function() {
-			this.modal = true;
-			this.bodyPadding = "5px 5px 1px 5px";
-
+			var me = this;
 			var canEditTheName = this.filter.isLocal();
-			this.nameField = new Ext.form.field.Text({
-				name: 'name',
+
+			this.nameField = Ext.create('Ext.form.field.Text', {
+				name: CMDBuild.core.proxy.CMProxyConstants.NAME,
 				fieldLabel: CMDBuild.Translation.administration.modClass.attributeProperties.name,
 				value: this.filter.getName(),
 				disabled: !canEditTheName,
 				width: CMDBuild.BIG_FIELD_WIDTH,
-				allowBlank: false //requires a non-empty value
+				allowBlank: false // Requires a non-empty value
 			});
 
-			this.descriptionField = new Ext.form.field.TextArea({
-				name: 'description',
+			this.descriptionField = Ext.create('Ext.form.field.TextArea', {
+				name: CMDBuild.core.proxy.CMProxyConstants.DESCRIPTION,
 				fieldLabel: CMDBuild.Translation.administration.modClass.attributeProperties.description,
 				value: this.filter.getDescription(),
 				width: CMDBuild.BIG_FIELD_WIDTH,
-				allowBlank: false //requires a non-empty value
+				allowBlank: false // Requires a non-empty value
 			});
 
 			this.items = [this.nameField, this.descriptionField];
 
-			var me = this;
-			this.buttonAlign = "center";
-			this.buttons = [{
-				text: CMDBuild.Translation.common.buttons.save,
-				handler: function() {
-					var name = me.nameField.getValue();
-					var description = me.descriptionField.getValue();
+			this.buttons = [
+				{
+					text: CMDBuild.Translation.common.buttons.save,
+					handler: function() {
+						var name = me.nameField.getValue();
+						var description = me.descriptionField.getValue();
 
-					me.callDelegates("onSaveFilterWindowConfirm", [me, me.filter, name, description]);
+						me.callDelegates('onSaveFilterWindowConfirm', [me, me.filter, name, description]);
+					}
+				},
+				{
+					text: CMDBuild.Translation.common.buttons.abort,
+					handler: function() {
+						me.destroy();
+					}
 				}
-			}, {
-				text: CMDBuild.Translation.common.buttons.abort,
-				handler: function() {
-					me.destroy();
-				}
-			}];
+			];
 
 			this.callParent(arguments);
 		}
 	});
+
 })();
