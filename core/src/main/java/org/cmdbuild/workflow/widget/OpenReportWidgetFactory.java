0e3640be5f1736e425662d4f9afafc0a36a94137
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-08-07 10:50:05 +0200

    #1121: Read-only attributes in report widget
    Server side implementation

--- core/src/main/java/org/cmdbuild/workflow/widget/OpenReportWidgetFactory.java
+++ core/src/main/java/org/cmdbuild/workflow/widget/OpenReportWidgetFactory.java
@@ -1,5 +1,10 @@
 package org.cmdbuild.workflow.widget;
 
+import static org.apache.commons.lang3.StringUtils.defaultString;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.Validate;
@@ -8,24 +13,29 @@ import org.cmdbuild.model.widget.Widget;
 import org.cmdbuild.notification.Notifier;
 import org.cmdbuild.services.template.store.TemplateRepository;
 
+import com.google.common.base.Splitter;
+
 public class OpenReportWidgetFactory extends ValuePairWidgetFactory {
 
 	private static final String WIDGET_NAME = "createReport";
 
+	/*
+	 * TODO: if a day we'll use multiple report types, use this now the only
+	 * allowed type is "custom" and is managed client side
+	 */
+	private static final String REPORT_TYPE = "ReportType";
 	public static final String REPORT_CODE = "ReportCode";
 	public static final String FORCE_PDF = "ForcePDF";
 	public static final String FORCE_CSV = "ForceCSV";
-
-	// TODO: use these when implementing save
+	/*
+	 * TODO: use these when implementing save
+	 */
 	private static final String SAVE_TO_ALFRESCO = "StoreInAlfresco"; // hahahah
 	public static final String STORE_IN_PROCESS = "StoreInProcess";
-
-	// TODO: if a day we'll use multiple report types, use this
-	// now the only allowed type is "custom" and is managed client side
-	private static final String REPORT_TYPE = "ReportType";
+	public static final String READ_ONLY_ATTRIBUTES = "ReadOnlyAttributes";
 
 	private static final String[] KNOWN_PARAMETERS = { BUTTON_LABEL, REPORT_TYPE, REPORT_CODE, SAVE_TO_ALFRESCO,
-			STORE_IN_PROCESS, FORCE_CSV, FORCE_PDF };
+			STORE_IN_PROCESS, FORCE_CSV, FORCE_PDF, READ_ONLY_ATTRIBUTES };
 
 	public OpenReportWidgetFactory(final TemplateRepository templateRespository, final Notifier notifier) {
 		super(templateRespository, notifier);
@@ -45,6 +55,7 @@ public class OpenReportWidgetFactory extends ValuePairWidgetFactory {
 		widget.setReportCode(reportCode);
 		widget.setPreset(extractUnmanagedParameters(valueMap, KNOWN_PARAMETERS));
 		forceFormat(valueMap, widget);
+		widget.setReadOnlyAttributes(readOnlyAttributesOf(readString(valueMap.get(READ_ONLY_ATTRIBUTES))));
 
 		return widget;
 	}
@@ -56,4 +67,18 @@ public class OpenReportWidgetFactory extends ValuePairWidgetFactory {
 			widget.setForceFormat("csv");
 		}
 	}
+
+	private List<String> readOnlyAttributesOf(final String value) {
+		final List<String> readOnlyAttributes;
+		if (isEmpty(value)) {
+			readOnlyAttributes = Collections.emptyList();
+		} else {
+			readOnlyAttributes = Splitter.on(",") //
+					.omitEmptyStrings() //
+					.trimResults() //
+					.splitToList(defaultString(value));
+		}
+		return readOnlyAttributes;
+	}
+
 }
