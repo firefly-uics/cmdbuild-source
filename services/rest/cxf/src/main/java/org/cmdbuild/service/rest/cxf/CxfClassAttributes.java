e80db61c343f12d1a4441689801002a48593468f
Davide Pavan <d.pavan@tecnoteca.com>
2014-08-18 15:42:04 +0200

    #1040: RESTful API implementation - Attribute list
    Changed classes organization.

---
+++ services/rest/cxf/src/main/java/org/cmdbuild/service/rest/cxf/CxfClassAttributes.java
@@ -0,0 +1,69 @@
+package org.cmdbuild.service.rest.cxf;
+
+import static com.google.common.collect.FluentIterable.from;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+
+import org.cmdbuild.common.utils.PagedElements;
+import org.cmdbuild.dao.entrytype.CMAttribute;
+import org.cmdbuild.dao.entrytype.CMClass;
+import org.cmdbuild.logic.data.access.DataAccessLogic.AttributesQuery;
+import org.cmdbuild.service.rest.ClassAttributes;
+import org.cmdbuild.service.rest.dto.AttributeDetail;
+import org.cmdbuild.service.rest.dto.AttributeDetailResponse;
+import org.cmdbuild.service.rest.dto.DetailResponseMetadata;
+import org.cmdbuild.service.rest.serialization.AttributeTypeResolver;
+import org.cmdbuild.service.rest.serialization.ToAttributeDetail;
+
+public class CxfClassAttributes extends CxfService implements ClassAttributes {
+
+	private static final AttributeTypeResolver ATTRIBUTE_TYPE_RESOLVER = new AttributeTypeResolver();
+
+	@Context
+	protected SecurityContext securityContext;
+
+	@Context
+	protected UriInfo uriInfo;
+
+	@Override
+	public AttributeDetailResponse getAttributes(final String name, final boolean activeOnly, final Integer limit,
+			final Integer offset) {
+		final CMClass target = userDataAccessLogic().findClass(name);
+		if (target == null) {
+			errorHandler().entryTypeNotFound(name);
+		}
+		final PagedElements<CMAttribute> filteredAttributes = userDataAccessLogic().getAttributes( //
+				name, //
+				activeOnly, //
+				new AttributesQuery() {
+
+					@Override
+					public Integer limit() {
+						return limit;
+					}
+
+					@Override
+					public Integer offset() {
+						return offset;
+					}
+
+				});
+
+		final ToAttributeDetail toAttributeDetails = ToAttributeDetail.newInstance() //
+				.withAttributeTypeResolver(ATTRIBUTE_TYPE_RESOLVER) //
+				.withDataView(systemDataView()) //
+				.withErrorHandler(errorHandler()) //
+				.build();
+		final Iterable<AttributeDetail> elements = from(filteredAttributes) //
+				.transform(toAttributeDetails);
+		return AttributeDetailResponse.newInstance() //
+				.withElements(elements) //
+				.withMetadata(DetailResponseMetadata.newInstance() //
+						.withTotal(filteredAttributes.totalSize()) //
+						.build()) //
+				.build();
+	}
+
+}
