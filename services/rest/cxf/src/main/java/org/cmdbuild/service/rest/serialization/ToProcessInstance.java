deecfc3757152c259ebe438262b6c0cddb5f8383
Davide Pavan <d.pavan@tecnoteca.com>
2014-08-27 12:15:31 +0200

    #1040: RESTful API implementation - Process instances management
    First implementation.

---
+++ services/rest/cxf/src/main/java/org/cmdbuild/service/rest/serialization/ToProcessInstance.java
@@ -0,0 +1,38 @@
+package org.cmdbuild.service.rest.serialization;
+
+import org.cmdbuild.service.rest.dto.ProcessInstance;
+import org.cmdbuild.workflow.user.UserProcessInstance;
+
+import com.google.common.base.Function;
+
+public class ToProcessInstance implements Function<UserProcessInstance, ProcessInstance> {
+
+	public static class Builder implements org.apache.commons.lang3.builder.Builder<ToProcessInstance> {
+
+		private Builder() {
+			// use static method
+		}
+
+		@Override
+		public ToProcessInstance build() {
+			return new ToProcessInstance(this);
+		}
+
+	}
+
+	public static Builder newInstance() {
+		return new Builder();
+	}
+
+	private ToProcessInstance(final Builder builder) {
+		// nothing to do
+	}
+
+	@Override
+	public ProcessInstance apply(final UserProcessInstance input) {
+		final ProcessInstance processInstance = new ProcessInstance();
+		processInstance.setName(input.getProcessInstanceId());
+		return processInstance;
+	}
+
+}
