541f3c36248718923e2007f4bb281083f8a30472
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-10-21 17:32:55 +0200

    #1299: [Fix] Wrong values in the list of the XPDL versions
    Code optimization

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/core/proxy/CMProxyWorkflow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/core/proxy/CMProxyWorkflow.js
@@ -4,16 +4,6 @@
 		alternateClassName: 'CMDBuild.ServiceProxy.workflow', // Legacy class name
 
 		statics: {
-			getstartactivitytemplate: function(classId, p) {
-				CMDBuild.ServiceProxy.core.doRequest(Ext.apply({
-					url: CMDBuild.ServiceProxy.url.workflow.getStartActivity,
-					method: 'GET',
-					params: {
-						classId: classId
-					}
-				}, p));
-			},
-
 			getActivityInstance: function(params, conf) {
 				conf.url = CMDBuild.ServiceProxy.url.workflow.getActivityInstance;
 				conf.method = 'GET';
@@ -29,9 +19,41 @@
 				CMDBuild.ServiceProxy.core.doRequest(conf);
 			},
 
+			getstartactivitytemplate: function(classId, p) {
+				CMDBuild.ServiceProxy.core.doRequest(Ext.apply({
+					url: CMDBuild.ServiceProxy.url.workflow.getStartActivity,
+					method: 'GET',
+					params: {
+						classId: classId
+					}
+				}, p));
+			},
+
+			/**
+			 * @param {} parameters
+			 */
+			getXpdlVersions: function(parameters) {
+				CMDBuild.Ajax.request({
+					method: 'POST',
+					url: CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlVersions,
+					params: parameters.params,
+					scope: parameters.scope,
+					success: parameters.success,
+					callback: parameters.callback,
+					failure: parameters.failure
+				});
+			},
+
 			isPorcessUpdated: function(p) {
 				p.url = CMDBuild.ServiceProxy.url.workflow.isProcessUpdated;
 				p.method = 'GET';
+
+				CMDBuild.ServiceProxy.core.doRequest(p);
+			},
+
+			saveActivity: function(p) {
+				p.url = CMDBuild.ServiceProxy.url.workflow.saveActivity;
+				p.method = 'POST';
 
 				CMDBuild.ServiceProxy.core.doRequest(p);
 			},
@@ -43,11 +65,18 @@
 				CMDBuild.ServiceProxy.core.doRequest(p);
 			},
 
-			saveActivity: function(p) {
-				p.url = CMDBuild.ServiceProxy.url.workflow.saveActivity;
-				p.method = 'POST';
-
-				CMDBuild.ServiceProxy.core.doRequest(p);
+			/**
+			 * @param {} parameters
+			 */
+			xpdlUpload: function(parameters) {
+				parameters.form.submit({
+					url: CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlUpload,
+					params: parameters.params,
+					scope: parameters.scope,
+					success: parameters.success,
+					callback: parameters.callback,
+					failure: parameters.failure
+				});
 			}
 		}
 	});
