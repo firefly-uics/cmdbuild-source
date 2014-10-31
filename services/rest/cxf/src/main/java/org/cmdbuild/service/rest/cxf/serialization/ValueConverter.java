12e12900d27e7ba811ee6cd4ce1df12c5e3aba94
Umberto Calligaro <u.calligaro@tecnoteca.com>
2014-10-31 14:59:32 +0100

    #1040: RESTful API implementation

--- services/rest/cxf/src/main/java/org/cmdbuild/service/rest/cxf/serialization/ValueConverter.java
+++ services/rest/cxf/src/main/java/org/cmdbuild/service/rest/cxf/serialization/ValueConverter.java
@@ -38,6 +38,8 @@ class ValueConverter {
 	public static Builder newInstance() {
 		return new Builder();
 	}
+	
+	private static final IdAndDescription NULL = new IdAndDescription(null, null);
 
 	private final CMClass type;
 
@@ -73,7 +75,7 @@ class ValueConverter {
 			}
 
 			private Object idFromIdAndDescription(final Object value) {
-				return IdAndDescription.class.cast(value).getId();
+				return ((value == null)? NULL : IdAndDescription.class.cast(value)).getId();
 			}
 
 		}.value();
