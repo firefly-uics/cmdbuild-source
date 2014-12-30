cf3892b508b9111131c937ac7ceae6c42e4b7116
Alessandro Romanin <a.romanin@tecnoteca.com>
2014-12-30 11:54:17 +0100

    #1404: [Fix] No loading frame on report create call

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/report/CMSingleReportPage.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/report/CMSingleReportPage.js
@@ -19,47 +19,51 @@
 	        cls: "x-panel-body-default-framed",
 		        layout: {
 	            pack: 'center'
-	        },	    	
+	        },
 	    	items: [
-	            { 	xtype: 'button', 
-	            	width: '80px', 
-	            	text:  CMDBuild.Translation.common.buttons.save, 
+	            { 	xtype: 'button',
+	            	width: '80px',
+	            	text:  CMDBuild.Translation.common.buttons.save,
 	            	handler: function() {
 	            	/*TODO*/
-//	        	        var xhr = new XMLHttpRequest(); 
+//	        	        var xhr = new XMLHttpRequest();
 //	        	        var cmUrl = 'services/json/management/modreport/printreportfactory';
-//	        	        xhr.open('GET', cmUrl, true); 
+//	        	        xhr.open('GET', cmUrl, true);
 //	        	        xhr.responseType = "blob";
-//	        	        xhr.onreadystatechange = function () { 
+//	        	        xhr.onreadystatechange = function () {
 //	        	            if (xhr.readyState == 4) {
 //	        	                var a = 4;//xhr.response);
 //	        	            }
 //	        	        };
 //	        	        xhr.send(null);
-	            	} 
+	            	}
 	            }
 	        ]
-	    }],	    
+	    }],
 		autoScroll: false,
 		buttonAlign: 'center',
 		reportId: undefined,
 		controller: undefined,
-	
+
 		setReportId: function(reportId) {
 			this.reportId = reportId;
 		},
 		setController: function(controller) {
 			this.controller = controller;
 		},
-	
+
 		onReportTypeSelected : function(report) {
 		},
-	
+
 		requestReport: function(reportParams) {
-			Ext.Ajax.request({
+			CMDBuild.LoadMask.get().show();
+
+			CMDBuild.Ajax.request({
 				url: 'services/json/management/modreport/createreportfactory',
 				params: reportParams,
 				success: function(response) {
+					CMDBuild.LoadMask.get().hide();
+
 					var ret = Ext.JSON.decode(response.responseText);
 					if(ret.filled) { // report with no parameters
 						var popup = window.open("services/json/management/modreport/printreportfactory", "Report", "height=400,width=550,status=no,toolbar=no,scrollbars=yes,menubar=no,location=no,resizable");
@@ -77,7 +81,7 @@
 				scope: this
 			});
 		}
-	
+
 	});
 })();
 
