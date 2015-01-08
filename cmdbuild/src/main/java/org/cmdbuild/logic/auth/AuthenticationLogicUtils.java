f5c0a057773551be84aff614d6d7bb28b35da2c7
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-01-08 15:21:35 +0100

    #682: Demomode doesn't work anymore

--- cmdbuild/src/main/java/org/cmdbuild/logic/auth/AuthenticationLogicUtils.java
+++ cmdbuild/src/main/java/org/cmdbuild/logic/auth/AuthenticationLogicUtils.java
@@ -32,42 +32,27 @@ public class AuthenticationLogicUtils {
 
 	public static void assureAdmin(final HttpServletRequest request, final AdminAccess adminAccess) {
 		if (adminAccess == AdminAccess.FULL) {
-			if (!isLoggedUserFullAdministrator()) {
+			if (!privilegedAdministrator()) {
 				throw AuthExceptionType.AUTH_NOT_AUTHORIZED.createException();
 			}
 		} else if (adminAccess == AdminAccess.DEMOSAFE) {
-			if (!isLoggedUserFullAdministrator() && !isLoggedUserDemoSafe()) {
+			if (!(privilegedAdministrator() && demoAdministrator())) {
 				throw AuthExceptionType.AUTH_DEMO_MODE.createException();
 			}
 		}
 	}
 
-	private static boolean isLoggedUserFullAdministrator() {
+	private static boolean privilegedAdministrator() {
 		final OperationUser operationUser = applicationContext().getBean(UserStore.class).getUser();
 		return operationUser != null
 				&& (operationUser.hasAdministratorPrivileges() || operationUser.hasDatabaseDesignerPrivileges());
 	}
 
-	private static boolean isLoggedUserDemoSafe() {
+	private static boolean demoAdministrator() {
 		final OperationUser operationUser = applicationContext().getBean(UserStore.class).getUser();
 		final String demoModeAdmin = CmdbuildProperties.getInstance().getDemoModeAdmin().trim();
 		return demoModeAdmin.equals(StringUtils.EMPTY)
 				|| demoModeAdmin.equals(operationUser.getAuthenticatedUser().getUsername());
-	}
-
-	// TODO
-	private static boolean doAutoLogin() {
-		// try {
-		// final String username = AuthProperties.getInstance().getAutologin();
-		// if (username != null && username.length() > 0) {
-		// loginAs(username);
-		// Log.OTHER.info("Autologin with user " + username);
-		// return true;
-		// }
-		// } catch (final Exception e) {
-		// Log.OTHER.warn("Autologin failed");
-		// }
-		return false;
 	}
 
 }
