461aba73ea1129f89b7fc47ebf82a7a80c8fb093
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-01-12 11:11:25 +0100

    #1423: [Fix] No Processes accordion update on create/delete/modify actions
    Code optimization

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/core/proxy/CMProxyWorkflow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/core/proxy/CMProxyWorkflow.js
@@ -3,81 +3,102 @@
 	Ext.define('CMDBuild.core.proxy.CMProxyWorkflow', {
 		alternateClassName: 'CMDBuild.ServiceProxy.workflow', // Legacy class name
 
-		statics: {
-			getActivityInstance: function(params, conf) {
-				conf.url = CMDBuild.ServiceProxy.url.workflow.getActivityInstance;
-				conf.method = 'GET';
-				conf.params = params;
-				conf.important = true;
+		requires: ['CMDBuild.core.proxy.CMProxyUrlIndex'],
 
-				if (typeof conf.callback == "undefined") {
-					conf.callback = function() {
-						CMDBuild.LoadMask.get().hide();
-					};
-				}
+		singleton: true,
 
-				CMDBuild.ServiceProxy.core.doRequest(conf);
-			},
+		/**
+		 * @param {Ext.form.Basic} form
+		 * @param {String} version
+		 * @param {Object} parameters
+		 */
+		downloadSubmit: function(form, version, parameters) {
+			var url = CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlDownload;
 
-			getstartactivitytemplate: function(classId, p) {
-				CMDBuild.ServiceProxy.core.doRequest(Ext.apply({
-					url: CMDBuild.ServiceProxy.url.workflow.getStartActivity,
-					method: 'GET',
-					params: {
-						classId: classId
-					}
-				}, p));
-			},
+			if (version == CMDBuild.core.proxy.CMProxyConstants.TEMPLATE || Ext.isEmpty(version))
+				url = CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlDownloadTemplate;
 
-			/**
-			 * @param {} parameters
-			 */
-			getXpdlVersions: function(parameters) {
-				CMDBuild.Ajax.request({
-					method: 'POST',
-					url: CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlVersions,
-					params: parameters.params,
-					scope: parameters.scope,
-					success: parameters.success,
-					callback: parameters.callback,
-					failure: parameters.failure
-				});
-			},
+			form.submit({
+				method: 'GET',
+				url: url,
+				target: '_self',
+				params: parameters.params
+			});
+		},
 
-			isPorcessUpdated: function(p) {
-				p.url = CMDBuild.ServiceProxy.url.workflow.isProcessUpdated;
-				p.method = 'GET';
+		getActivityInstance: function(params, conf) {
+			conf.url = CMDBuild.ServiceProxy.url.workflow.getActivityInstance;
+			conf.method = 'GET';
+			conf.params = params;
+			conf.important = true;
 
-				CMDBuild.ServiceProxy.core.doRequest(p);
-			},
-
-			saveActivity: function(p) {
-				p.url = CMDBuild.ServiceProxy.url.workflow.saveActivity;
-				p.method = 'POST';
-
-				CMDBuild.ServiceProxy.core.doRequest(p);
-			},
-
-			terminateActivity: function(p) {
-				p.url = CMDBuild.ServiceProxy.url.workflow.abortProcess;
-				p.method = 'POST';
-
-				CMDBuild.ServiceProxy.core.doRequest(p);
-			},
-
-			/**
-			 * @param {} parameters
-			 */
-			xpdlUpload: function(parameters) {
-				parameters.form.submit({
-					url: CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlUpload,
-					params: parameters.params,
-					scope: parameters.scope,
-					success: parameters.success,
-					callback: parameters.callback,
-					failure: parameters.failure
-				});
+			if (typeof conf.callback == "undefined") {
+				conf.callback = function() {
+					CMDBuild.LoadMask.get().hide();
+				};
 			}
+
+			CMDBuild.ServiceProxy.core.doRequest(conf);
+		},
+
+		getstartactivitytemplate: function(classId, p) {
+			CMDBuild.ServiceProxy.core.doRequest(Ext.apply({
+				url: CMDBuild.ServiceProxy.url.workflow.getStartActivity,
+				method: 'GET',
+				params: {
+					classId: classId
+				}
+			}, p));
+		},
+
+		/**
+		 * @param {} parameters
+		 */
+		getXpdlVersions: function(parameters) {
+			CMDBuild.Ajax.request({
+				method: 'POST',
+				url: CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlVersions,
+				params: parameters.params,
+				scope: parameters.scope,
+				success: parameters.success,
+				callback: parameters.callback,
+				failure: parameters.failure
+			});
+		},
+
+		isPorcessUpdated: function(p) {
+			p.url = CMDBuild.ServiceProxy.url.workflow.isProcessUpdated;
+			p.method = 'GET';
+
+			CMDBuild.ServiceProxy.core.doRequest(p);
+		},
+
+		saveActivity: function(p) {
+			p.url = CMDBuild.ServiceProxy.url.workflow.saveActivity;
+			p.method = 'POST';
+
+			CMDBuild.ServiceProxy.core.doRequest(p);
+		},
+
+		terminateActivity: function(p) {
+			p.url = CMDBuild.ServiceProxy.url.workflow.abortProcess;
+			p.method = 'POST';
+
+			CMDBuild.ServiceProxy.core.doRequest(p);
+		},
+
+		/**
+		 * @param {} parameters
+		 */
+		xpdlUpload: function(parameters) {
+			parameters.form.submit({
+				url: CMDBuild.core.proxy.CMProxyUrlIndex.workflow.xpdlUpload,
+				params: parameters.params,
+				scope: parameters.scope,
+				success: parameters.success,
+				callback: parameters.callback,
+				failure: parameters.failure
+			});
 		}
 	});
 
