ee558700703b2d423533577b81717600ef491ec4
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-01-12 14:16:25 +0100

    #1423: [Fix] No Processes accordion update on create/delete/modify actions
    Code optimization

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/common/widgets/CMWidgetController.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/common/widgets/CMWidgetController.js
@@ -1,75 +1,146 @@
 (function() {
-	/*
-	 * Use it as mixin to have basic functionalities for
-	 * CMWidgetController
+
+	/**
+	 * Use it as mixin to have basic functionalities for CMWidgetController
 	 */
-	Ext.define("CMDBuild.controller.management.common.widgets.CMWidgetController", {
+	Ext.define('CMDBuild.controller.management.common.widgets.CMWidgetController', {
+
+		requires: ['CMDBuild.core.proxy.CMProxyConstants'],
 
 		statics: {
-			WIDGET_NAME: "",
+			WIDGET_NAME: '',
 			getTemplateResolverServerVars: getTemplateResolverServerVarsFromModel
 		},
 
-		constructor: function(view, ownerController, widgetConf, clientForm, card) {
-			if (typeof view != "object") {
-				throw "The view of a WidgetController must be an object";
-			}
+		/**
+		 * @property {CMDBuild.model.CMActivityInstance}
+		 */
+		card: undefined,
 
-			if (typeof widgetConf != "object") {
-				throw "The widget configuration is mandatory";
-			}
+		/**
+		 * @property {Ext.form.Basic}
+		 */
+		clientForm: undefined,
+
+		/**
+		 * @cfg {CMDBuild.controller.management.common.CMWidgetManagerController}
+		 */
+		ownerController: undefined,
+
+		/**
+		 * @property {CMDBuild.Management.TemplateResolver}
+		 */
+		templateResolver: undefined,
+
+		/**
+		 * @property {CMDBuild.view.management.common.widgets.linkCards.LinkCards}
+		 */
+		view: undefined,
+
+		/**
+		 * @cfg {Object}
+		 */
+		widgetConf: undefined,
+
+		/**
+		 * @param {CMDBuild.view.management.common.widgets.CMManageEmail} view
+		 * @param {CMDBuild.controller.management.common.CMWidgetManagerController} ownerController
+		 * @param {Object} widgetConf
+		 * @param {Ext.form.Basic} clientForm
+		 * @param {CMDBuild.model.CMActivityInstance} card
+		 */
+		constructor: function(view, ownerController, widgetConf, clientForm, card) {
+			if (typeof view != 'object')
+				throw 'The view of a WidgetController must be an object';
+
+			if (typeof widgetConf != 'object')
+				throw 'The widget configuration is mandatory';
 
 			this.WIDGET_NAME = this.self.WIDGET_NAME;
 
-			this.view = view;
-			this.ownerController = ownerController;
-			this.widgetConf = widgetConf;
-			this.clientForm = clientForm;
 			this.card = card;
-
+			this.clientForm = clientForm;
+			this.ownerController = ownerController;
+			this.view = view;
+			this.widgetConf = widgetConf;
 			this.outputName = this.widgetConf.outputName;
 		},
 
-		toString: function() {
-			return Ext.getClassName(this);
-		},
+		/**
+		 * @abstract
+		 */
+		beforeActiveView: Ext.emptyFn,
 
-		isBusy: function() {
-			return false;
-		},
+		/**
+		 * @abstract
+		 */
+		destroy: Ext.emptyFn,
 
+		/**
+		 * @return {Mixed}
+		 */
 		getData: function() {
 			return null;
 		},
 
+		/**
+		 * @param {String}
+		 */
+		getLabel: function() {
+			return this.widgetConf[CMDBuild.core.proxy.CMProxyConstants.LABEL];
+		},
+
+		/**
+		 * @param {String} variableName
+		 *
+		 * @return {Mixed}
+		 */
 		getVariable: function(variableName) {
 			try {
 				return this.templateResolver.getVariable(variableName);
 			} catch (e) {
-				_debug("There is no template resolver");
+				_debug('There is no template resolver');
+
 				return undefined;
 			}
-		},
-
-		getWidgetId: function() {
-			return this.widgetConf.id;
-		},
-
-		getLabel: function() {
-			return this.widgetConf.label;
-		},
-
-		isValid: function() {
-			return true;
 		},
 
 		getTemplateResolverServerVars: function() {
 			return getTemplateResolverServerVarsFromModel(this.card);
 		},
 
-		beforeActiveView: Ext.emptyFn,
-		destroy: Ext.emptyFn,
-		onEditMode: Ext.emptyFn
+		/**
+		 * @return {Int}
+		 */
+		getWidgetId: function() {
+			return this.widgetConf[CMDBuild.core.proxy.CMProxyConstants.ID];
+		},
+
+		/**
+		 * @return {Boolean}
+		 */
+		isBusy: function() {
+			return false;
+		},
+
+		/**
+		 * @return {Boolean}
+		 */
+		isValid: function() {
+			return true;
+		},
+
+		/**
+		 * @abstract
+		 */
+		onEditMode: Ext.emptyFn,
+
+		/**
+		 * @return {String}
+		 */
+		toString: function() {
+			return Ext.getClassName(this);
+		}
 	});
 
 	function getTemplateResolverServerVarsFromModel(model) {
@@ -77,31 +148,28 @@
 
 		if (model) {
 			var pi = null;
-			if (Ext.getClassName(model) == "CMDBuild.model.CMActivityInstance") {
-				// Retrieve the process instance because it stores
-				// the data. this.card has only the varibles to show in this step
-				// (is the activity instance)
+
+			if (Ext.getClassName(model) == 'CMDBuild.model.CMActivityInstance') {
+				// Retrieve the process instance because it stores the data. this.card has only the varibles to show in this step (is the activity instance)
 				pi = _CMWFState.getProcessInstance();
-			} else if (Ext.getClassName(model) == "CMDBuild.model.CMProcessInstance") {
+			} else if (Ext.getClassName(model) == 'CMDBuild.model.CMProcessInstance') {
 				pi = model;
 			}
 
-			if (pi != null) {
-				// The processes use a new serialization.
-				// Add backward compatibility attributes
-				// to the card values
+			if (pi != null) { // The processes use a new serialization. Add backward compatibility attributes to the card values
 				out = Ext.apply({
-					"Id": pi.get("Id"),
-					"IdClass": pi.get("IdClass"),
-					"IdClass_value": pi.get("IdClass_value")
+					'Id': pi.get('Id'),
+					'IdClass': pi.get('IdClass'),
+					'IdClass_value': pi.get('IdClass_value')
 				}, pi.getValues());
 			} else {
 				out = model.raw || model.data;
 			}
 		}
 
-		_debug("Server vars", out);
+		_debug('Server vars', out);
 
 		return out;
 	}
+
 })();
