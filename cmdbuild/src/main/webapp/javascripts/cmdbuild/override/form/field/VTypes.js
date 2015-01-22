589fdf16f5c63281192f9edc27ca5712ae4f945a
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-01-22 16:52:00 +0100

    #1445: [Fix] Javascript problems on usernames/group descriptions/group name with quotes and double quotes
    JS client fixes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/override/form/field/VTypes.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/override/form/field/VTypes.js
@@ -1,9 +1,32 @@
 (function() {
 
 	/**
-	 * To add multimail as a VType to validate a field with multiple email addresses separated by commas (,)
+	 * Custom VTypes:
+	 * 		- multimail: to validate a field with multiple email addresses separated by commas (,)
+	 * 		- alphanumextended: to validate user names /[a-z0-9_-.]/i
 	 */
 	Ext.apply(Ext.form.field.VTypes, {
+		/**
+		 * @param {String} value
+		 *
+		 * @return {Boolean}
+		 */
+		alphanumextended: function(value) {
+			var username = /^[a-zA-Z0-9_.-]+$/;
+
+			return username.test(value);
+		},
+
+		/**
+		 * @type {String}
+		 */
+		alphanumextendedText: 'This field should only contain letters, numbers and underscore (_), hyphen (-) and dot (.)',
+
+		/**
+		 * @type {RegExp}
+		 */
+		alphanumextendedMask: /[a-z0-9_.-]/i,
+
 		/**
 		 * The function used to validated multiple email addresses on a single line
 		 *
@@ -29,14 +52,14 @@
 		/**
 		 * The error text to display when the multi email validation function returns false
 		 *
-		 * @type String
+		 * @type {String}
 		 */
 		multiemailText: 'This field should be an e-mail address, or a list of email addresses separated by commas (,) in the format "user@domain.com,test@test.com"',
 
 		/**
 		 * The keystroke filter mask to be applied on multi email input
 		 *
-		 * @type RegExp
+		 * @type {RegExp}
 		 */
 		multiemailMask: /[\w.\-@'"!#$%&'*+/=?^_`{|}~,]/i
 	});
