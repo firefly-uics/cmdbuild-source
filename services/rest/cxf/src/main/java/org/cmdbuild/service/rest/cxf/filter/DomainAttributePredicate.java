3e24897f34a76414bdcbfe642c9cec79a793f70c
Davide Pavan <d.pavan@tecnoteca.com>
2015-01-27 15:31:22 +0100

    #1040: RESTful API implementation - Domains management
    Filter improvements.

--- services/rest/cxf/src/main/java/org/cmdbuild/service/rest/cxf/filter/DomainAttributePredicate.java
+++ services/rest/cxf/src/main/java/org/cmdbuild/service/rest/cxf/filter/DomainAttributePredicate.java
@@ -1,10 +1,18 @@
 package org.cmdbuild.service.rest.cxf.filter;
 
+import static com.google.common.base.Functions.toStringFunction;
+import static com.google.common.collect.FluentIterable.from;
+import static com.google.common.collect.Sets.newHashSet;
 import static java.lang.String.format;
-import static org.cmdbuild.logic.data.access.filter.model.Predicates.equalTo;
+import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
+import static org.cmdbuild.dao.entrytype.Functions.name;
 import static org.cmdbuild.service.rest.constants.Serialization.CLASS_DESTINATION;
 import static org.cmdbuild.service.rest.constants.Serialization.CLASS_SOURCE;
 
+import java.util.Collection;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.cmdbuild.dao.entrytype.CMClass;
 import org.cmdbuild.dao.entrytype.CMDomain;
 import org.cmdbuild.logic.data.access.filter.model.And;
 import org.cmdbuild.logic.data.access.filter.model.Attribute;
@@ -25,6 +33,7 @@ import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
 
 public class DomainAttributePredicate implements Predicate<CMDomain>, PredicateVisitor, LoggingSupport {
 
@@ -48,27 +57,49 @@ public class DomainAttributePredicate implements Predicate<CMDomain>, PredicateV
 
 	@Override
 	public void visit(final And predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final Contains predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		final boolean _output;
+		final Object expected = predicate.getValue();
+		if (CLASS_SOURCE.equals(attribute.getName())) {
+			_output = contains(input.getClass1(), expected);
+		} else if (CLASS_DESTINATION.equals(attribute.getName())) {
+			_output = contains(input.getClass2(), expected);
+		} else {
+			logger.warn(marker, format("attribute '%s' not supported", attribute.getName()));
+			_output = true;
+		}
+		output = _output;
+
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
+	}
+
+	private boolean contains(final CMClass target, final Object expected) {
+		final Collection<CMClass> classes = newHashSet(target.getDescendants());
+		classes.add(target);
+		return from(classes) //
+				.transform(name()) //
+				.contains(expected);
 	}
 
 	@Override
 	public void visit(final EndsWith predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final EqualTo predicate) {
 		final boolean _output;
+		final Object expected = predicate.getValue();
 		if (CLASS_SOURCE.equals(attribute.getName())) {
-			_output = equalTo(input.getClass1().getName()).equals(predicate);
+			_output = input.getClass1().getName().equals(expected);
 		} else if (CLASS_DESTINATION.equals(attribute.getName())) {
-			_output = equalTo(input.getClass2().getName()).equals(predicate);
+			_output = input.getClass2().getName().equals(expected);
 		} else {
+			logger.warn(marker, format("attribute '%s' not supported", attribute.getName()));
 			_output = true;
 		}
 		output = _output;
@@ -76,42 +107,61 @@ public class DomainAttributePredicate implements Predicate<CMDomain>, PredicateV
 
 	@Override
 	public void visit(final GreaterThan predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final In predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		final boolean _output;
+		final FluentIterable<String> expected = from(predicate.getValues()) //
+				.transform(toStringFunction());
+		if (CLASS_SOURCE.equals(attribute.getName())) {
+			_output = expected.contains(input.getClass1().getName());
+		} else if (CLASS_DESTINATION.equals(attribute.getName())) {
+			_output = expected.contains(input.getClass2().getName());
+		} else {
+			logger.warn(marker, format("attribute '%s' not supported", attribute.getName()));
+			_output = true;
+		}
+		output = _output;
 	}
 
 	@Override
 	public void visit(final IsNull predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final LessThan predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final Like predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final Not predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final Or predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
 	}
 
 	@Override
 	public void visit(final StartsWith predicate) {
-		logger.warn(format("predicate '%s' not supported", predicate));
+		logger.warn(marker, format("predicate '%s' not supported", predicate));
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this, SHORT_PREFIX_STYLE) //
+				.append("attribute", attribute.getName()) //
+				.append("predicate", attribute.getPredicate()) //
+				.build();
 	}
 
 }
