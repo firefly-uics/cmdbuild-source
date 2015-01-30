3b563ea29a33354919bfdafbfab6f9ef03d64a1a
Davide Pavan <d.pavan@tecnoteca.com>
2015-01-30 09:26:49 +0100

    #1040: RESTful API implementation - Relations management
    Fix for relation detail.

--- core/src/main/java/org/cmdbuild/logic/commands/GetRelationSingle.java
+++ core/src/main/java/org/cmdbuild/logic/commands/GetRelationSingle.java
@@ -33,8 +33,8 @@ public class GetRelationSingle extends AbstractGetRelation {
 
 	private RelationInfo relationInfo(final CMQueryRow row) {
 		final QueryRelation rel = row.getRelation(DOM_ALIAS);
-		final CMCard dst = row.getCard(SRC_ALIAS);
-		final CMCard src = row.getCard(DST_ALIAS);
+		final CMCard src = row.getCard(SRC_ALIAS);
+		final CMCard dst = row.getCard(DST_ALIAS);
 		return new RelationInfo(rel, src, dst);
 	}
 
