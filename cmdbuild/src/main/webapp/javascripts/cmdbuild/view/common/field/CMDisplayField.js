db480a2ba5f16afccc059c62572480a8af6b3a75
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-03-02 12:39:18 +0100

    #1525: [Fix] Multiple blank rows added on HTML editor fields

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/field/CMDisplayField.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/field/CMDisplayField.js
@@ -1,6 +1,32 @@
 (function() {
-
 	var MAX_HEIGHT = 100;
+
+	Ext.form.field.Display.override({
+		setValue : function(value) {
+			// for the attributes like lookup and reference
+			// that has as value an object like {id:"", description:""}
+			if (value != null
+					&& typeof value == "object") {
+
+				value = value.description;
+			}
+
+			this.callOverridden([value]);
+			this._addTargetToLinks();
+		},
+
+		_addTargetToLinks: function() {
+			var ct = this.getContentTarget();
+			if (ct) {
+				var links = Ext.DomQuery.select("a", ct.dom);
+				if (links) {
+					for (var i=0, l=links.length; i<l; ++i) {
+						links[i].target = "_blank";
+					}
+				}
+			}
+		}
+	});
 
 	Ext.define("CMDBuild.view.common.field.CMDisplayField", {
 		extend : "Ext.form.field.Display",
@@ -78,5 +104,4 @@
 			displayField.setValue(field.getValue());
 		});
 	}
-
 })();
