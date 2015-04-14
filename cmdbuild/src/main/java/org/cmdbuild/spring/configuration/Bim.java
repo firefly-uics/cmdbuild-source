1b6bc7a58538e9eac6a939c75aa1f23d0c2cc582
Davide Pavan <d.pavan@tecnoteca.com>
2015-04-14 15:06:22 +0200

    #1568: E-mail management review (delay, tab, widget, task, template)
    Fix to Spring configuration.

--- cmdbuild/src/main/java/org/cmdbuild/spring/configuration/Bim.java
+++ cmdbuild/src/main/java/org/cmdbuild/spring/configuration/Bim.java
@@ -24,7 +24,6 @@ import org.cmdbuild.logic.bim.ViewerLogic;
 import org.cmdbuild.logic.bim.project.DefaultProjectLogic;
 import org.cmdbuild.logic.bim.project.ProjectLogic;
 import org.cmdbuild.logic.data.DataDefinitionLogic;
-import org.cmdbuild.logic.data.access.SystemDataAccessLogicBuilder;
 import org.cmdbuild.logic.data.lookup.LookupLogic;
 import org.cmdbuild.model.bim.StorableLayer;
 import org.cmdbuild.model.bim.StorableProject;
