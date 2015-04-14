0ee0f0c97ccaaab07711f615a5d923a6c787f92e
Davide Pavan <d.pavan@tecnoteca.com>
2015-04-14 11:43:48 +0200

    #1568: E-mail management review (delay, tab, widget, task, template) - E-mail sending using a queue
    Queue management from JSON-RPC methods.

--- core/src/main/java/org/cmdbuild/logic/email/EmailQueueLogic.java
+++ core/src/main/java/org/cmdbuild/logic/email/EmailQueueLogic.java
@@ -1,9 +1,8 @@
 package org.cmdbuild.logic.email;
 
 import org.cmdbuild.logic.Logic;
-import org.cmdbuild.scheduler.command.Command;
 
-public interface EmailQueueLogic extends Command, Logic {
+public interface EmailQueueLogic extends Logic {
 
 	interface Configuration {
 
