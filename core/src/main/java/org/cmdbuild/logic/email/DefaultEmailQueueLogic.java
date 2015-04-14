cc8e7b801cf3eafea56497775a3d8486cfeb4018
Davide Pavan <d.pavan@tecnoteca.com>
2015-04-14 16:08:33 +0200

    #1568: E-mail management review (delay, tab, widget, task, template) - E-mail sending using a queue
    Configuration management.

--- core/src/main/java/org/cmdbuild/logic/email/DefaultEmailQueueLogic.java
+++ core/src/main/java/org/cmdbuild/logic/email/DefaultEmailQueueLogic.java
@@ -1,6 +1,10 @@
 package org.cmdbuild.logic.email;
 
 import static org.cmdbuild.scheduler.Triggers.everyMinute;
+import static org.cmdbuild.scheduler.command.Commands.safe;
+import static org.joda.time.DateTime.now;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.commons.lang3.Validate;
 import org.cmdbuild.config.EmailConfiguration;
@@ -8,8 +12,39 @@ import org.cmdbuild.scheduler.Job;
 import org.cmdbuild.scheduler.SchedulerService;
 import org.cmdbuild.scheduler.command.BuildableCommandBasedJob;
 import org.cmdbuild.scheduler.command.Command;
+import org.joda.time.DateTime;
 
 public class DefaultEmailQueueLogic implements EmailQueueLogic {
+
+	private static class AdvancedCommand implements Command {
+
+		private final Command delegate;
+		private final EmailConfiguration configuration;
+
+		private final AtomicBoolean running = new AtomicBoolean(false);
+		private DateTime lastExecution;
+
+		public AdvancedCommand(final Command delegate, EmailConfiguration configuration) {
+			this.delegate = delegate;
+			this.configuration = configuration;
+		}
+
+		@Override
+		public void execute() {
+			if (running.getAndSet(true)) {
+				if ((lastExecution == null) || now().isAfter(lastExecution.plus(configuration.getQueueTime()))) {
+					safe(delegate).execute();
+					lastExecution = now();
+				} else {
+					logger.debug("time not elapsed");
+				}
+				running.set(false);
+			} else {
+				logger.debug("queue already running");
+			}
+		}
+
+	}
 
 	private final EmailConfiguration configuration;
 	private final SchedulerService schedulerService;
@@ -21,7 +56,7 @@ public class DefaultEmailQueueLogic implements EmailQueueLogic {
 		this.schedulerService = schedulerService;
 		this.job = BuildableCommandBasedJob.newInstance() //
 				.withName(DefaultEmailQueueLogic.class.getName()) //
-				.withCommand(command) //
+				.withCommand(new AdvancedCommand(command, configuration)) //
 				.build();
 	}
 
@@ -32,7 +67,6 @@ public class DefaultEmailQueueLogic implements EmailQueueLogic {
 
 	@Override
 	public void start() {
-		// TODO add time check
 		schedulerService.add(job, everyMinute());
 	}
 
