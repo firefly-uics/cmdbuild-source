a5d225e16b21633a0b126bcd85e9788ece4c1147
Davide Pavan <d.pavan@tecnoteca.com>
2015-05-04 12:11:53 +0200

    #1606: [Fix] Error when sending notification e-mail from tasks

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/report/Parameters.js
@@ -0,0 +1,109 @@
+(function () {
+
+	Ext.define('CMDBuild.controller.management.report.Parameters', {
+		extend: 'CMDBuild.controller.common.AbstractController',
+
+		requires: [
+			'CMDBuild.core.proxy.CMProxyConstants',
+			'CMDBuild.core.proxy.CMProxyUrlIndex',
+			'CMDBuild.core.proxy.Report'
+		],
+
+		/**
+		 * @cfg {Mixed}
+		 */
+		parentDelegate: undefined,
+
+		/**
+		 * @cfg {Array}
+		 */
+		attributeList: undefined,
+
+		/**
+		 * @cfg {Boolean}
+		 */
+		forceDownload: undefined,
+
+		/**
+		 * @cfg {Array}
+		 */
+		cmfgCatchedFunctions: [
+			'onParametersAbortButtonClick',
+			'onParametersSaveButtonClick'
+		],
+
+		/**
+		 * @property {CMDBuild.view.management.report.ParametersWindow} emailWindows
+		 */
+		view: undefined,
+
+		/**
+		 * @param {Object} configurationObject
+		 * @param {Mixed} configurationObject.parentDelegate
+		 */
+		constructor: function(configurationObject) {
+			this.callParent(arguments);
+
+			this.view = Ext.create('CMDBuild.view.management.report.ParametersWindow', {
+				delegate: this
+			});
+
+			// Show window
+			if (!Ext.isEmpty(this.view)) {
+				this.view.show();
+
+				this.buildFields();
+			}
+		},
+
+		buildFields: function() {
+			if (this.attributeList.length > 0)
+				Ext.Array.forEach(this.attributeList, function(attribute, index, allAttributes) {
+					var field = CMDBuild.Management.FieldManager.getFieldForAttr(attribute, false, false);
+
+					if (!Ext.isEmpty(field)) {
+						if (attribute.defaultvalue)
+							field.setValue(attribute.defaultvalue);
+
+						this.view.form.add(field);
+					}
+				}, this);
+		},
+
+		onParametersAbortButtonClick: function() {
+			this.view.destroy();
+		},
+
+		onParametersSaveButtonClick: function() {
+			if (this.view.form.getForm().isValid()) {
+				CMDBuild.core.proxy.Report.updateReport({
+					form: this.view.form.getForm(),
+					scope: this,
+					failure: function(response, options, decodedResponse) {
+						this.onParametersAbortButtonClick();
+					},
+					success: function(response, options) {
+						if (Ext.isEmpty(this.parentDelegate)) {
+							var popup = window.open(
+								CMDBuild.core.proxy.CMProxyUrlIndex.reports.printReportFactory,
+								'Report',
+								'height=400,width=550,status=no,toolbar=no,scrollbars=yes,menubar=no,location=no,resizable'
+							);
+
+							if (Ext.isEmpty(popup))
+								CMDBuild.Msg.warn(
+									CMDBuild.Translation.warnings.warning_message,
+									CMDBuild.Translation.warnings.popup_block
+								);
+						} else if (!Ext.isEmpty(this.parentDelegate) && Ext.isFunction(this.parentDelegate.showReport)) {
+							this.parentDelegate.showReport(this.forceDownload);
+						}
+
+						this.onParametersAbortButtonClick();
+					}
+				});
+			}
+		}
+	});
+
+})();
---
+++ core/src/main/java/org/cmdbuild/logic/email/SendTemplateEmail.java
@@ -0,0 +1,170 @@
+package org.cmdbuild.logic.email;
+
+import static com.google.common.base.Splitter.on;
+import static java.util.Collections.emptyList;
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+import static org.apache.commons.lang3.StringUtils.defaultString;
+import static org.cmdbuild.common.template.TemplateResolvers.identity;
+import static org.cmdbuild.data.store.email.EmailConstants.ADDRESSES_SEPARATOR;
+import static org.joda.time.DateTime.now;
+
+import org.apache.commons.lang3.Validate;
+import org.cmdbuild.common.template.TemplateResolver;
+import org.cmdbuild.data.store.email.EmailAccount;
+import org.cmdbuild.logic.Action;
+import org.cmdbuild.logic.email.EmailTemplateLogic.Template;
+import org.cmdbuild.services.email.Attachment;
+import org.cmdbuild.services.email.Email;
+import org.cmdbuild.services.email.EmailService;
+import org.cmdbuild.services.email.EmailServiceFactory;
+import org.joda.time.DateTime;
+
+import com.google.common.base.Supplier;
+
+public class SendTemplateEmail implements Action {
+
+	public static class Builder implements org.apache.commons.lang3.builder.Builder<SendTemplateEmail> {
+
+		private Supplier<EmailAccount> emailAccoutSupplier;
+		private EmailServiceFactory emailServiceFactory;
+		private Supplier<Template> emailTemplateSupplier;
+		private TemplateResolver templateResolver;
+
+		private Builder() {
+			// use factory method
+		}
+
+		@Override
+		public SendTemplateEmail build() {
+			validate();
+			return new SendTemplateEmail(this);
+		}
+
+		private void validate() {
+			Validate.notNull(emailAccoutSupplier, "missing '%s' supplier", EmailAccount.class);
+			Validate.notNull(emailServiceFactory, "missing '%s'", EmailServiceFactory.class);
+			Validate.notNull(emailTemplateSupplier, "missing '%s' supplier", Template.class);
+
+			templateResolver = defaultIfNull(templateResolver, identity());
+		}
+
+		public Builder withEmailAccountSupplier(final Supplier<EmailAccount> emailAccoutSupplier) {
+			this.emailAccoutSupplier = emailAccoutSupplier;
+			return this;
+		}
+
+		public Builder withEmailServiceFactory(final EmailServiceFactory emailServiceFactory) {
+			this.emailServiceFactory = emailServiceFactory;
+			return this;
+		}
+
+		public Builder withEmailTemplateSupplier(final Supplier<Template> emailTemplateSupplier) {
+			this.emailTemplateSupplier = emailTemplateSupplier;
+			return this;
+		}
+
+		public Builder withTemplateResolver(final TemplateResolver templateResolver) {
+			this.templateResolver = templateResolver;
+			return this;
+		}
+
+	}
+
+	public static Builder newInstance() {
+		return new Builder();
+	}
+
+	private static class TemplateAdapter implements Email {
+
+		private static final Iterable<String> NO_ADDRESSES = emptyList();
+		private static final Iterable<Attachment> NO_ATTACHMENTS = emptyList();
+
+		private final DateTime OBJECT_CREATION_TIME = now();
+
+		private final Template delegate;
+		private final TemplateResolver templateResolver;
+
+		public TemplateAdapter(final Template delegate, final TemplateResolver templateResolver) {
+			this.delegate = delegate;
+			this.templateResolver = templateResolver;
+		}
+
+		@Override
+		public DateTime getDate() {
+			return OBJECT_CREATION_TIME;
+		}
+
+		@Override
+		public String getFromAddress() {
+			return templateResolver.resolve(delegate.getFrom());
+		}
+
+		@Override
+		public Iterable<String> getToAddresses() {
+			return addresses(delegate.getTo());
+		}
+
+		@Override
+		public Iterable<String> getCcAddresses() {
+			return addresses(delegate.getCc());
+		}
+
+		@Override
+		public Iterable<String> getBccAddresses() {
+			return addresses(delegate.getBcc());
+		}
+
+		private Iterable<String> addresses(final String value) {
+			return on(ADDRESSES_SEPARATOR) //
+					.omitEmptyStrings() //
+					.trimResults() //
+					.split(defaultString(templateResolver.resolve(value)));
+		}
+
+		@Override
+		public String getSubject() {
+			return templateResolver.resolve(delegate.getSubject());
+		}
+
+		@Override
+		public String getContent() {
+			return templateResolver.resolve(delegate.getBody());
+		}
+
+		@Override
+		public Iterable<Attachment> getAttachments() {
+			return NO_ATTACHMENTS;
+		}
+
+		@Override
+		public String getAccount() {
+			return delegate.getAccount();
+		}
+
+		@Override
+		public long getDelay() {
+			return 0;
+		}
+
+	}
+
+	private final Supplier<EmailAccount> emailAccoutSupplier;
+	private final EmailServiceFactory emailServiceFactory;
+	private final Supplier<Template> emailTemplateSupplier;
+	private final TemplateResolver templateResolver;
+
+	private SendTemplateEmail(final Builder builder) {
+		this.emailAccoutSupplier = builder.emailAccoutSupplier;
+		this.emailServiceFactory = builder.emailServiceFactory;
+		this.emailTemplateSupplier = builder.emailTemplateSupplier;
+		this.templateResolver = builder.templateResolver;
+	}
+
+	@Override
+	public void execute() {
+		final EmailService emailService = emailServiceFactory.create(emailAccoutSupplier);
+		final Template template = emailTemplateSupplier.get();
+		emailService.send(new TemplateAdapter(template, templateResolver));
+	}
+
+}
