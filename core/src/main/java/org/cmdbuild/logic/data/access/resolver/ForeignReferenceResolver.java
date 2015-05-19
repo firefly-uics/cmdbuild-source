ef1e7d989dc7fa4bdfec03693b8e550c8a6ea925
Davide Pavan <d.pavan@tecnoteca.com>
2015-05-19 09:42:13 +0200

    #1631: REST - Getting the position of cards or process's instances
    Implementation for cards.

---
+++ core/src/main/java/org/cmdbuild/logic/data/access/resolver/ForeignReferenceResolver.java
@@ -0,0 +1,123 @@
+package org.cmdbuild.logic.data.access.resolver;
+
+import static com.google.common.collect.FluentIterable.from;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.Builder;
+import org.cmdbuild.dao.entry.CMEntry;
+import org.cmdbuild.dao.entrytype.CMAttribute;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+
+public class ForeignReferenceResolver<T extends CMEntry> {
+
+	public static abstract class EntryFiller<T extends CMEntry> {
+
+		protected Map<String, Object> values = Maps.newHashMap();
+		protected T input;
+
+		public void setInput(final T input) {
+			this.input = input;
+			this.values = Maps.newHashMap();
+		}
+
+		public void setValue(final String name, final Object value) {
+			values.put(name, value);
+		}
+
+		public abstract T getOutput();
+
+	}
+
+	public static class ForeignReferenceResolverBuilder<T extends CMEntry> implements
+			Builder<ForeignReferenceResolver<T>> {
+
+		private Iterable<T> entries;
+		public EntryFiller<T> entryFiller;
+		public AbstractSerializer<T> serializer;
+
+		@Override
+		public ForeignReferenceResolver<T> build() {
+			return new ForeignReferenceResolver<T>(this);
+		}
+
+		public ForeignReferenceResolverBuilder<T> withEntries(final Iterable<T> value) {
+			entries = value;
+			return this;
+		}
+
+		public ForeignReferenceResolverBuilder<T> withEntryFiller(final EntryFiller<T> value) {
+			entryFiller = value;
+			return this;
+		}
+
+		public ForeignReferenceResolverBuilder<T> withSerializer(final AbstractSerializer<T> value) {
+			serializer = value;
+			return this;
+		}
+
+	}
+
+	public static <T extends CMEntry> ForeignReferenceResolverBuilder<T> newInstance() {
+		return new ForeignReferenceResolverBuilder<T>();
+	}
+
+	private final Iterable<T> entries;
+	private final EntryFiller<T> entryFiller;
+	private final AbstractSerializer<T> serializer;
+
+	public ForeignReferenceResolver(final ForeignReferenceResolverBuilder<T> builder) {
+		this.entries = builder.entries;
+		this.entryFiller = builder.entryFiller;
+		this.serializer = builder.serializer;
+	}
+
+	public Iterable<T> resolve() {
+
+		return from(entries) //
+				.transform(new Function<T, T>() {
+
+					@Override
+					public T apply(final T input) {
+
+						entryFiller.setInput(input);
+
+						for (final CMAttribute attribute : input.getType().getAllAttributes()) {
+							final String attributeName = attribute.getName();
+
+							final Object rawValue;
+							try {
+								rawValue = input.get(attributeName);
+							} catch (final Throwable e) {
+								/*
+								 * This could happen for ImportCSV because the
+								 * fake card has no the whole attributes of the
+								 * relative CMClass
+								 */
+								continue;
+							}
+
+							/**
+							 * must be kept in the same order. If not, an
+							 * attribute with null value will not be returned
+							 */
+							entryFiller.setValue(attributeName, rawValue);
+							if (rawValue == null) {
+								continue;
+							}
+
+							serializer.setRawValue(rawValue);
+							serializer.setAttributeName(attributeName);
+							serializer.setEntryFiller(entryFiller);
+							attribute.getType().accept(serializer);
+
+						}
+						return entryFiller.getOutput();
+					}
+
+				});
+	}
+
+}
---
+++ dao/core/src/main/java/org/cmdbuild/dao/entry/ForwardingValueSet.java
@@ -0,0 +1,33 @@
+package org.cmdbuild.dao.entry;
+
+import java.util.Map.Entry;
+
+import com.google.common.collect.ForwardingObject;
+
+public abstract class ForwardingValueSet extends ForwardingObject implements CMValueSet {
+
+	/**
+	 * Usable by subclasses only.
+	 */
+	protected ForwardingValueSet() {
+	}
+
+	@Override
+	protected abstract CMValueSet delegate();
+
+	@Override
+	public Object get(final String key) {
+		return delegate().get(key);
+	}
+
+	@Override
+	public <T> T get(final String key, final Class<? extends T> requiredType) {
+		return delegate().get(key, requiredType);
+	}
+
+	@Override
+	public Iterable<Entry<String, Object>> getValues() {
+		return delegate().getValues();
+	}
+
+}
