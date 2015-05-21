ace8a1d386e2af97a6236728d86362192609dc61
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-05-21 12:33:43 +0200

    #1639: Email attachments download button implementation
    Server side

--- commons/src/main/java/org/cmdbuild/common/utils/Reflection.java
+++ commons/src/main/java/org/cmdbuild/common/utils/Reflection.java
@@ -1,15 +1,20 @@
 package org.cmdbuild.common.utils;
 
+import static java.lang.String.format;
 import static org.apache.commons.lang3.StringUtils.defaultString;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+import org.cmdbuild.common.logging.LoggingSupport;
+
 import com.google.common.reflect.AbstractInvocationHandler;
 
-public class Reflection {
+public class Reflection implements LoggingSupport {
 
 	private static class UnsupportedInvocationHandler extends AbstractInvocationHandler {
+
+		private static final String UNSUPPORTED = "unsupported";
 
 		private final String message;
 
@@ -20,9 +25,8 @@ public class Reflection {
 		@Override
 		protected Object handleInvocation(final Object proxy, final Method method, final Object[] args)
 				throws Throwable {
-			throw new UnsupportedOperationException(defaultString(message));
+			throw new UnsupportedOperationException(format("%s (%s)", defaultString(message, UNSUPPORTED), method));
 		}
-
 	}
 
 	public static InvocationHandler unsupported(final String message) {
