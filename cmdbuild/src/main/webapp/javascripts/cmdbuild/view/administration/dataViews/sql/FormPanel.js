24a755ff3d64b34c78a7a3260cd2e49a7736e03e
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-06-04 08:56:15 +0200

    #1664: View code improvements - From SQL administration interface

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/administration/dataViews/sql/FormPanel.js
@@ -0,0 +1,142 @@
+(function() {
+
+	Ext.define('CMDBuild.view.administration.dataViews.sql.FormPanel', {
+		extend: 'Ext.form.Panel',
+
+		requires: ['CMDBuild.core.proxy.CMProxyConstants'],
+
+		mixins: {
+			panelFunctions: 'CMDBuild.view.common.PanelFunctions'
+		},
+
+		/**
+		 * @cfg {CMDBuild.controller.administration.dataViews.Sql}
+		 */
+		delegate: undefined,
+
+		/**
+		 * @property {CMDBuild.view.common.field.translatable.Text}
+		 */
+		descriptionTextField: undefined,
+
+		/**
+		 * @property {Ext.form.field.ComboBox}
+		 */
+		sourceFunctionCombobox: undefined,
+
+		bodyCls: 'cmgraypanel',
+		border: false,
+		cls: 'x-panel-body-default-framed cmbordertop',
+		frame: false,
+		overflowY: 'auto',
+		split: true,
+
+		layout: {
+			type: 'vbox',
+			align:'stretch'
+		},
+
+		initComponent: function() {
+
+			Ext.apply(this, {
+				dockedItems: [
+					Ext.create('Ext.toolbar.Toolbar', {
+						dock: 'top',
+						itemId: CMDBuild.core.proxy.CMProxyConstants.TOOLBAR_TOP,
+
+						items: [
+							Ext.create('CMDBuild.core.buttons.Modify', {
+								text: CMDBuild.Translation.modifyView,
+								scope: this,
+
+								handler: function(button, e) {
+									this.delegate.cmfg('onDataViewSqlModifyButtonClick');
+								}
+							}),
+							Ext.create('CMDBuild.core.buttons.Delete', {
+								text: CMDBuild.Translation.removeView,
+								scope: this,
+
+								handler: function(button, e) {
+									this.delegate.cmfg('onDataViewSqlRemoveButtonClick');
+								}
+							})
+						]
+					}),
+					Ext.create('Ext.toolbar.Toolbar', {
+						dock: 'bottom',
+						itemId: CMDBuild.core.proxy.CMProxyConstants.TOOLBAR_BOTTOM,
+						ui: 'footer',
+
+						layout: {
+							type: 'hbox',
+							align: 'middle',
+							pack: 'center'
+						},
+
+						items: [
+							Ext.create('CMDBuild.core.buttons.Save', {
+								scope: this,
+
+								handler: function(button, e) {
+									this.delegate.cmfg('onDataViewSqlSaveButtonClick');
+								}
+							}),
+							Ext.create('CMDBuild.core.buttons.Abort', {
+								scope: this,
+
+								handler: function(button, e) {
+									this.delegate.cmfg('onDataViewSqlAbortButtonClick');
+								}
+							})
+						]
+					})
+				],
+				items: [
+					Ext.create('Ext.form.field.Text', {
+						name: CMDBuild.core.proxy.CMProxyConstants.NAME,
+						itemId: CMDBuild.core.proxy.CMProxyConstants.NAME,
+						fieldLabel: CMDBuild.Translation.name,
+						labelWidth: CMDBuild.LABEL_WIDTH,
+						maxWidth: CMDBuild.ADM_BIG_FIELD_WIDTH,
+						allowBlank: false,
+						cmImmutable: true
+					}),
+					this.descriptionTextField = Ext.create('CMDBuild.view.common.field.translatable.Text', {
+						name: _CMProxy.parameter.DESCRIPTION,
+						fieldLabel: CMDBuild.Translation.descriptionLabel,
+						labelWidth: CMDBuild.LABEL_WIDTH,
+						maxWidth: CMDBuild.ADM_BIG_FIELD_WIDTH,
+						allowBlank: false,
+						vtype: 'cmdbcomment',
+						translationsKeyType: 'View',
+						translationsKeyField: 'Description'
+					}),
+					this.sourceFunctionCombobox = Ext.create('Ext.form.field.ComboBox', {
+						name: CMDBuild.core.proxy.CMProxyConstants.SOURCE_FUNCTION,
+						fieldLabel: CMDBuild.Translation.dataSource,
+						labelWidth: CMDBuild.LABEL_WIDTH,
+						maxWidth: CMDBuild.ADM_BIG_FIELD_WIDTH,
+						valueField: CMDBuild.core.proxy.CMProxyConstants.NAME,
+						displayField: CMDBuild.core.proxy.CMProxyConstants.NAME,
+						forceSelection: true,
+						editable: false,
+						allowBlank: false,
+
+						store: _CMCache.getAvailableDataSourcesStore(),
+						queryMode: 'local',
+					}),
+					{
+						xtype: 'hiddenfield',
+						name: CMDBuild.core.proxy.CMProxyConstants.ID
+					}
+				]
+			});
+
+			this.callParent(arguments);
+
+			this.setDisabledModify(true, true, true);
+		}
+	});
+
+})();
