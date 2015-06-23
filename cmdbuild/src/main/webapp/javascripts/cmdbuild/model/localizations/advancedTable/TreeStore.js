1736ce1d0f4766bc540d5d38dbfb51f496863ecb
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-06-23 15:21:21 +0200

    #1447: Localization table - Translatable fields

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/model/localizations/advancedTable/TreeStore.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/model/localizations/advancedTable/TreeStore.js
@@ -7,11 +7,13 @@
 
 		fields: [
 			{ name: CMDBuild.core.proxy.Constants.DEFAULT, type: 'string'}, // Default translation
-			{ name: CMDBuild.core.proxy.Constants.ENTITY_IDENTIFIER, type: 'string' }, // Node's entity identifier to avoid to be lock to displayed label value
+			{ name: CMDBuild.core.proxy.Constants.FIELD, type: 'string' }, // Field to translate (description, inverseDescription, ...)
+			{ name: CMDBuild.core.proxy.Constants.IDENTIFIER, type: 'string' }, // Entity's attribute/property identifier
+			{ name: CMDBuild.core.proxy.Constants.OWNER, type: 'string' }, // Translation owner identifier (className, domainName, ...) used only to translate attribute's entities
 			{ name: CMDBuild.core.proxy.Constants.PARENT, type: 'auto' }, // Parent node
-			{ name: CMDBuild.core.proxy.Constants.PROPERTY_IDENTIFIER, type: 'string' },
-			{ name: CMDBuild.core.proxy.Constants.TEXT, type: 'string'}, // Label to display in grid's tree column
-		],
+			{ name: CMDBuild.core.proxy.Constants.TEXT, type: 'string'}, // Label to display in grid's tree column (usually name attribute)
+			{ name: CMDBuild.core.proxy.Constants.TYPE, type: 'string'} // Entity type identifier (class, attributeclass, domain, attributedomain, filter, instancename, lookupvalue, menuitem, report, view, classwidget)
+			],
 
 		/**
 		 * Complete fields properties with all configured languages
@@ -27,6 +29,20 @@
 			CMDBuild.model.localizations.advancedTable.TreeStore.setFields(modelFields);
 
 			this.callParent(arguments);
+		},
+
+		/**
+		 * @returns {Object} translationsObject
+		 */
+		getTranslations: function() {
+			var translationsObject = {};
+			var enabledLanguages = CMDBuild.configuration[CMDBuild.core.proxy.Constants.LOCALIZATION].getEnabledLanguages();
+
+			Ext.Object.each(enabledLanguages, function(key, value, myself) {
+				translationsObject[key] = this.get(key);
+			}, this);
+
+			return translationsObject;
 		}
 	});
 
