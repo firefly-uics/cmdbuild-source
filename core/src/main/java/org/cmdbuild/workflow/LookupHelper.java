b1e9eec7de4af765567e54d6cceb16d38405b4d4
Davide Pavan <d.pavan@tecnoteca.com>
2015-07-16 10:32:58 +0200

    #1719: [Fix] Synchronous events cannot get process values at the "after creation" step
    Spring issues.

--- core/src/main/java/org/cmdbuild/workflow/LookupHelper.java
+++ core/src/main/java/org/cmdbuild/workflow/LookupHelper.java
@@ -1,12 +1,11 @@
 package org.cmdbuild.workflow;
 
 import org.cmdbuild.data.store.lookup.Lookup;
-import org.cmdbuild.data.store.lookup.LookupStore;
 import org.cmdbuild.workflow.service.WSProcessInstanceState;
 
 import com.google.common.base.Optional;
 
-public interface LookupHelper extends LookupStore {
+public interface LookupHelper {
 
 	WSProcessInstanceState stateForLookupCode(String code);
 
