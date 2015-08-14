c70c28dfdeb2ec64b3ad106f77c6ce569fb4ea54
Davide Pavan <d.pavan@tecnoteca.com>
2015-08-14 09:19:28 +0200

    #1721: Custom Form widget - Presets

--- core/src/main/java/org/cmdbuild/model/widget/customform/FunctionDataBuilder.java
+++ core/src/main/java/org/cmdbuild/model/widget/customform/FunctionDataBuilder.java
@@ -2,22 +2,34 @@ package org.cmdbuild.model.widget.customform;
 
 import static com.google.common.collect.FluentIterable.from;
 import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.reflect.Reflection.newProxy;
+import static org.cmdbuild.common.Constants.DATETIME_PRINTING_PATTERN;
+import static org.cmdbuild.common.Constants.DATE_PRINTING_PATTERN;
+import static org.cmdbuild.common.Constants.TIME_PRINTING_PATTERN;
+import static org.cmdbuild.common.utils.Reflection.defaultValues;
 import static org.cmdbuild.dao.guava.Functions.toValueSet;
 import static org.cmdbuild.dao.query.clause.AnyAttribute.anyAttribute;
 import static org.cmdbuild.dao.query.clause.FunctionCall.call;
 import static org.cmdbuild.dao.query.clause.alias.Aliases.name;
+import static org.joda.time.format.DateTimeFormat.forPattern;
 
 import java.util.List;
 import java.util.Map;
 
 import org.cmdbuild.dao.entry.CMValueSet;
 import org.cmdbuild.dao.entrytype.attributetype.CMAttributeType;
+import org.cmdbuild.dao.entrytype.attributetype.CMAttributeTypeVisitor;
+import org.cmdbuild.dao.entrytype.attributetype.DateAttributeType;
+import org.cmdbuild.dao.entrytype.attributetype.DateTimeAttributeType;
+import org.cmdbuild.dao.entrytype.attributetype.ForwardingAttributeTypeVisitor;
+import org.cmdbuild.dao.entrytype.attributetype.TimeAttributeType;
 import org.cmdbuild.dao.function.CMFunction;
 import org.cmdbuild.dao.function.CMFunction.CMFunctionParameter;
 import org.cmdbuild.dao.query.CMQueryResult;
 import org.cmdbuild.dao.query.clause.alias.Alias;
 import org.cmdbuild.dao.view.CMDataView;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.joda.time.format.DateTimeFormatter;
 
 import com.google.common.base.Function;
 
@@ -70,8 +82,46 @@ public class FunctionDataBuilder implements DataBuilder {
 					}
 
 					private Object convert(final CMAttributeType<?> type, final Object value) {
-						// TODO convert
-						return value;
+						return (value == null) ? null : new ForwardingAttributeTypeVisitor() {
+
+							private final CMAttributeTypeVisitor DELEGATE = newProxy(CMAttributeTypeVisitor.class,
+									defaultValues());
+
+							public final DateTimeFormatter DATE_FORMATTER = forPattern(DATE_PRINTING_PATTERN);
+							public final DateTimeFormatter DATE_TIME_FORMATTER = forPattern(DATETIME_PRINTING_PATTERN);
+							public final DateTimeFormatter TIME_FORMATTER = forPattern(TIME_PRINTING_PATTERN);
+
+							private Object converted;
+
+							@Override
+							protected CMAttributeTypeVisitor delegate() {
+								return DELEGATE;
+							}
+
+							public Object convert() {
+								converted = value;
+								if (converted != null) {
+									type.accept(this);
+								}
+								return converted;
+							}
+
+							@Override
+							public void visit(final DateAttributeType attributeType) {
+								converted = DATE_FORMATTER.print(attributeType.convertValue(value));
+							}
+
+							@Override
+							public void visit(final DateTimeAttributeType attributeType) {
+								converted = DATE_TIME_FORMATTER.print(attributeType.convertValue(value));
+							}
+
+							@Override
+							public void visit(final TimeAttributeType attributeType) {
+								converted = TIME_FORMATTER.print(attributeType.convertValue(value));
+							}
+
+						}.convert();
 					}
 
 				}) //
