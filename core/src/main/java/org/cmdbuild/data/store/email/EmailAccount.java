b5e00f30dcc559c3eea27a10eef044a6691b3296
Davide Pavan <d.pavan@tecnoteca.com>
2015-08-17 17:23:45 +0200

    #1756: Filters for report and dashboard parameters - JasperReport property management for report parameters
    First commit.

---
+++ core/src/main/java/org/cmdbuild/data/store/email/EmailAccount.java
@@ -0,0 +1,37 @@
+package org.cmdbuild.data.store.email;
+
+import org.cmdbuild.data.store.Storable;
+
+public interface EmailAccount extends Storable {
+
+	Long getId();
+
+	String getName();
+
+	boolean isDefault();
+
+	String getUsername();
+
+	String getPassword();
+
+	String getAddress();
+
+	String getSmtpServer();
+
+	Integer getSmtpPort();
+
+	boolean isSmtpSsl();
+
+	boolean isSmtpConfigured();
+
+	String getOutputFolder();
+
+	String getImapServer();
+
+	Integer getImapPort();
+
+	boolean isImapSsl();
+
+	boolean isImapConfigured();
+
+}
---
+++ core/src/main/java/org/cmdbuild/report/CustomProperties.java
@@ -0,0 +1,15 @@
+package org.cmdbuild.report;
+
+public class CustomProperties {
+
+	public static final String //
+			FILTER = "filter", //
+			REQUIRED = "required";
+
+	/**
+	 * Prevents instantiation.
+	 */
+	private CustomProperties() {
+	}
+
+}
