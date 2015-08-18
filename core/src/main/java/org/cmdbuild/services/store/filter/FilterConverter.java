1f4e02f48bbc2961ca918fd881b3fbf7b0c81069
Davide Pavan <d.pavan@tecnoteca.com>
2015-08-18 13:03:25 +0200

    #1733: Default filter for a given group - New filters logic

--- core/src/main/java/org/cmdbuild/services/store/filter/FilterConverter.java
+++ core/src/main/java/org/cmdbuild/services/store/filter/FilterConverter.java
@@ -1,11 +1,9 @@
 package org.cmdbuild.services.store.filter;
 
-import static org.cmdbuild.services.store.filter.DataViewFilterStore.DESCRIPTION_ATTRIBUTE_NAME;
-import static org.cmdbuild.services.store.filter.DataViewFilterStore.ENTRYTYPE_ATTRIBUTE_NAME;
-import static org.cmdbuild.services.store.filter.DataViewFilterStore.FILTER_ATTRIBUTE_NAME;
-import static org.cmdbuild.services.store.filter.DataViewFilterStore.MASTER_ATTRIBUTE_NAME;
-import static org.cmdbuild.services.store.filter.DataViewFilterStore.NAME_ATTRIBUTE_NAME;
-import static org.cmdbuild.services.store.filter.DataViewFilterStore.TEMPLATE_ATTRIBUTE_NAME;
+import static com.google.common.collect.Maps.newHashMap;
+import static org.cmdbuild.common.Constants.CODE_ATTRIBUTE;
+import static org.cmdbuild.common.Constants.DESCRIPTION_ATTRIBUTE;
+import static org.cmdbuild.common.Constants.ID_ATTRIBUTE;
 
 import java.util.Map;
 
@@ -17,6 +15,16 @@ import org.cmdbuild.services.store.filter.FilterStore.Filter;
 
 public class FilterConverter extends BaseStorableConverter<Filter> {
 
+	public static final String CLASS_NAME = "_Filter";
+
+	public static final String ID = ID_ATTRIBUTE;
+	public static final String NAME = CODE_ATTRIBUTE;
+	public static final String DESCRIPTION = DESCRIPTION_ATTRIBUTE;
+	public static final String ENTRYTYPE = "IdSourceClass";
+	public static final String FILTER = "Filter";
+	public static final String FOR_GROUP = "Template";
+	public static final String OWNER = "IdOwner";
+
 	private final CMDataView dataView;
 
 	public FilterConverter(final CMDataView dataView) {
@@ -25,30 +33,34 @@ public class FilterConverter extends BaseStorableConverter<Filter> {
 
 	@Override
 	public String getClassName() {
-		return DataViewFilterStore.FILTERS_CLASS_NAME;
+		return CLASS_NAME;
 	}
 
 	@Override
 	public Filter convert(final CMCard card) {
-
-		final Long etr = card.get(ENTRYTYPE_ATTRIBUTE_NAME, Long.class);
-		final CMClass clazz = dataView.findClass(etr);
-
+		final Long classId = card.get(ENTRYTYPE, Long.class);
+		final CMClass clazz = dataView.findClass(classId);
 		return FilterDTO.newFilter() //
 				.withId(card.getId()) //
-				.withName(card.get(NAME_ATTRIBUTE_NAME, String.class)) //
-				.withDescription(card.get(DESCRIPTION_ATTRIBUTE_NAME, String.class)) //
+				.withName(card.get(NAME, String.class)) //
+				.withDescription(card.get(DESCRIPTION, String.class)) //
 				.forClass(clazz.getIdentifier().getLocalName()) //
-				.withValue(card.get(FILTER_ATTRIBUTE_NAME, String.class)) //
-				.asTemplate(card.get(TEMPLATE_ATTRIBUTE_NAME, Boolean.class)) //
-				.withOwner(Number.class.cast(card.get(MASTER_ATTRIBUTE_NAME, Integer.class)).longValue()) //
+				.withValue(card.get(FILTER, String.class)) //
+				.asTemplate(card.get(FOR_GROUP, Boolean.class)) //
+				.withOwner(Number.class.cast(card.get(OWNER, Integer.class)).longValue()) //
 				.build();
 	}
 
 	@Override
 	public Map<String, Object> getValues(final Filter storable) {
-		throw new UnsupportedOperationException(
-				"This converter must be used only for reading. Writing is not supported.");
+		final Map<String, Object> values = newHashMap();
+		values.put(NAME, storable.getName());
+		values.put(DESCRIPTION, storable.getDescription());
+		values.put(ENTRYTYPE, dataView.findClass(storable.getClassName()).getId());
+		values.put(FILTER, storable.getValue());
+		values.put(FOR_GROUP, storable.isTemplate());
+		values.put(OWNER, storable.getOwner());
+		return values;
 	}
 
 }
