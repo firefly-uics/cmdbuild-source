5fbdf5815921d2e7c79c8ab8bc1f2ed3e64bacba
Davide Pavan <d.pavan@tecnoteca.com>
2015-09-03 15:17:48 +0200

    #1678: Custom pages - Persistence
    First commit.

---
+++ core/src/main/java/org/cmdbuild/logic/custompages/CustomPagesLogic.java
@@ -0,0 +1,11 @@
+package org.cmdbuild.logic.custompages;
+
+import org.cmdbuild.logic.Logic;
+
+public interface CustomPagesLogic extends Logic {
+
+	Iterable<CustomPage> read();
+
+	Iterable<CustomPage> readForCurrentUser();
+
+}
