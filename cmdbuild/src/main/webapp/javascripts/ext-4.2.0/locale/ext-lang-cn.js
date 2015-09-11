66d4b753a3f1587d7a17792f5dac9ceaf09ae7ce
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-09-11 10:12:59 +0200

    Added Chinese translation

--- cmdbuild/src/main/webapp/javascripts/ext-4.2.0/locale/ext-lang-cn.js
+++ cmdbuild/src/main/webapp/javascripts/ext-4.2.0/locale/ext-lang-cn.js
@@ -1,346 +1,339 @@
-/*
-This file is part of Ext JS 4.2
+(function() {
 
-Copyright (c) 2011-2013 Sencha Inc
+	Ext.onReady(function() {
+		var cm = Ext.ClassManager;
+		var exists = Ext.Function.bind(cm.get, cm);
 
-Contact:  http://www.sencha.com/contact
+		if (Ext.Updater)
+			Ext.Updater.defaults.indicatorText = '<div class="loading-indicator">加载...</div>';
 
-GNU General Public License Usage
-This file may be used under the terms of the GNU General Public License version 3.0 as
-published by the Free Software Foundation and appearing in the file LICENSE included in the
-packaging of this file.
+		if (exists('Ext.data.Types'))
+			Ext.data.Types.stripRe = /[\$,%]/g;
 
-Please review the following information to ensure the GNU General Public License version 3.0
-requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+		if (Ext.Date) {
+			Ext.Date.monthNames = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'];
 
-If you are unsure which license is appropriate for your use, please contact the sales department
-at http://www.sencha.com/contact.
+			Ext.Date.getShortMonthName = function(month) {
+				return Ext.Date.monthNames[month].substring(0, 3);
+			};
 
-Build date: 2013-03-11 22:33:40 (aed16176e68b5e8aa1433452b12805c0ad913836)
-*/
-/**
- * List compiled by mystix on the extjs.com forums.
- * Thank you Mystix!
- *
- * English Translations
- * updated to 2.2 by Condor (8 Aug 2008)
- */
-Ext.onReady(function() {
-    var cm = Ext.ClassManager,
-        exists = Ext.Function.bind(cm.get, cm);
+			Ext.Date.monthNumbers = {
+				Jan: 0,
+				Feb: 1,
+				Mar: 2,
+				Apr: 3,
+				May: 4,
+				Jun: 5,
+				Jul: 6,
+				Aug: 7,
+				Sep: 8,
+				Oct: 9,
+				Nov: 10,
+				Dec: 11
+			};
 
-    if (Ext.Updater) {
-        Ext.Updater.defaults.indicatorText = '<div class="loading-indicator">Loading...</div>';
-    }
+			Ext.Date.getMonthNumber = function(name) {
+				return Ext.Date.monthNumbers[name.substring(0, 1).toUpperCase() + name.substring(1, 3).toLowerCase()];
+			};
 
-    if (exists('Ext.data.Types')) {
-        Ext.data.Types.stripRe = /[\$,%]/g;
-    }
+			Ext.Date.dayNames = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
 
-    if (Ext.Date) {
-        Ext.Date.monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+			Ext.Date.getShortDayName = function(day) {
+				return Ext.Date.dayNames[day].substring(0, 3);
+			};
 
-        Ext.Date.getShortMonthName = function(month) {
-            return Ext.Date.monthNames[month].substring(0, 3);
-        };
+			Ext.Date.parseCodes.S.s = "(?:st|nd|rd|th)";
+		}
 
-        Ext.Date.monthNumbers = {
-            Jan: 0,
-            Feb: 1,
-            Mar: 2,
-            Apr: 3,
-            May: 4,
-            Jun: 5,
-            Jul: 6,
-            Aug: 7,
-            Sep: 8,
-            Oct: 9,
-            Nov: 10,
-            Dec: 11
-        };
+		if (Ext.MessageBox)
+			Ext.MessageBox.buttonText = {
+				ok: 'OK',
+				cancel: '取消',
+				yes: '是',
+				no: '否'
+			};
 
-        Ext.Date.getMonthNumber = function(name) {
-            return Ext.Date.monthNumbers[name.substring(0, 1).toUpperCase() + name.substring(1, 3).toLowerCase()];
-        };
+		if (exists('Ext.util.Format'))
+			Ext.apply(Ext.util.Format, {
+				thousandSeparator: ',',
+				decimalSeparator: '.',
+				currencySign: '$',
+				dateFormat: 'm/d/Y'
+			});
 
-        Ext.Date.dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		if (exists('Ext.form.field.VTypes')) {
+			Ext.apply(Ext.form.field.VTypes, {
+				emailText: 'This field should be an e-mail address in the format "user@example.com"',
+				urlText: 'This field should be a URL in the format "http:/' + '/www.example.com"',
+				alphaText: 'This field should only contain letters and _',
+				alphanumText: 'This field should only contain letters, numbers and _'
+			});
+		}
+	});
 
-        Ext.Date.getShortDayName = function(day) {
-            return Ext.Date.dayNames[day].substring(0, 3);
-        };
+	Ext.define('Ext.locale.cn.view.View', {
+		override: 'Ext.view.View',
 
-        Ext.Date.parseCodes.S.s = "(?:st|nd|rd|th)";
-    }
+		emptyText: ''
+	});
 
-    if (Ext.MessageBox) {
-        Ext.MessageBox.buttonText = {
-            ok: "OK",
-            cancel: "Cancel",
-            yes: "Yes",
-            no: "No"
-        };
-    }
+	Ext.define('Ext.locale.cn.grid.plugin.DragDrop', {
+		override: 'Ext.grid.plugin.DragDrop',
 
-    if (exists('Ext.util.Format')) {
-        Ext.apply(Ext.util.Format, {
-            thousandSeparator: ',',
-            decimalSeparator: '.',
-            currencySign: '$',
-            dateFormat: 'm/d/Y'
-        });
-    }
+		dragText: '{0} selected row{1}'
+	});
 
-    if (exists('Ext.form.field.VTypes')) {
-        Ext.apply(Ext.form.field.VTypes, {
-            emailText: 'This field should be an e-mail address in the format "user@example.com"',
-            urlText: 'This field should be a URL in the format "http:/' + '/www.example.com"',
-            alphaText: 'This field should only contain letters and _',
-            alphanumText: 'This field should only contain letters, numbers and _'
-        });
-    }
-});
+	// changing the msg text below will affect the LoadMask
+	Ext.define('Ext.locale.cn.view.AbstractView', {
+		override: 'Ext.view.AbstractView',
 
-Ext.define("Ext.locale.cn.view.View", {
-    override: "Ext.view.View",
-    emptyText: ""
-});
+		msg: '加载...'
+	});
 
-Ext.define("Ext.locale.cn.grid.plugin.DragDrop", {
-    override: "Ext.grid.plugin.DragDrop",
-    dragText: "{0} selected row{1}"
-});
+	Ext.define('Ext.locale.cn.picker.Date', {
+		override: 'Ext.picker.Date',
 
-// changing the msg text below will affect the LoadMask
-Ext.define("Ext.locale.cn.view.AbstractView", {
-    override: "Ext.view.AbstractView",
-    msg: "Loading..."
-});
+		todayText: '今天',
+		minText: '此日期在最小日期前',
+		maxText: '此日期在最大日期之后',
+		disabledDaysText: '',
+		disabledDatesText: '',
+		monthNames: Ext.Date.monthNames,
+		dayNames: Ext.Date.dayNames,
+		nextText: 'Next Month (Control+Right)',
+		prevText: 'Previous Month (Control+Left)',
+		monthYearText: 'Choose a month (Control+Up/Down to move years)',
+		todayTip: '{0} (Spacebar)',
+		format: 'm/d/y',
+		startDay: 0
+	});
 
-Ext.define("Ext.locale.cn.picker.Date", {
-    override: "Ext.picker.Date",
-    todayText: "Today",
-    minText: "This date is before the minimum date",
-    maxText: "This date is after the maximum date",
-    disabledDaysText: "",
-    disabledDatesText: "",
-    monthNames: Ext.Date.monthNames,
-    dayNames: Ext.Date.dayNames,
-    nextText: 'Next Month (Control+Right)',
-    prevText: 'Previous Month (Control+Left)',
-    monthYearText: 'Choose a month (Control+Up/Down to move years)',
-    todayTip: "{0} (Spacebar)",
-    format: "m/d/y",
-    startDay: 0
-});
+	Ext.define('Ext.locale.cn.picker.Month', {
+			override: 'Ext.picker.Month',
 
-Ext.define("Ext.locale.cn.picker.Month", {
-    override: "Ext.picker.Month",
-    okText: "&#160;OK&#160;",
-    cancelText: "Cancel"
-});
+		okText: '&#160;OK&#160;',
+		cancelText: '取消'
+	});
 
-Ext.define("Ext.locale.cn.toolbar.Paging", {
-    override: "Ext.PagingToolbar",
-    beforePageText: "Page",
-    afterPageText: "of {0}",
-    firstText: "First Page",
-    prevText: "Previous Page",
-    nextText: "Next Page",
-    lastText: "Last Page",
-    refreshText: "Refresh",
-    displayMsg: "Displaying {0} - {1} of {2}",
-    emptyMsg: 'No data to display'
-});
+	Ext.define('Ext.locale.cn.toolbar.Paging', {
+		override: 'Ext.PagingToolbar',
 
-Ext.define("Ext.locale.cn.form.Basic", {
-    override: "Ext.form.Basic",
-    waitTitle: "Please Wait..."
-});
+		beforePageText: '页',
+		afterPageText: 'of {0}',
+		firstText: '第一页',
+		prevText: '前一页',
+		nextText: '下一页',
+		lastText: '最后一页',
+		refreshText: '刷新',
+		displayMsg: '显示 {0} - {1} of {2}',
+		emptyMsg: 'No data to display'
+	});
 
-Ext.define("Ext.locale.cn.form.field.Base", {
-    override: "Ext.form.field.Base",
-    invalidText: "The value in this field is invalid"
-});
+	Ext.define('Ext.locale.cn.form.Basic', {
+		override: 'Ext.form.Basic',
 
-Ext.define("Ext.locale.cn.form.field.Text", {
-    override: "Ext.form.field.Text",
-    minLengthText: "The minimum length for this field is {0}",
-    maxLengthText: "The maximum length for this field is {0}",
-    blankText: "This field is required",
-    regexText: "",
-    emptyText: null
-});
+		waitTitle: '请稍等...'
+	});
 
-Ext.define("Ext.locale.cn.form.field.Number", {
-    override: "Ext.form.field.Number",
-    decimalSeparator: ".",
-    decimalPrecision: 2,
-    minText: "The minimum value for this field is {0}",
-    maxText: "The maximum value for this field is {0}",
-    nanText: "{0} is not a valid number"
-});
+	Ext.define('Ext.locale.cn.form.field.Base', {
+		override: 'Ext.form.field.Base',
 
-Ext.define("Ext.locale.cn.form.field.Date", {
-    override: "Ext.form.field.Date",
-    disabledDaysText: "Disabled",
-    disabledDatesText: "Disabled",
-    minText: "The date in this field must be after {0}",
-    maxText: "The date in this field must be before {0}",
-    invalidText: "{0} is not a valid date - it must be in the format {1}",
-    format: "m/d/y",
-    altFormats: "m/d/Y|m-d-y|m-d-Y|m/d|m-d|md|mdy|mdY|d|Y-m-d"
-});
+		invalidText: '此字段的值无效'
+	});
 
-Ext.define("Ext.locale.cn.form.field.ComboBox", {
-    override: "Ext.form.field.ComboBox",
-    valueNotFoundText: undefined
-}, function() {
-    Ext.apply(Ext.form.field.ComboBox.prototype.defaultListConfig, {
-        loadingText: "Loading..."
-    });
-});
+	Ext.define('Ext.locale.cn.form.field.Text', {
+		override: 'Ext.form.field.Text',
 
-Ext.define("Ext.locale.cn.form.field.HtmlEditor", {
-    override: "Ext.form.field.HtmlEditor",
-    createLinkText: 'Please enter the URL for the link:'
-}, function() {
-    Ext.apply(Ext.form.field.HtmlEditor.prototype, {
-        buttonTips: {
-            bold: {
-                title: 'Bold (Ctrl+B)',
-                text: 'Make the selected text bold.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            italic: {
-                title: 'Italic (Ctrl+I)',
-                text: 'Make the selected text italic.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            underline: {
-                title: 'Underline (Ctrl+U)',
-                text: 'Underline the selected text.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            increasefontsize: {
-                title: 'Grow Text',
-                text: 'Increase the font size.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            decreasefontsize: {
-                title: 'Shrink Text',
-                text: 'Decrease the font size.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            backcolor: {
-                title: 'Text Highlight Color',
-                text: 'Change the background color of the selected text.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            forecolor: {
-                title: 'Font Color',
-                text: 'Change the color of the selected text.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            justifyleft: {
-                title: 'Align Text Left',
-                text: 'Align text to the left.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            justifycenter: {
-                title: 'Center Text',
-                text: 'Center text in the editor.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            justifyright: {
-                title: 'Align Text Right',
-                text: 'Align text to the right.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            insertunorderedlist: {
-                title: 'Bullet List',
-                text: 'Start a bulleted list.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            insertorderedlist: {
-                title: 'Numbered List',
-                text: 'Start a numbered list.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            createlink: {
-                title: 'Hyperlink',
-                text: 'Make the selected text a hyperlink.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            },
-            sourceedit: {
-                title: 'Source Edit',
-                text: 'Switch to source editing mode.',
-                cls: Ext.baseCSSPrefix + 'html-editor-tip'
-            }
-        }
-    });
-});
+		minLengthText: '此字段最小长度是 {0}',
+		maxLengthText: '此字段最大长度是{0}',
+		blankText: '此字段是必须的',
+		regexText: '',
+		emptyText: null
+	});
 
-Ext.define("Ext.locale.cn.grid.header.Container", {
-    override: "Ext.grid.header.Container",
-    sortAscText: "Sort Ascending",
-    sortDescText: "Sort Descending",
-    columnsText: "Columns"
-});
+	Ext.define('Ext.locale.cn.form.field.Number', {
+		override: 'Ext.form.field.Number',
 
-Ext.define("Ext.locale.cn.grid.GroupingFeature", {
-    override: "Ext.grid.GroupingFeature",
-    emptyGroupText: '(None)',
-    groupByText: 'Group By This Field',
-    showGroupsText: 'Show in Groups'
-});
+		decimalSeparator: '.',
+		decimalPrecision: 2,
+		minText: '此字段最小长度是 {0}',
+		maxText: '此字段最大长度是 {0}',
+		nanText: '{0} 不是一个有效值'
+	});
 
-Ext.define("Ext.locale.cn.grid.PropertyColumnModel", {
-    override: "Ext.grid.PropertyColumnModel",
-    nameText: "Name",
-    valueText: "Value",
-    dateFormat: "m/j/Y",
-    trueText: "true",
-    falseText: "false"
-});
+	Ext.define('Ext.locale.cn.form.field.Date', {
+		override: 'Ext.form.field.Date',
 
-Ext.define("Ext.locale.cn.grid.BooleanColumn", {
-    override: "Ext.grid.BooleanColumn",
-    trueText: "true",
-    falseText: "false",
-    undefinedText: '&#160;'
-});
+		disabledDaysText: '不可用',
+		disabledDatesText: '不可用',
+		minText: '此字段的日期必须在 {0}之后',
+		maxText: '此字段的日期必须在{0}这前',
+		invalidText: '{0} 是一个无效值 - 它的格式必须是 {1}',
+		format: 'm/d/y',
+		altFormats: 'm/d/Y|m-d-y|m-d-Y|m/d|m-d|md|mdy|mdY|d|Y-m-d'
+	});
 
-Ext.define("Ext.locale.cn.grid.NumberColumn", {
-    override: "Ext.grid.NumberColumn",
-    format: '0,000.00'
-});
+	Ext.define('Ext.locale.cn.form.field.ComboBox', {
+		override: 'Ext.form.field.ComboBox',
 
-Ext.define("Ext.locale.cn.grid.DateColumn", {
-    override: "Ext.grid.DateColumn",
-    format: 'm/d/Y'
-});
+		valueNotFoundText: undefined
+	}, function() {
+		Ext.apply(Ext.form.field.ComboBox.prototype.defaultListConfig, {
+			loadingText: '加载...'
+		});
+	});
 
-Ext.define("Ext.locale.cn.form.field.Time", {
-    override: "Ext.form.field.Time",
-    minText: "The time in this field must be equal to or after {0}",
-    maxText: "The time in this field must be equal to or before {0}",
-    invalidText: "{0} is not a valid time",
-    format: "g:i A",
-    altFormats: "g:ia|g:iA|g:i a|g:i A|h:i|g:i|H:i|ga|ha|gA|h a|g a|g A|gi|hi|gia|hia|g|H"
-});
+	Ext.define('Ext.locale.cn.form.field.HtmlEditor', {
+		override: 'Ext.form.field.HtmlEditor',
+		createLinkText: 'Please enter the URL for the link:'
+	}, function() {
+		Ext.apply(Ext.form.field.HtmlEditor.prototype, {
+			buttonTips: {
+				bold: {
+					title: 'Bold (Ctrl+B)',
+					text: 'Make the selected text bold.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				italic: {
+					title: 'Italic (Ctrl+I)',
+					text: 'Make the selected text italic.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				underline: {
+					title: 'Underline (Ctrl+U)',
+					text: 'Underline the selected text.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				increasefontsize: {
+					title: 'Grow Text',
+					text: 'Increase the font size.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				decreasefontsize: {
+					title: 'Shrink Text',
+					text: 'Decrease the font size.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				backcolor: {
+					title: 'Text Highlight Color',
+					text: 'Change the background color of the selected text.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				forecolor: {
+					title: 'Font Color',
+					text: 'Change the color of the selected text.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				justifyleft: {
+					title: 'Align Text Left',
+					text: 'Align text to the left.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				justifycenter: {
+					title: 'Center Text',
+					text: 'Center text in the editor.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				justifyright: {
+					title: 'Align Text Right',
+					text: 'Align text to the right.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				insertunorderedlist: {
+					title: 'Bullet List',
+					text: 'Start a bulleted list.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				insertorderedlist: {
+					title: 'Numbered List',
+					text: 'Start a numbered list.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				createlink: {
+					title: 'Hyperlink',
+					text: 'Make the selected text a hyperlink.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				},
+				sourceedit: {
+					title: 'Source Edit',
+					text: 'Switch to source editing mode.',
+					cls: Ext.baseCSSPrefix + 'html-editor-tip'
+				}
+			}
+		});
+	});
 
-Ext.define("Ext.locale.cn.form.CheckboxGroup", {
-    override: "Ext.form.CheckboxGroup",
-    blankText: "You must select at least one item in this group"
-});
+	Ext.define('Ext.locale.cn.grid.header.Container', {
+		override: 'Ext.grid.header.Container',
 
-Ext.define("Ext.locale.cn.form.RadioGroup", {
-    override: "Ext.form.RadioGroup",
-    blankText: "You must select one item in this group"
-});
+		sortAscText: '升序排序',
+		sortDescText: '降序排序',
+		columnsText: '列'
+	});
 
-// This is needed until we can refactor all of the locales into individual files
-Ext.define("Ext.locale.cn.Component", {
-    override: "Ext.Component"
-});
+	Ext.define('Ext.locale.cn.grid.GroupingFeature', {
+		override: 'Ext.grid.GroupingFeature',
 
+		emptyGroupText: '(None)',
+		groupByText: 'Group By This Field',
+		showGroupsText: 'Show in Groups'
+	});
+
+	Ext.define('Ext.locale.cn.grid.PropertyColumnModel', {
+		override: 'Ext.grid.PropertyColumnModel',
+
+		nameText: '名称',
+		valueText: '值',
+		dateFormat: 'm/j/Y',
+		trueText: '正确',
+		falseText: '错误'
+	});
+
+	Ext.define('Ext.locale.cn.grid.BooleanColumn', {
+		override: 'Ext.grid.BooleanColumn',
+
+		trueText: '正确',
+		falseText: '错误',
+		undefinedText: '&#160;'
+	});
+
+	Ext.define('Ext.locale.cn.grid.NumberColumn', {
+		override: 'Ext.grid.NumberColumn',
+
+		format: '0,000.00'
+	});
+
+	Ext.define('Ext.locale.cn.grid.DateColumn', {
+		override: 'Ext.grid.DateColumn',
+
+		format: 'm/d/Y'
+	});
+
+	Ext.define('Ext.locale.cn.form.field.Time', {
+		override: 'Ext.form.field.Time',
+
+		minText: '此字段的时间必须等于或晚于{0}',
+		maxText: '此字段的时间必须等于或早于 {0}',
+		invalidText: '{0} 是一个无效时间',
+		format: 'g:i A',
+		altFormats: 'g:ia|g:iA|g:i a|g:i A|h:i|g:i|H:i|ga|ha|gA|h a|g a|g A|gi|hi|gia|hia|g|H'
+	});
+
+	Ext.define('Ext.locale.cn.form.CheckboxGroup', {
+		override: 'Ext.form.CheckboxGroup',
+
+		blankText: '在此组中你必须选择一项'
+	});
+
+	Ext.define('Ext.locale.cn.form.RadioGroup', {
+		override: 'Ext.form.RadioGroup',
+
+		blankText: '在此组中你必须选择一项'
+	});
+
+	// This is needed until we can refactor all of the locales into individual files
+	Ext.define('Ext.locale.cn.Component', {
+		override: 'Ext.Component'
+	});
+
+})();
