58dfeabf4be9b0772002d2ab97a3482380e153be
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-09-29 11:45:21 +0200

    #1801: Print the output of SQL views - UI implementation

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/dataView/sql/FormPanel.js
@@ -0,0 +1,58 @@
+(function() {
+
+	Ext.define('CMDBuild.view.management.dataView.sql.FormPanel', {
+		extend: 'Ext.tab.Panel',
+
+		requires: ['CMDBuild.core.proxy.CMProxyConstants'],
+
+		/**
+		 * @cfg {CMDBuild.controller.management.dataView.CMModCardController}
+		 */
+		delegate: undefined,
+
+		/**
+		 * @property {CMDBuild.view.management.dataView.sql.tabs.CardPanel}
+		 */
+		cardPanel: undefined,
+
+		bodyCls: 'x-panel-body-default-framed cmbordertop',
+		border: false,
+		frame: false,
+
+		initComponent: function() {
+			Ext.apply(this, {
+				items: [
+					this.cardPanel = Ext.create('CMDBuild.view.management.dataView.sql.tabs.CardPanel', { delegate: this.delegate }),
+					Ext.create('Ext.panel.Panel', {
+						title: CMDBuild.Translation.management.modcard.tabs.detail,
+						border: false,
+						disabled: true
+					}),
+					Ext.create('Ext.panel.Panel', {
+						title: CMDBuild.Translation.management.modcard.tabs.notes,
+						border: false,
+						disabled: true
+					}),
+					Ext.create('Ext.panel.Panel', {
+						title: CMDBuild.Translation.management.modcard.tabs.relations,
+						border: false,
+						disabled: true
+					}),
+					Ext.create('Ext.panel.Panel', {
+						title: CMDBuild.Translation.management.modcard.tabs.history,
+						border: false,
+						disabled: true
+					}),
+					Ext.create('Ext.panel.Panel', {
+						title: CMDBuild.Translation.management.modcard.tabs.attachments,
+						border: false,
+						disabled: true
+					})
+				]
+			});
+
+			this.callParent(arguments);
+		}
+	});
+
+})();
