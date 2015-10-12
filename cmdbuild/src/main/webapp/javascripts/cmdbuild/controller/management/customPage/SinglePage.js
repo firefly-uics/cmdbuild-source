c9c6262aae2219d1a038d3d1bb541a2c0542acd4
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-10-12 14:26:11 +0200

    #1678: Custom Pages - Custom Pages on menu

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/customPage/SinglePage.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/customPage/SinglePage.js
@@ -20,6 +20,9 @@
 		 */
 		onViewOnFront: function(node) {
 			if (!Ext.isEmpty(node)) {
+				var guiFrameworkPath = window.location.toString().split('/');
+				guiFrameworkPath = Ext.Array.slice(guiFrameworkPath, 0, guiFrameworkPath.length - 1).join('/');
+
 				this.setViewTitle(node.get(CMDBuild.core.proxy.CMProxyConstants.TEXT));
 
 				this.view.removeAll();
@@ -28,7 +31,9 @@
 
 					autoEl: {
 						tag: 'iframe',
-						src: CMDBuild.Constants.customPages.path + node.get(CMDBuild.core.proxy.CMProxyConstants.TEXT)
+						src: CMDBuild.Constants.customPages.customizationsPath
+							+ node.get(CMDBuild.core.proxy.CMProxyConstants.TEXT)
+							+ '/?guiFrameworkCorePath=' + guiFrameworkPath + '/' + CMDBuild.Constants.customPages.corePath
 					}
 				});
 			}
