71194ca96dc24873f56186b13ddfb0a11afca681
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-11-19 09:30:15 +0100

    #1866: Widget CustomForm: row clone button implementation

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/core/buttons/iconized/Clone.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/core/buttons/iconized/Clone.js
@@ -1,6 +1,6 @@
 (function() {
 
-	Ext.define('CMDBuild.core.buttons.iconized.row.Clone', {
+	Ext.define('CMDBuild.core.buttons.iconized.Clone', {
 		extend: 'CMDBuild.core.buttons.Base',
 
 		iconCls: 'clone',
