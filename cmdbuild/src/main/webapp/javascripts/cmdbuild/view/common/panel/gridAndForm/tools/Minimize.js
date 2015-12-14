e8c0af81a1da17a400eb60847bfff418af417fe7
Alessandro Romanin <a.romanin@tecnoteca.com>
2015-12-14 15:53:57 +0100

    #1728: Navigation history of default client

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/common/panel/gridAndForm/tools/Minimize.js
@@ -0,0 +1,20 @@
+(function() {
+
+	Ext.define('CMDBuild.view.common.panel.gridAndForm.tools.Minimize', {
+		extend: 'Ext.panel.Tool',
+
+		tooltip: CMDBuild.Translation.minimizeGrid,
+		type: 'minimize',
+
+		/**
+		 * @param {Ext.EventObject} event
+		 * @param {Ext.Element} target
+		 * @param {Ext.panel.Header} owner
+		 * @param {Ext.panel.Tool} tool
+		 */
+		handler: function(event, target, owner, tool) {
+			_CMUIState.onlyForm();
+		}
+	});
+
+})();
