f1bc4036328d5442d00200155d59cce918421fed
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-01-11 12:43:47 +0100

    Added Arabic translation

--- cmdbuild/src/main/webapp/javascripts/ext-4.2.0/locale/ext-lang-ar.js
+++ cmdbuild/src/main/webapp/javascripts/ext-4.2.0/locale/ext-lang-ar.js
@@ -5,16 +5,16 @@
 		var exists = Ext.Function.bind(cm.get, cm);
 
 		if (Ext.Updater)
-			Ext.Updater.defaults.indicatorText = '<div class="loading-indicator">Loading...</div>';
+			Ext.Updater.defaults.indicatorText = '<div class="loading-indicator">جاري التحميل ...</div>';
 
 		if (exists('Ext.data.Types'))
 			Ext.data.Types.stripRe = /[\$,%]/g;
 
 		if (Ext.Date) {
-			Ext.Date.monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			Ext.Date.monthNames = ['يناير', 'فبراير', 'مارس', 'إبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سمبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'];
 
 			Ext.Date.getShortMonthName = function(month) {
-				return Ext.Date.monthNames[month].substring(0, 3);
+					return Ext.Date.monthNames[month].substring(0, 3);
 			};
 
 			Ext.Date.monthNumbers = {
@@ -36,7 +36,7 @@
 				return Ext.Date.monthNumbers[name.substring(0, 1).toUpperCase() + name.substring(1, 3).toLowerCase()];
 			};
 
-			Ext.Date.dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+			Ext.Date.dayNames = ['الأحد', 'الإثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'];
 
 			Ext.Date.getShortDayName = function(day) {
 				return Ext.Date.dayNames[day].substring(0, 3);
@@ -47,10 +47,10 @@
 
 		if (Ext.MessageBox) {
 			Ext.MessageBox.buttonText = {
-				ok: 'OK',
-				cancel: 'Cancel',
-				yes: 'Yes',
-				no: 'No'
+				ok: 'حسنًا',
+				cancel: 'إلغاء',
+				yes: 'نعم',
+				no: 'لا'
 			};
 		}
 
@@ -64,10 +64,10 @@
 
 		if (exists('Ext.form.field.VTypes'))
 			Ext.apply(Ext.form.field.VTypes, {
-				emailText: 'This field should be an e-mail address in the format "user@example.com"',
-				urlText: 'This field should be a URL in the format "http:/' + '/www.example.com"',
-				alphaText: 'This field should only contain letters and _',
-				alphanumText: 'This field should only contain letters, numbers and _'
+				emailText: 'يجب أن يكون هذا الحقل بريد إلكتروني كهذا "user@example.com"',
+				urlText: 'يجب أن يكون هذا الحقل رابط كهذا "http:/' + '/www.example.com"',
+				alphaText: 'هذا الحقل يمكن أن يحتوي فقط على حروف و _',
+				alphanumText: 'هذا الحقل يمكن أن يحتوي فقط على حروف وأرقام و _'
 			});
 	});
 
@@ -80,73 +80,79 @@
 	Ext.define('Ext.locale.ar.grid.plugin.DragDrop', {
 		override: 'Ext.grid.plugin.DragDrop',
 
-		dragText: '{0} selected row{1}'
+		dragText: '{0} {صف مختار{1'
 	});
 
 	// changing the msg text below will affect the LoadMask
+
 	Ext.define('Ext.locale.ar.view.AbstractView', {
 		override: 'Ext.view.AbstractView',
 
-		msg: 'Loading...'
+		msg: 'جاري التحميل ...'
 	});
 
 	Ext.define('Ext.locale.ar.picker.Date', {
 		override: 'Ext.picker.Date',
 
-		todayText: 'Today',
-		minText: 'This date is before the minimum date',
-		maxText: 'This date is after the maximum date',
+		todayText: 'اليوم',
+		minText: 'هذا التاريخ قبل التاريخ الأدنى',
+		maxText: 'هذا التاريخ قبل التاريخ الأقصى',
 		disabledDaysText: '',
 		disabledDatesText: '',
 		monthNames: Ext.Date.monthNames,
 		dayNames: Ext.Date.dayNames,
-		nextText: 'Next Month (Control+Right)',
-		prevText: 'Previous Month (Control+Left)',
-		monthYearText: 'Choose a month (Control+Up/Down to move years)',
+		nextText: 'الشهر التالي (Control+Right)',
+		prevText: 'الشهر السابق (Control+Left)',
+		monthYearText: 'اختر شهر (Control+Up/Down لتحريك السنوات)',
 		todayTip: '{0} (Spacebar)',
 		format: 'm/d/y',
 		startDay: 0
+
 	});
 
 	Ext.define('Ext.locale.ar.picker.Month', {
 		override: 'Ext.picker.Month',
 
-		okText: '&#160;OK&#160;',
-		cancelText: 'Cancel'
+		okText: 'حسنًا',
+		cancelText: 'إلغاء'
 	});
 
 	Ext.define('Ext.locale.ar.toolbar.Paging', {
 		override: 'Ext.PagingToolbar',
 
-		beforePageText: 'Page',
-		afterPageText: 'of {0}',
-		firstText: 'First Page',
-		prevText: 'Previous Page',
-		nextText: 'Next Page',
-		lastText: 'Last Page',
-		refreshText: 'Refresh',
-		displayMsg: 'Displaying {0} - {1} of {2}',
-		emptyMsg: 'No data to display'
+		beforePageText: 'صفحة',
+		afterPageText: '{من {0',
+		firstText: 'الصفحة الأولى',
+		prevText: 'الصفحة السابقة',
+		nextText: 'الصفحة التالية',
+		lastText: 'الصفحة الأخيرة',
+		refreshText: 'تحديث',
+		displayMsg: '{عرض {0} - {1} من {2',
+		emptyMsg: 'ليس هناك تاريخ لعرضه'
 	});
 
 	Ext.define('Ext.locale.ar.form.Basic', {
 		override: 'Ext.form.Basic',
 
-		waitTitle: 'Please Wait...'
+		waitTitle: 'الرجاء الإنتظار...'
 	});
+
+
 
 	Ext.define('Ext.locale.ar.form.field.Base', {
 		override: 'Ext.form.field.Base',
 
-		invalidText: 'The value in this field is invalid'
+		invalidText: 'قيمة هذا الحقل غير صالحة'
 	});
+
+
 
 	Ext.define('Ext.locale.ar.form.field.Text', {
 		override: 'Ext.form.field.Text',
 
-		minLengthText: 'The minimum length for this field is {0}',
-		maxLengthText: 'The maximum length for this field is {0}',
-		blankText: 'This field is required',
+		minLengthText: 'أدنى طول لهذا الحقل {0} حرف',
+		maxLengthText: 'أقصى طول لهذا الحقل {0} حرف',
+		blankText: 'هذا الحقل مطلوب',
 		regexText: '',
 		emptyText: null
 	});
@@ -156,19 +162,19 @@
 
 		decimalSeparator: '.',
 		decimalPrecision: 2,
-		minText: 'The minimum value for this field is {0}',
-		maxText: 'The maximum value for this field is {0}',
-		nanText: '{0} is not a valid number'
+		minText: '{أدنى قيمة لهذا الحقل {0',
+		maxText: '{أقصى قيمة لهذا الحقل {0',
+		nanText: '{0} ليس برقم'
 	});
 
 	Ext.define('Ext.locale.ar.form.field.Date', {
 		override: 'Ext.form.field.Date',
 
-		disabledDaysText: 'Disabled',
-		disabledDatesText: 'Disabled',
-		minText: 'The date in this field must be after {0}',
-		maxText: 'The date in this field must be before {0}',
-		invalidText: '{0} is not a valid date - it must be in the format {1}',
+		disabledDaysText: 'معطل',
+		disabledDatesText: 'معطل',
+		minText: '{تاريخ هذا الحقل يجب أن يكون بعد {0',
+		maxText: '{تاريخ هذا الحقل يجب أن يكون قبل {0',
+		invalidText: '{0} {1} ليس بتاريخ - يجب أن يكون كـ',
 		format: 'm/d/y',
 		altFormats: 'm/d/Y|m-d-y|m-d-Y|m/d|m-d|md|mdy|mdY|d|Y-m-d'
 	});
@@ -179,85 +185,85 @@
 		valueNotFoundText: undefined
 	}, function() {
 		Ext.apply(Ext.form.field.ComboBox.prototype.defaultListConfig, {
-			loadingText: 'Loading...'
+			loadingText: 'جاري التحميل...'
 		});
 	});
 
 	Ext.define('Ext.locale.ar.form.field.HtmlEditor', {
 		override: 'Ext.form.field.HtmlEditor',
 
-		createLinkText: 'Please enter the URL for the link:'
+		createLinkText: 'رجاءً أدخل الرابط:'
 	}, function() {
 		Ext.apply(Ext.form.field.HtmlEditor.prototype, {
 			buttonTips: {
 				bold: {
-					title: 'Bold (Ctrl+B)',
-					text: 'Make the selected text bold.',
+					title: 'ثخين (Ctrl+B)',
+					text: 'يجعل النص المختار ثخينًا.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				italic: {
-					title: 'Italic (Ctrl+I)',
-					text: 'Make the selected text italic.',
+					title: 'مائل (Ctrl+I)',
+					text: 'يجعل النص المختار مائلًا.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				underline: {
-					title: 'Underline (Ctrl+U)',
-					text: 'Underline the selected text.',
+					title: 'تحته خط (Ctrl+U)',
+					text: 'يضع خط تحت النص المختار.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				increasefontsize: {
-					title: 'Grow Text',
-					text: 'Increase the font size.',
+					title: 'تضخيم النص',
+					text: 'يزيد من حجم الخط.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				decreasefontsize: {
-					title: 'Shrink Text',
-					text: 'Decrease the font size.',
+					title: 'إكماش النص',
+					text: 'يقلل من حجم الخط.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				backcolor: {
-					title: 'Text Highlight Color',
-					text: 'Change the background color of the selected text.',
+					title: 'لون التسليط',
+					text: 'يغيير لون خلفية النص المختار.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				forecolor: {
-					title: 'Font Color',
-					text: 'Change the color of the selected text.',
+					title: 'لون الخط',
+					text: 'يغيير من لون النص المختار.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				justifyleft: {
-					title: 'Align Text Left',
-					text: 'Align text to the left.',
+					title: 'محاذاة لليسار',
+					text: 'محاذاة النص لليسار.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				justifycenter: {
-					title: 'Center Text',
-					text: 'Center text in the editor.',
+					title: 'توسيط',
+					text: 'توسيط النص في المحرر.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				justifyright: {
-					title: 'Align Text Right',
-					text: 'Align text to the right.',
+					title: 'محاذاة لليمين',
+					text: 'محاذاة النص لليمين.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				insertunorderedlist: {
-					title: 'Bullet List',
-					text: 'Start a bulleted list.',
+					title: 'قائمة',
+					text: 'إنشاء قائمة.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				insertorderedlist: {
-					title: 'Numbered List',
-					text: 'Start a numbered list.',
+					title: 'قائمة مرقمة',
+					text: 'إنشاء قائمة مرقمة.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				createlink: {
-					title: 'Hyperlink',
-					text: 'Make the selected text a hyperlink.',
+					title: 'رابط',
+					text: 'جعل النص المختار رابط.',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				},
 				sourceedit: {
-					title: 'Source Edit',
-					text: 'Switch to source editing mode.',
+					title: 'تحرير المصدر',
+					text: 'التبديل إلى وضع تحرير المصدر',
 					cls: Ext.baseCSSPrefix + 'html-editor-tip'
 				}
 			}
@@ -267,34 +273,34 @@
 	Ext.define('Ext.locale.ar.grid.header.Container', {
 		override: 'Ext.grid.header.Container',
 
-		sortAscText: 'Sort Ascending',
-		sortDescText: 'Sort Descending',
-		columnsText: 'Columns'
+		sortAscText: 'رتب تصاعديًا',
+		sortDescText: 'رتب تنازليًا',
+		columnsText: 'الأعمدة'
 	});
 
 	Ext.define('Ext.locale.ar.grid.GroupingFeature', {
 		override: 'Ext.grid.GroupingFeature',
 
-		emptyGroupText: '(None)',
-		groupByText: 'Group By This Field',
-		showGroupsText: 'Show in Groups'
+		emptyGroupText: '(بدون)',
+		groupByText: 'جمع حسب هذا الحقل',
+		showGroupsText: 'أظهر في مجموعات'
 	});
 
 	Ext.define('Ext.locale.ar.grid.PropertyColumnModel', {
 		override: 'Ext.grid.PropertyColumnModel',
 
-		nameText: 'Name',
-		valueText: 'Value',
+		nameText: 'الاسم',
+		valueText: 'القيمة',
 		dateFormat: 'm/j/Y',
-		trueText: 'true',
-		falseText: 'false'
+		trueText: 'صواب',
+		falseText: 'خطأ'
 	});
 
 	Ext.define('Ext.locale.ar.grid.BooleanColumn', {
 		override: 'Ext.grid.BooleanColumn',
 
-		trueText: 'true',
-		falseText: 'false',
+		trueText: 'صواب',
+		falseText: 'خطأ',
 		undefinedText: '&#160;'
 	});
 
@@ -313,9 +319,9 @@
 	Ext.define('Ext.locale.ar.form.field.Time', {
 		override: 'Ext.form.field.Time',
 
-		minText: 'The time in this field must be equal to or after {0}',
-		maxText: 'The time in this field must be equal to or before {0}',
-		invalidText: '{0} is not a valid time',
+		minText: '{الوقت في هذا الحقل يجب أن يكون بعد أو مساوٍ لـ {0',
+		maxText: '{الوقت في هذا الحقل يجب أن يكون قبل أو مساوٍ لـ {0',
+		invalidText: '{0} ليس بوقت',
 		format: 'g:i A',
 		altFormats: 'g:ia|g:iA|g:i a|g:i A|h:i|g:i|H:i|ga|ha|gA|h a|g a|g A|gi|hi|gia|hia|g|H'
 	});
@@ -323,13 +329,13 @@
 	Ext.define('Ext.locale.ar.form.CheckboxGroup', {
 		override: 'Ext.form.CheckboxGroup',
 
-		blankText: 'You must select at least one item in this group'
+		blankText: 'يجب أن تختار عنصرًا واحدًا على الأقل من هذه المجموعة'
 	});
 
 	Ext.define('Ext.locale.ar.form.RadioGroup', {
 		override: 'Ext.form.RadioGroup',
 
-		blankText: 'You must select one item in this group'
+		blankText: 'يجب أن تختار عنصرًا واحدًا فقط من هذه المجموعة'
 	});
 
 	// This is needed until we can refactor all of the locales into individual files
