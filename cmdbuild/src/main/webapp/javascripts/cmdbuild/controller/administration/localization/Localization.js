84035881c57c15cec05efefaf34888b94433c579
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-01-12 09:43:07 +0100

    #1447: Localization table - Base module structure

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/administration/localization/Localization.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/administration/localization/Localization.js
@@ -39,30 +39,38 @@
 		 *
 		 * @override
 		 */
-		onLocalizationModuleInit: function(node) { // TODO: implementare lo switch come funzione esterna per ridurre il numero di righe ecc con il return
+		onLocalizationModuleInit: function(node) {
 			if (!Ext.Object.isEmpty(node)) {
 				this.view.removeAll(true);
 
-				switch(node.get(CMDBuild.core.constants.Proxy.SECTION_HIERARCHY)[0]) {
-					case 'advancedTranslationsTable': {
-						this.sectionController = Ext.create('CMDBuild.controller.administration.localization.advancedTable.AdvancedTable', { parentDelegate: this });
-					} break;
-
-					case 'importExport': {
-						this.sectionController = Ext.create('CMDBuild.controller.administration.localization.ImportExport', { parentDelegate: this });
-					} break;
-
-					case 'configuration':
-					default: {
-						this.sectionController = Ext.create('CMDBuild.controller.administration.localization.Configuration', { parentDelegate: this });
-					}
-				}
+				this.sectionController = this.sectionIdentifierEvaluation(node.get(CMDBuild.core.constants.Proxy.SECTION_HIERARCHY)[0]);
 
 				this.view.add(this.sectionController.getView());
 
 				this.setViewTitle(node.get(CMDBuild.core.constants.Proxy.TEXT));
 
 				this.onModuleInit(node); // Custom callParent() implementation
+			}
+		},
+
+		/**
+		 * @param {String} sectionIdentifier
+		 *
+		 * @returns {Mixed}
+		 *
+		 * @private
+		 */
+		sectionIdentifierEvaluation: function(sectionIdentifier) {
+			switch (sectionIdentifier) {
+				case 'advancedTranslationsTable':
+					return Ext.create('CMDBuild.controller.administration.localization.advancedTable.AdvancedTable', { parentDelegate: this });
+
+				case 'importExport':
+					return Ext.create('CMDBuild.controller.administration.localization.ImportExport', { parentDelegate: this });
+
+				case 'configuration':
+				default:
+					return Ext.create('CMDBuild.controller.administration.localization.Configuration', { parentDelegate: this });
 			}
 		}
 	});
