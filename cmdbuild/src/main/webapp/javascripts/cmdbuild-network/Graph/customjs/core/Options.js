243bbc86c0fe5907ac0cf60620652d6aa4bb7944
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-01-12 14:58:27 +0100

    #1726: Relations graph on gui-framework - Configuration on Json

--- cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/Options.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/Options.js
@@ -1,6 +1,6 @@
 (function($) {
 	var INCLUDED_FILE = "NetworkConfigurationFile";
-	if (! $.Cmdbuild.g3d) {
+	if (!$.Cmdbuild.g3d) {
 		$.Cmdbuild.g3d = {};
 	}
 	var Options = function() {
@@ -8,11 +8,11 @@
 		this.init = function(name) {
 			return this[name];
 		};
-		
-		if (! $.Cmdbuild.customvariables.options) {
+
+		if (!$.Cmdbuild.customvariables.options) {
 			$.Cmdbuild.customvariables.options = {};
 		}
-		for (var key in $.Cmdbuild.custom.configuration) {
+		for ( var key in $.Cmdbuild.custom.configuration) {
 			this[key] = $.Cmdbuild.custom.configuration[key];
 		}
 		this.data = function(name) {
@@ -28,9 +28,34 @@
 		};
 		this.init();
 	};
-	$.Cmdbuild.g3d.Options = Options;	
-	//statics
-	$.Cmdbuild.g3d.Options.chargeConfiguration = function(configurationFile, callback, callbackScope) {
+	$.Cmdbuild.g3d.Options = Options;
+	$.Cmdbuild.g3d.Options.initFields = function() {
+		$("#explosionLevels").spinner("value",
+				$.Cmdbuild.custom.configuration.explosionLevels);
+
+	};
+	$.Cmdbuild.g3d.Options.loadConfiguration = function(configurationFile,
+			callback, callbackScope) {
+		$.Cmdbuild.g3d.Options
+				.loadJsonConfiguration(
+						configurationFile,
+						function(configuration) {
+							$.Cmdbuild.utilities.proxy
+									.getGraphConfiguration(
+											function(graphConfiguration) {
+												// CONFIGURATION:
+												// here the mixing between the
+												// Json properties and those
+												// defined
+												// in administration/setup
+												configuration.explosionLevels = graphConfiguration.baseLevel;
+												callback.apply(callbackScope,
+														[configuration]);
+											}, this);
+						}, this);
+	};
+	$.Cmdbuild.g3d.Options.loadJsonConfiguration = function(configurationFile,
+			callback, callbackScope) {
 		var fileName = $.Cmdbuild.appConfigUrl + configurationFile;
 		$.getJSON(fileName, {}, function(configuration) {
 			$.Cmdbuild.g3d.Options.includeFiles(configuration, function() {
@@ -38,32 +63,42 @@
 			}, this);
 		}, this);
 	};
-	$.Cmdbuild.g3d.Options.includeFiles = function(configuration, callback, callbackScope) {
+	$.Cmdbuild.g3d.Options.includeFiles = function(configuration, callback,
+			callbackScope) {
 		var filesToInclude = [];
-		for (var key in configuration) {
+		for ( var key in configuration) {
 			if (configuration[key][INCLUDED_FILE]) {
 				filesToInclude.push({
 					key: key,
-					fileName: configuration[key][INCLUDED_FILE] 
+					fileName: configuration[key][INCLUDED_FILE]
 				});
 			}
 		}
-		$.Cmdbuild.g3d.Options.chargeFiles(configuration, filesToInclude, function() {
-			callback.apply(callbackScope, [configuration]);
-		}), this;
+		$.Cmdbuild.g3d.Options.loadFiles(configuration, filesToInclude,
+				function() {
+					callback.apply(callbackScope, [configuration]);
+				}), this;
 	};
-	$.Cmdbuild.g3d.Options.chargeFiles = function(configuration, filesToInclude, callback, callbackScope) {
+	$.Cmdbuild.g3d.Options.loadFiles = function(configuration, filesToInclude,
+			callback, callbackScope) {
 		if (filesToInclude.length == 0) {
 			callback.apply(callbackScope, [configuration]);
 			return;
 		}
 		var file = filesToInclude[0];
 		filesToInclude.splice(0, 1);
-		$.Cmdbuild.g3d.Options.chargeConfiguration(file.fileName, function(response) {
+		$.Cmdbuild.g3d.Options.loadJsonConfiguration(file.fileName, function(
+				response) {
 			configuration[file.key] = response;
-			$.Cmdbuild.g3d.Options.chargeFiles(configuration, filesToInclude, function() {
-				callback.apply(callbackScope, [configuration]);
-			}), this;
+			$.Cmdbuild.g3d.Options.loadFiles(configuration, filesToInclude,
+					function() {
+						callback.apply(callbackScope, [configuration]);
+					}), this;
 		}, this);
 	};
+	// $.Cmdbuild.utilities.proxy.getGraphConfiguration(function(
+	// graphConfiguration) {
+	// console.log(graphConfiguration, "-----", configuration);
+	// alert(1);
+	// configuration.explosionLevels = graphConfiguration.baseLevel;
 })(jQuery);
