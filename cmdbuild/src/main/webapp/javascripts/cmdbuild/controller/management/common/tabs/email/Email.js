1e27f257b54bc81351352e341be9b88fb684e9fa
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-01-18 11:47:39 +0100

    #1968: [Fix] Tab Email: noSubjectPrefix configuration parameter not correctly managed

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/common/tabs/email/Email.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/common/tabs/email/Email.js
@@ -255,9 +255,7 @@
 			this.callParent(arguments);
 
 			// Build controllers
-			this.controllerGrid = Ext.create('CMDBuild.controller.management.common.tabs.email.Grid', {
-				parentDelegate: this
-			});
+			this.controllerGrid = Ext.create('CMDBuild.controller.management.common.tabs.email.Grid', { parentDelegate: this });
 			this.grid = this.controllerGrid.getView();
 
 			this.controllerConfirmRegenerationWindow = Ext.create('CMDBuild.controller.management.common.tabs.email.ConfirmRegenerationWindow', {
@@ -763,7 +761,7 @@
 			) {
 				var me = this;
 				var xaVars = Ext.apply({}, template.getData(), template.get(CMDBuild.core.proxy.CMProxyConstants.VARIABLES));
-
+_debug('templateResolver template', template);
 				var templateResolver = new CMDBuild.Management.TemplateResolver({
 					clientForm: this.parentDelegate.getFormForTemplateResolver(),
 					xaVars: xaVars,
@@ -771,7 +769,7 @@
 						this.selectedEntity.get(CMDBuild.core.proxy.CMProxyConstants.ENTITY)
 					)
 				});
-
+_debug('templateResolver', templateResolver);
 				templateResolver.resolveTemplates({
 					attributes: Ext.Object.getKeys(xaVars),
 					callback: function(values, ctx) {
@@ -793,7 +791,7 @@
 						emailObject.set(CMDBuild.core.proxy.CMProxyConstants.REFERENCE, me.cmfg('selectedEntityIdGet'));
 						emailObject.set(CMDBuild.core.proxy.CMProxyConstants.TEMPLATE, template.get(CMDBuild.core.proxy.CMProxyConstants.KEY));
 						emailObject.set(CMDBuild.core.proxy.CMProxyConstants.TEMPORARY, me.cmfg('selectedEntityIdGet') < 0); // Setup temporary parameter
-
+_debug('template regen', emailObject, values);
 						if (me.checkCondition(values, templateResolver)) {
 							_msg('Template with subject "' + values[CMDBuild.core.proxy.CMProxyConstants.SUBJECT] + '" regenerated');
 
