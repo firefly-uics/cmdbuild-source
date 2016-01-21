f0743ba77660500cceeb9fee8e9cdb77f79a37f0
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-01-21 14:49:03 +0100

    #1726: Relations graph on gui-framework - Configuration on Json
    Values and sprites configuration refactoring

--- cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/constants.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/constants.js
@@ -1,5 +1,10 @@
 (function($) {
 	$.Cmdbuild.g3d.constants = {
-			GUICOMPOUNDNODE: "GUICOMPOUNDNODE"
+			GUICOMPOUNDNODE: "GUICOMPOUNDNODE",
+			DEFAULTSHAPE: "cmdbuildDefaultShape",
+			SELECTIONSHAPE: "cmdbuildSelectionShape",
+			RELATIONSHAPE: "cmdbuildRelationShape",
+			CONFIGURATION_FILE: "configurations/configuration.json",
+			SPRITESPATH: "sprites/"
 	};
 })(jQuery);
