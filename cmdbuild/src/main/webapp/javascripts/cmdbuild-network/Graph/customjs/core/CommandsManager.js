be1524980d1a2309d77f9d6bd4825dff60e4e8b7
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-01-26 10:33:58 +0100

    #1726: Relations graph on gui-framework -  Problems on Undo The undo cannot be called during a command's execution

--- cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/CommandsManager.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/CommandsManager.js
@@ -1,12 +1,13 @@
 (function($) {
-	if (! $.Cmdbuild.g3d) {
+	if (!$.Cmdbuild.g3d) {
 		$.Cmdbuild.g3d = {};
 	}
 	var CommandsManager = function(model) {
 		this.model = model;
 		this.commandsStack = [];
 		this.execute = function(command, params, callback, callbackScope) {
-			if ($.Cmdbuild.customvariables.commandInExecution && params.batch !== true) {
+			if ($.Cmdbuild.customvariables.commandInExecution
+					&& params.batch !== true) {
 				if (callback) {
 					callback.apply(callbackScope, []);
 				}
@@ -15,11 +16,11 @@
 			var me = this;
 			var me = this;
 			$.Cmdbuild.authProxy.maskRequest();
-			//this for give control at the mask to start
-			setTimeout(function() { 
+			// this for give control at the mask to start
+			setTimeout(function() {
 				command.execute(function(response) {
 					command.batch = (params.batch === true);
-					me.commandsStack.push(command);		
+					me.commandsStack.push(command);
 					$.Cmdbuild.authProxy.unmaskRequest();
 					if (callback) {
 						callback.apply(callbackScope, [response]);
@@ -28,15 +29,16 @@
 			}, 10);
 		};
 		this.undo = function() {
-			if (this.commandsStack.length == 1) {
+			if (this.commandsStack.length == 1 || $.Cmdbuild.customvariables.commandInExecution) {
 				return;
 			}
 			var command = this.commandsStack.pop();
 			var me = this;
 			$.Cmdbuild.authProxy.maskRequest();
-			//this for give control at the mask to start
-			setTimeout(function() { 
-				command.undo(me.model); 
+			// this for give control at the mask to start
+			$.Cmdbuild.customvariables.selected.erase();
+			setTimeout(function() {
+				command.undo(me.model);
 				$.Cmdbuild.authProxy.unmaskRequest();
 			}, 10);
 		};
