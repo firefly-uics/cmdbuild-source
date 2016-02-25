2653d38000cc0fc50fb52337e7d3100caf2046e8
Davide Pavan <d.pavan@tecnoteca.com>
2016-02-25 10:02:54 +0100

    #1726: Relations graph on gui-framework - REST resources for domain trees
    Filter.

--- core/src/main/java/org/cmdbuild/logic/NavigationTreeLogic.java
+++ core/src/main/java/org/cmdbuild/logic/NavigationTreeLogic.java
@@ -4,13 +4,15 @@ import java.util.Map;
 
 import org.cmdbuild.model.domainTree.DomainTreeNode;
 
+import com.google.common.base.Predicate;
+
 public interface NavigationTreeLogic extends Logic {
 
 	void create(final String name, final String description, final boolean active, DomainTreeNode deserialize);
 
 	void save(final String name, final String description, final boolean active, DomainTreeNode deserialize);
 
-	Map<String, String> get();
+	Map<String, String> get(Predicate<DomainTreeNode> predicate);
 
 	DomainTreeNode getTree(final String name);
 
--- services/rest/v2/src/main/java/org/cmdbuild/service/rest/v2/cxf/CxfDomainTrees.java
+++ services/rest/v2/src/main/java/org/cmdbuild/service/rest/v2/cxf/CxfDomainTrees.java
@@ -1,7 +1,12 @@
 package org.cmdbuild.service.rest.v2.cxf;
 
+import static com.google.common.base.Predicates.alwaysTrue;
 import static com.google.common.collect.FluentIterable.from;
 import static com.google.common.collect.Lists.newArrayList;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.cmdbuild.service.rest.v2.constants.Serialization.DOMAIN;
+import static org.cmdbuild.service.rest.v2.constants.Serialization.RECURSION_ENABLED;
+import static org.cmdbuild.service.rest.v2.constants.Serialization.TARGET_CLASS;
 import static org.cmdbuild.service.rest.v2.model.Models.newMetadata;
 import static org.cmdbuild.service.rest.v2.model.Models.newNode;
 import static org.cmdbuild.service.rest.v2.model.Models.newResponseMultiple;
@@ -10,12 +15,19 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.cmdbuild.logic.NavigationTreeLogic;
+import org.cmdbuild.logic.data.access.filter.json.JsonParser;
+import org.cmdbuild.logic.data.access.filter.model.Element;
+import org.cmdbuild.logic.data.access.filter.model.Filter;
+import org.cmdbuild.logic.data.access.filter.model.Parser;
 import org.cmdbuild.model.domainTree.DomainTreeNode;
 import org.cmdbuild.service.rest.v2.DomainTrees;
+import org.cmdbuild.service.rest.v2.cxf.filter.DomainTreeNodeElementPredicate;
 import org.cmdbuild.service.rest.v2.model.Node;
 import org.cmdbuild.service.rest.v2.model.ResponseMultiple;
 
 import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 
 public class CxfDomainTrees implements DomainTrees {
 
@@ -29,7 +41,20 @@ public class CxfDomainTrees implements DomainTrees {
 
 	@Override
 	public ResponseMultiple<String> readAll(final String filter, final Integer limit, final Integer offset) {
-		final Collection<String> elements = logic.get().keySet();
+		final Predicate<DomainTreeNode> predicate;
+		if (isNotBlank(filter)) {
+			final Parser parser = new JsonParser(filter);
+			final Filter filterModel = parser.parse();
+			final Optional<Element> element = filterModel.attribute();
+			if (element.isPresent()) {
+				predicate = new DomainTreeNodeElementPredicate(element.get());
+			} else {
+				predicate = alwaysTrue();
+			}
+		} else {
+			predicate = alwaysTrue();
+		}
+		final Collection<String> elements = logic.get(predicate).keySet();
 		final int total = elements.size();
 		return newResponseMultiple(String.class) //
 				.withElements(elements) //
@@ -54,9 +79,9 @@ public class CxfDomainTrees implements DomainTrees {
 								return newNode() //
 										.withId(input.getId()) //
 										.withParent(input.getIdParent()) //
-										.withMetadata("domain", input.getDomainName()) //
-										.withMetadata("targetClass", input.getTargetClassName()) //
-										.withMetadata("recursionEnabled", input.isEnableRecursion()) //
+										.withMetadata(DOMAIN, input.getDomainName()) //
+										.withMetadata(TARGET_CLASS, input.getTargetClassName()) //
+										.withMetadata(RECURSION_ENABLED, input.isEnableRecursion()) //
 										.build();
 							}
 
