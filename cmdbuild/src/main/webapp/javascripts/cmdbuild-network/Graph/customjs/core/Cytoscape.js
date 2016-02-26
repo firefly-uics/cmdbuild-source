7947cf996fe9504559244a030025a53a9deea77f
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-02-26 17:22:25 +0100

    #1726: Relations graph on gui-framework - Cytoscape is slow on large graphs

--- cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/Cytoscape.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/Cytoscape.js
@@ -1,31 +1,32 @@
 (function($) {
 	$('#cy').cytoscape({
+		container: undefined,
+		zoomingEnabled: false,
+		userZoomingEnabled: false,
+		panningEnabled: false,
+		userPanningEnabled: false,
+		boxSelectionEnabled: false,
+		selectionType: 'single',
+		touchTapThreshold: 8,
+		desktopTapThreshold: 4,
+		autolock: false,
+		autoungrabify: false,
+		autounselectify: false,
+
+		// rendering options:
+		headless: true,
+		styleEnabled: false,
+		hideEdgesOnViewport: true,
+		hideLabelsOnViewport: true,
+		textureOnViewport: true,
+		motionBlur: false,
+		motionBlurOpacity: 0.2,
+		wheelSensitivity: 1,
+		pixelRatio: 'auto',
+
 		elements: {
 			nodes: [],
 			edges: [],
-			container: undefined,
-			zoomingEnabled: false,
-			userZoomingEnabled: false,
-			panningEnabled: false,
-			userPanningEnabled: false,
-			boxSelectionEnabled: false,
-			selectionType: 'single',
-			touchTapThreshold: 8,
-			desktopTapThreshold: 4,
-			autolock: false,
-			autoungrabify: false,
-			autounselectify: false,
-
-			// rendering options:
-			headless: true,
-			styleEnabled: false,
-			hideEdgesOnViewport: true,
-			hideLabelsOnViewport: true,
-			textureOnViewport: true,
-			motionBlur: false,
-			motionBlurOpacity: 0.2,
-			wheelSensitivity: 1,
-			pixelRatio: 'auto',
 		},
 
 		ready: function() {
