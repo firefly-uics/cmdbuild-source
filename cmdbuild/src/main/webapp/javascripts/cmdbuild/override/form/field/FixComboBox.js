a92a8ff42de492bfc11d0af607b89c76700d2d30
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-02-26 10:43:38 +0100

    Merge with 2.3
    Various fixes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/override/form/field/FixComboBox.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/override/form/field/FixComboBox.js
@@ -7,12 +7,14 @@
 		 * To fix problem that don't set combo value if forceSelection is true - 21/05/2014
 		 */
 		setValue: function(value, doSelect) {
+			var comboboxField = this;
 			var forceSelectionState = this.forceSelection;
 
 			if (forceSelectionState)
 				this.forceSelection = false;
 
-			var comboboxField = this.callParent(arguments);
+			if (!Ext.isEmpty(this.getStore()))
+				comboboxField = this.callParent(arguments);
 
 			if (forceSelectionState)
 				this.forceSelection = true;
