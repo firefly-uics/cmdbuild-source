5e1672f078b1883df18c876b33a4964b4385d876
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-03-01 15:46:13 +0100

    #1726: Relations graph on gui-framework - Code cleaning

--- cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/constants.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild-network/Graph/customjs/core/constants.js
@@ -3,7 +3,6 @@
 		GUICOMPOUNDNODE : "GUICOMPOUNDNODE",
 		DEFAULT_SHAPE : "cmdbuildDefaultShape",
 		SELECTION_SHAPE : "cmdbuildSelectionShape",
-		RELATION_SHAPE : "cmdbuildRelationShape",
 		CONFIGURATION_FILE : "configurations/configuration.json",
 		SPRITES_PATH : "sprites/",
 		LABELS_ON_SELECTED : "selected",
