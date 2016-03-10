98ce7815fd73c1db92a4cb1ca5ded35e23c8fde3
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-03-10 17:33:40 +0100

    #1969: Add attachments on closed activities

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/model/configuration/workflow/Workflow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/model/configuration/workflow/Workflow.js
@@ -9,6 +9,8 @@
 		extend: 'Ext.data.Model',
 
 		fields: [
+			{ name: CMDBuild.core.constants.Proxy.DISABLE_SYNCHRONIZATION_OF_MISSING_VARIABLES, type: 'boolean' },
+			{ name: CMDBuild.core.constants.Proxy.ENABLE_ADD_ATTACHMENT_ON_CLOSED_ACTIVITIES, type: 'boolean' },
 			{ name: CMDBuild.core.constants.Proxy.ENABLED, type: 'boolean', defaultValue: false },
 			{ name: CMDBuild.core.constants.Proxy.PASSWORD, type: 'string' },
 			{ name: CMDBuild.core.constants.Proxy.URL, type: 'string' },
