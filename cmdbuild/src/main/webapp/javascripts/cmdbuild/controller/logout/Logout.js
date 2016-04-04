53e0fd53c765566b54e5f4f02e6f6dd3a026e468
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-04-04 14:32:33 +0200

    #2086: [Fix] REST logout error with header autenticator

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/logout/Logout.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/logout/Logout.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
 	Ext.define('CMDBuild.controller.logout.Logout', {
 		extend: 'CMDBuild.controller.common.abstract.Base',
@@ -11,33 +11,50 @@
 
 		/**
 		 * @param {Object} configurationObject
+		 *
+		 * @returns {Void}
+		 *
+		 * @override
 		 */
 		constructor: function (configurationObject) {
 			this.callParent(arguments);
 
-			this.doLogout();
+			if (!Ext.isEmpty(Ext.util.Cookies.get(CMDBuild.core.constants.Proxy.SESSION_TOKEN))) {
+				this.doRestLogout();
+			} else {
+				this.doJsonRpcLogout();
+			}
 		},
 
 		/**
+		 * @returns {Void}
+		 *
 		 * @private
 		 */
-		doLogout: function () {
+		doJsonRpcLogout: function () {
+			CMDBuild.core.proxy.session.JsonRpc.logout({
+				scope: this,
+				callback: function (options, success, response) {
+					Ext.util.Cookies.clear(CMDBuild.core.constants.Proxy.SESSION_TOKEN);
+
+					window.location = 'index.jsp';
+				}
+			});
+		},
+
+		/**
+		 * @returns {Void}
+		 *
+		 * @private
+		 */
+		doRestLogout: function () {
 			var urlParams = {};
 			urlParams[CMDBuild.core.constants.Proxy.TOKEN] = Ext.util.Cookies.get(CMDBuild.core.constants.Proxy.SESSION_TOKEN);
 
 			CMDBuild.core.proxy.session.Rest.logout({
 				urlParams: urlParams,
 				scope: this,
-				callback: function (options, success, response) {
-					CMDBuild.core.proxy.session.JsonRpc.logout({
-						scope: this,
-						callback: function (options, success, response) {
-							Ext.util.Cookies.clear(CMDBuild.core.constants.Proxy.SESSION_TOKEN);
-
-							window.location = 'index.jsp';
-						}
-					});
-				}
+				callback: this.doJsonRpcLogout
 			});
 		}
 	});
