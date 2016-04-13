43476dbf7d8213742640d9e5744200264af97ad1
Davide Pavan <d.pavan@tecnoteca.com>
2016-04-13 09:04:34 +0200

    #2089: Single authentication system used by all web service - Session and user management

--- core/src/main/java/org/cmdbuild/data/store/session/Session.java
+++ core/src/main/java/org/cmdbuild/data/store/session/Session.java
@@ -1,7 +1,12 @@
 package org.cmdbuild.data.store.session;
 
+import org.cmdbuild.auth.user.OperationUser;
 import org.cmdbuild.data.store.Storable;
 
-public interface Session extends Storable{
+public interface Session extends Storable {
+
+	OperationUser getUser();
+
+	OperationUser getImpersonated();
 
 }
