e40beafb115e8713da2038c18151cb52ae3842da
Davide Pavan <d.pavan@tecnoteca.com>
2016-05-09 13:05:58 +0200

    #2089: Single authentication system used by all web service - Session and user management

---
+++ core/src/main/java/org/cmdbuild/logic/workflow/UserWorkflowLogicBuilder.java
@@ -0,0 +1,26 @@
+package org.cmdbuild.logic.workflow;
+
+import org.apache.commons.lang3.builder.Builder;
+import org.cmdbuild.auth.acl.PrivilegeContext;
+import org.cmdbuild.auth.user.OperationUser;
+import org.cmdbuild.config.WorkflowConfiguration;
+import org.cmdbuild.dao.view.CMDataView;
+import org.cmdbuild.logic.data.LockLogic;
+import org.cmdbuild.services.FilesStore;
+import org.cmdbuild.workflow.DefaultWorkflowEngine;
+
+public class UserWorkflowLogicBuilder extends WorkflowLogicBuilder {
+
+	public UserWorkflowLogicBuilder( //
+			final OperationUser operationUser, //
+			final PrivilegeContext privilegeContext, //
+			final Builder<DefaultWorkflowEngine> workflowEngineBuilder, //
+			final CMDataView dataView, //
+			final WorkflowConfiguration configuration, //
+			final FilesStore filesStore, //
+			final LockLogic lockLogic //
+	) {
+		super(operationUser, privilegeContext, workflowEngineBuilder, dataView, configuration, filesStore, lockLogic);
+	}
+
+}
