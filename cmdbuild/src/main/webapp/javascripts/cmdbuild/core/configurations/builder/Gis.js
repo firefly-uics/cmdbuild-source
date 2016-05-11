e2b7c4feda017a1eb9d3f4a6bed810cb7625d65e
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-05-11 12:33:35 +0200

    #2089: Single authentication system used by all web service - Client proxy refactor
    Fixes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/core/configurations/builder/Gis.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/core/configurations/builder/Gis.js
@@ -36,9 +36,9 @@
 				loadMask: false,
 				scope: this,
 				success: function (response, options, decodedResponse) {
-					var gisConfigurationObject = decodedResponse[CMDBuild.core.constants.Proxy.DATA];
+					var gisConfigurationObject = decodedResponse[CMDBuild.core.constants.Proxy.DATA] || {};
 
-					if (!Ext.isEmpty(gisConfigurationObject[CMDBuild.core.constants.Proxy.DATA]))
+					if (!Ext.isEmpty(gisConfigurationObject) && !Ext.isEmpty(gisConfigurationObject[CMDBuild.core.constants.Proxy.DATA]))
 						gisConfigurationObject = gisConfigurationObject[CMDBuild.core.constants.Proxy.DATA];
 
 					CMDBuild.proxy.core.configurations.builder.Gis.readTreeNavigation({
