3e16d083c1624d8939e7f26af20d4531177e5466
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-05-19 11:14:11 +0200

    Translation fixes

--- cmdbuild/src/main/webapp/javascripts/ext-4.2.0/locale/ext-lang-pt_BR.js
+++ cmdbuild/src/main/webapp/javascripts/ext-4.2.0/locale/ext-lang-pt_BR.js
@@ -1,23 +1,23 @@
-/*
-This file is part of Ext JS 4.2
-
-Copyright (c) 2011-2013 Sencha Inc
-
-Contact:  http://www.sencha.com/contact
-
-GNU General Public License Usage
-This file may be used under the terms of the GNU General Public License version 3.0 as
-published by the Free Software Foundation and appearing in the file LICENSE included in the
-packaging of this file.
-
-Please review the following information to ensure the GNU General Public License version 3.0
-requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-
-If you are unsure which license is appropriate for your use, please contact the sales department
-at http://www.sencha.com/contact.
-
-Build date: 2013-03-11 22:33:40 (aed16176e68b5e8aa1433452b12805c0ad913836)
-*/
+/**
+ * This file is part of Ext JS 4.2
+ *
+ * Copyright (c) 2011-2013 Sencha Inc
+ *
+ * Contact:  http://www.sencha.com/contact
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public License version 3.0 as
+ * published by the Free Software Foundation and appearing in the file LICENSE included in the
+ * packaging of this file.
+ *
+ * Please review the following information to ensure the GNU General Public License version 3.0
+ * requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+ *
+ * If you are unsure which license is appropriate for your use, please contact the sales department
+ * at http://www.sencha.com/contact.
+ *
+ * Build date: 2013-03-11 22:33:40 (aed16176e68b5e8aa1433452b12805c0ad913836)
+ */
 /**
  * Portuguese/Brazil Translation by Weber Souza
  * 08 April 2007
@@ -289,7 +289,7 @@ Ext.define("Ext.locale.pt_BR.grid.PropertyColumnModel", {
 });
 
 // This is needed until we can refactor all of the locales into individual files
-Ext.define("Ext.locale.pt_BR.Component", {	
+Ext.define("Ext.locale.pt_BR.Component", {
     override: "Ext.Component"
 });
 
