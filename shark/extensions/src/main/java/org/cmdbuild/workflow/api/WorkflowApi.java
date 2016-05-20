91bfcd18d397c17d8d3ddf35c5db3f71e867be13
Davide Pavan <d.pavan@tecnoteca.com>
2016-05-20 15:29:38 +0200

    #2174: Impersonate another user and/or group using workflow API

--- shark/extensions/src/main/java/org/cmdbuild/workflow/api/WorkflowApi.java
+++ shark/extensions/src/main/java/org/cmdbuild/workflow/api/WorkflowApi.java
@@ -1,177 +1,26 @@
 package org.cmdbuild.workflow.api;
 
-import static java.lang.String.format;
-
 import org.cmdbuild.api.fluent.Card;
 import org.cmdbuild.api.fluent.CardDescriptor;
 import org.cmdbuild.api.fluent.FluentApi;
-import org.cmdbuild.api.fluent.FluentApiExecutor;
-import org.cmdbuild.api.fluent.ws.EntryTypeAttribute;
-import org.cmdbuild.common.Constants;
-import org.cmdbuild.common.api.mail.FetchedMail;
 import org.cmdbuild.common.api.mail.MailApi;
-import org.cmdbuild.common.api.mail.NewMailQueue;
-import org.cmdbuild.common.api.mail.SelectFolder;
-import org.cmdbuild.common.api.mail.SelectMail;
-import org.cmdbuild.common.api.mail.SendableNewMail;
 import org.cmdbuild.services.soap.Private;
-import org.cmdbuild.workflow.type.LookupType;
 import org.cmdbuild.workflow.type.ReferenceType;
 
-public class WorkflowApi extends FluentApi implements SchemaApi, MailApi {
+public interface WorkflowApi extends FluentApi, SchemaApi, MailApi {
 
-	@SuppressWarnings("serial")
-	private static class ClassNotFound extends RuntimeException {
+	Impersonate impersonate();
 
-		public ClassNotFound(final int id) {
-			super(format("class '%d' not found", id));
-		}
+	Private soap();
 
-	}
+	ReferenceType referenceTypeFrom(Card card);
 
-	private final Private proxy;
-	private final SchemaApi schemaApi;
-	private final MailApi mailApi;
+	ReferenceType referenceTypeFrom(CardDescriptor cardDescriptor);
 
-	/**
-	 * It's really ugly but fortunately all is hidden behind the
-	 * {@link SharkWorkflowApiFactory}.
-	 */
-	public WorkflowApi(final FluentApiExecutor executor, final Private proxy, final SchemaApi schemaApi,
-			final MailApi mailApi) {
-		super(executor);
-		this.proxy = proxy;
-		this.schemaApi = schemaApi;
-		this.mailApi = mailApi;
-	}
+	ReferenceType referenceTypeFrom(Object idAsObject);
 
-	public Private soap() {
-		return proxy;
-	}
+	CardDescriptor cardDescriptorFrom(ReferenceType referenceType);
 
-	/*
-	 * Schema
-	 */
-
-	@Override
-	public ClassInfo findClass(final String className) {
-		return schemaApi.findClass(className);
-	}
-
-	@Override
-	public ClassInfo findClass(final int classId) {
-		return schemaApi.findClass(classId);
-	}
-
-	@Override
-	public AttributeInfo findAttributeFor(final EntryTypeAttribute entryTypeAttribute) {
-		return schemaApi.findAttributeFor(entryTypeAttribute);
-	}
-
-	@Override
-	public LookupType selectLookupById(final int id) {
-		return (id <= 0) ? new LookupType() : schemaApi.selectLookupById(id);
-	}
-
-	@Override
-	public LookupType selectLookupByCode(final String type, final String code) {
-		return schemaApi.selectLookupByCode(type, code);
-	}
-
-	@Override
-	public LookupType selectLookupByDescription(final String type, final String description) {
-		return schemaApi.selectLookupByDescription(type, description);
-	}
-
-	/*
-	 * Mail
-	 */
-
-	@Override
-	public SendableNewMail newMail() {
-		return mailApi.newMail();
-	}
-
-	@Override
-	public NewMailQueue newMailQueue() {
-		return mailApi.newMailQueue();
-	}
-
-	@Override
-	public SelectFolder selectFolder(final String folder) {
-		return mailApi.selectFolder(folder);
-	}
-
-	@Override
-	public SelectMail selectMail(final FetchedMail mail) {
-		return mailApi.selectMail(mail);
-	}
-
-	/*
-	 * Data type conversion
-	 */
-
-	public ReferenceType referenceTypeFrom(final Card card) {
-		return referenceTypeFrom(card, card.getDescription());
-	}
-
-	public ReferenceType referenceTypeFrom(final CardDescriptor cardDescriptor) {
-		return referenceTypeFrom(cardDescriptor, null);
-	}
-
-	public ReferenceType referenceTypeFrom(final Object idAsObject) {
-		final int id = objectToInt(idAsObject);
-		return (id <= 0) ? new ReferenceType() : referenceTypeFrom( //
-				existingCard(Constants.BASE_CLASS_NAME, id) //
-						.limitAttributes(Constants.DESCRIPTION_ATTRIBUTE) //
-						.fetch());
-	}
-
-	private int objectToInt(final Object id) {
-		final int idAsInt;
-		if (id instanceof String) {
-			idAsInt = Integer.parseInt(String.class.cast(id));
-		} else if (id instanceof Number) {
-			idAsInt = Number.class.cast(id).intValue();
-		} else {
-			throw new IllegalArgumentException(format("invalid class '%s' for id", id.getClass()));
-		}
-		return idAsInt;
-	}
-
-	private ReferenceType referenceTypeFrom(final CardDescriptor cardDescriptor, final String description) {
-		return new ReferenceType( //
-				cardDescriptor.getId(), //
-				findClass(cardDescriptor.getClassName()).getId(), //
-				(description == null) ? descriptionFor(cardDescriptor) : description);
-	}
-
-	private String descriptionFor(final CardDescriptor cardDescriptor) {
-		return existingCard(cardDescriptor) //
-				.limitAttributes(Constants.DESCRIPTION_ATTRIBUTE) //
-				.fetch() //
-				.getDescription();
-	}
-
-	public CardDescriptor cardDescriptorFrom(final ReferenceType referenceType) {
-		final ClassInfo classInfo = findClass(referenceType.getIdClass());
-		final ClassInfo _classInfo;
-		if (classInfo == null) {
-			final ReferenceType fallbackReferenceType = referenceTypeFrom(referenceType.getId());
-			_classInfo = findClass(fallbackReferenceType.getIdClass());
-			if (_classInfo == null) {
-				throw new ClassNotFound(referenceType.getIdClass());
-			}
-		} else {
-			_classInfo = classInfo;
-		}
-		return new CardDescriptor( //
-				_classInfo.getName(), //
-				referenceType.getId());
-	}
-
-	public Card cardFrom(final ReferenceType referenceType) {
-		return existingCard(cardDescriptorFrom(referenceType)).fetch();
-	}
+	Card cardFrom(ReferenceType referenceType);
 
 }
--- shark/extensions/src/test/java/unit/WorkflowApiImplTest.java
+++ shark/extensions/src/test/java/unit/WorkflowApiImplTest.java
@@ -1,5 +1,6 @@
 package unit;
 
+import static org.cmdbuild.workflow.api.WorkflowApiImpl.context;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
@@ -8,8 +9,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.cmdbuild.api.fluent.Card;
 import org.cmdbuild.api.fluent.CardDescriptor;
+import org.cmdbuild.api.fluent.ExecutorBasedFluentApi;
 import org.cmdbuild.api.fluent.ExistingCard;
 import org.cmdbuild.api.fluent.FluentApi;
 import org.cmdbuild.api.fluent.FluentApiExecutor;
@@ -18,12 +22,14 @@ import org.cmdbuild.services.soap.Private;
 import org.cmdbuild.workflow.api.SchemaApi;
 import org.cmdbuild.workflow.api.SchemaApi.ClassInfo;
 import org.cmdbuild.workflow.api.WorkflowApi;
+import org.cmdbuild.workflow.api.WorkflowApiImpl;
+import org.cmdbuild.workflow.api.WorkflowApiImpl.Context;
 import org.cmdbuild.workflow.type.ReferenceType;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
-public class WorkflowApiTest {
+public class WorkflowApiImplTest {
 
 	private static final String CLASS_NAME = "className";
 	private static final int ID_CLASS = CLASS_NAME.hashCode();
@@ -45,15 +51,121 @@ public class WorkflowApiTest {
 		schemaApi = mock(SchemaApi.class);
 		mailApi = mock(MailApi.class);
 
-		fluentApi = new FluentApi(fluentApiExecutor);
-		workflowApi = new WorkflowApi(fluentApiExecutor, proxy, schemaApi, mailApi);
+		fluentApi = new ExecutorBasedFluentApi(fluentApiExecutor);
+		workflowApi = new WorkflowApiImpl(context(fluentApi, proxy, schemaApi, mailApi));
+	}
+
+	@Test
+	public void callbackInvokedOnCreation() throws Exception {
+		// given
+		final AtomicReference<WorkflowApiImpl> reference = new AtomicReference<>();
+		final Context context = new Context() {
+
+			@Override
+			public FluentApi fluentApi() {
+				return fluentApi;
+			}
+
+			@Override
+			public Private proxy() {
+				return proxy;
+			}
+
+			@Override
+			public SchemaApi schemaApi() {
+				return schemaApi;
+			}
+
+			@Override
+			public MailApi mailApi() {
+				return mailApi;
+			}
+
+			@Override
+			public void callback(final WorkflowApiImpl object) {
+				reference.set(object);
+			}
+
+			@Override
+			public Context impersonate(final String username, final String group) {
+				throw new UnsupportedOperationException();
+			}
+
+		};
+
+		// then
+		assertThat(reference.get(), equalTo(null));
+
+		// when
+		workflowApi = new WorkflowApiImpl(context);
+
+		// then
+		assertThat(reference.get(), equalTo(workflowApi));
+	}
+
+	@Test
+	public void impersonateInvoked() throws Exception {
+		// given
+		final AtomicReference<String> _username = new AtomicReference<>();
+		final AtomicReference<String> _group = new AtomicReference<>();
+		final Context context = new Context() {
+
+			@Override
+			public FluentApi fluentApi() {
+				return fluentApi;
+			}
+
+			@Override
+			public Private proxy() {
+				return proxy;
+			}
+
+			@Override
+			public SchemaApi schemaApi() {
+				return schemaApi;
+			}
+
+			@Override
+			public MailApi mailApi() {
+				return mailApi;
+			}
+
+			@Override
+			public void callback(final WorkflowApiImpl object) {
+				// nothing to do
+			}
+
+			@Override
+			public Context impersonate(final String username, final String group) {
+				_username.set(username);
+				_group.set(group);
+				// not important
+				return this;
+			}
+
+		};
+
+		// then
+		assertThat(_username.get(), equalTo(null));
+		assertThat(_group.get(), equalTo(null));
+
+		// when
+		workflowApi = new WorkflowApiImpl(context) //
+				.impersonate() //
+				.username("foo") //
+				.group("bar") //
+				.impersonate();
+
+		// then
+		assertThat(_username.get(), equalTo("foo"));
+		assertThat(_group.get(), equalTo("bar"));
 	}
 
 	@Test
 	public void findClassByNameAndExistingCardWhenConvertingFromCardDescriptorToReferenceType() throws Exception {
 		when(schemaApi.findClass(CLASS_NAME)).thenReturn(newClassInfo(CLASS_NAME, ID_CLASS));
-		when(fluentApiExecutor.fetch(any(ExistingCard.class))).thenReturn(
-				cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
+		when(fluentApiExecutor.fetch(any(ExistingCard.class)))
+				.thenReturn(cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
 
 		final CardDescriptor cardDescriptor = new CardDescriptor(CLASS_NAME, CARD_ID);
 		final ReferenceType referenceType = workflowApi.referenceTypeFrom(cardDescriptor);
@@ -69,10 +181,11 @@ public class WorkflowApiTest {
 	}
 
 	@Test
-	public void findClassByNameAndExistingCardWhenConvertingFromCardWithNoDescriptionToReferenceType() throws Exception {
+	public void findClassByNameAndExistingCardWhenConvertingFromCardWithNoDescriptionToReferenceType()
+			throws Exception {
 		when(schemaApi.findClass(CLASS_NAME)).thenReturn(newClassInfo(CLASS_NAME, ID_CLASS));
-		when(fluentApiExecutor.fetch(any(ExistingCard.class))).thenReturn(
-				cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
+		when(fluentApiExecutor.fetch(any(ExistingCard.class)))
+				.thenReturn(cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
 
 		final Card card = cardWithNoDescription(CLASS_NAME, CARD_ID);
 		final ReferenceType referenceType = workflowApi.referenceTypeFrom(card);
@@ -105,8 +218,8 @@ public class WorkflowApiTest {
 
 	@Test
 	public void fetchCardFromBaseClassWhenConvertingFromIdToReferenceType() throws Exception {
-		when(fluentApiExecutor.fetch(any(ExistingCard.class))).thenReturn(
-				cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
+		when(fluentApiExecutor.fetch(any(ExistingCard.class)))
+				.thenReturn(cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
 		when(schemaApi.findClass(CLASS_NAME)).thenReturn(newClassInfo(CLASS_NAME, ID_CLASS));
 
 		final ReferenceType referenceType = workflowApi.referenceTypeFrom(CARD_ID);
@@ -140,8 +253,8 @@ public class WorkflowApiTest {
 	@Test
 	public void findClassByIdAndExistingCardWhenConvertingFromReferenceTypeToCard() throws Exception {
 		when(schemaApi.findClass(ID_CLASS)).thenReturn(newClassInfo(CLASS_NAME, ID_CLASS));
-		when(fluentApiExecutor.fetch(any(ExistingCard.class))).thenReturn(
-				cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
+		when(fluentApiExecutor.fetch(any(ExistingCard.class)))
+				.thenReturn(cardWithDescription(CLASS_NAME, CARD_ID, DESCRIPTION));
 
 		final ReferenceType referenceType = new ReferenceType(CARD_ID, ID_CLASS, DESCRIPTION);
 		final Card card = workflowApi.cardFrom(referenceType);
