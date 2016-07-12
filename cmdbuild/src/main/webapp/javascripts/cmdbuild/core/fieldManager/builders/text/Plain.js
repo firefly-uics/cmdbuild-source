95128ec474eb298e93da7b782390a5a1531c5167
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-07-12 10:05:03 +0200

    #2208: [Fix] Widget CustomForm: readOnly attributes view mode format doesn't follow standards

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/core/fieldManager/builders/text/Plain.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/core/fieldManager/builders/text/Plain.js
@@ -14,56 +14,72 @@
 		parentDelegate: undefined,
 
 		/**
-		 * @param {Boolean} withEditor
+		 * @param {Object} parameters
 		 *
 		 * @returns {Ext.grid.column.Column or Object}
 		 */
-		buildColumn: function(withEditor) {
-			withEditor = Ext.isBoolean(withEditor) ? withEditor : false;
-
-			return this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN) ? {} : Ext.create('Ext.grid.column.Column', {
-				dataIndex: this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.NAME),
-				disabled: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
-				editor: withEditor ? this.buildEditor() : null,
+		buildColumn: function (parameters) {
+			return this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN) ? {} : Ext.create('Ext.grid.column.Column', {
+				dataIndex: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME),
+				disabled: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
+				editor: parameters.withEditor ? this.buildEditor() : null,
 				flex: 1,
-				hidden: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.SHOW_COLUMN),
+				hidden: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.SHOW_COLUMN),
 				renderer: this.rendererColumn,
 				scope: this,
 				sortable: true,
-				text: this.applyMandatoryLabelFlag(this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION))
+				text: this.applyMandatoryLabelFlag(this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION))
 			});
 		},
 
 		/**
 		 * @returns {Object}
 		 */
-		buildEditor: function() {
-			return this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN) ? {} : {
+		buildEditor: function () {
+			return this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN) ? {} : {
 				xtype: 'textfield',
-				allowBlank: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
-				disabled: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
-				name: this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.NAME),
-				readOnly: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE)
+				allowBlank: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
+				disabled: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
+				name: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME),
+				readOnly: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE)
 			};
 		},
 
 		/**
 		 * @returns {Ext.form.field.TextArea}
 		 */
-		buildField: function() {
+		buildField: function () {
 			return Ext.create('Ext.form.field.TextArea', {
-				allowBlank: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
-				disabled: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
+				allowBlank: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
+				disabled: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
 				fieldLabel: this.applyMandatoryLabelFlag(
-					this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION)
-					|| this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.NAME)
+					this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION)
+					|| this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME)
 				),
-				hidden: this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN),
+				hidden: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN),
 				labelAlign: 'right',
 				labelWidth: CMDBuild.core.constants.FieldWidths.LABEL,
 				maxWidth: CMDBuild.core.constants.FieldWidths.STANDARD_BIG,
-				name: this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.NAME),
-				readOnly: !this.cmfg('attributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE)
+				name: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME),
+				readOnly: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE)
+			});
+		},
+
+		/**
+		 * @returns {CMDBuild.view.common.field.display.Text}
+		 */
+		buildFieldReadOnly: function () {
+			return Ext.create('CMDBuild.view.common.field.display.Text', {
+				allowBlank: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
+				fieldLabel: this.applyMandatoryLabelFlag(
+					this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION)
+					|| this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME)
+				),
+				hidden: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN),
+				labelAlign: 'right',
+				labelWidth: CMDBuild.core.constants.FieldWidths.LABEL,
+				maxWidth: CMDBuild.core.constants.FieldWidths.STANDARD_BIG,
+				name: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME)
 			});
 		}
 	});
