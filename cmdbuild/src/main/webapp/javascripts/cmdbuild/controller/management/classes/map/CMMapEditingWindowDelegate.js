4c75807f7c263372d4bf886a6e763e9d6e137ac4
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-07-21 17:26:05 +0200

    #2209: Gis Refactoring - Editing the current card

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/classes/map/CMMapEditingWindowDelegate.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/classes/map/CMMapEditingWindowDelegate.js
@@ -1,43 +1,26 @@
 (function() {
-	Ext
-			.define(
-					"CMDBuild.controller.management.classes.map.CMMapEditingWindowDelegate",
-					{
-						extend : "CMDBuild.view.management.map.CMMapEditingToolsWindowDelegate",
-						currentGisShape : undefined,
-						constructor : function(master, interactionDocument) {
-							this.master = master;
-							this.interactionDocument = interactionDocument;
-							this.callParent(arguments);
-						},
+	Ext.define("CMDBuild.controller.management.classes.map.CMMapEditingWindowDelegate", {
+		extend : "CMDBuild.view.management.map.CMMapEditingToolsWindowDelegate",
+		currentGisShape : undefined,
+		constructor : function(master, interactionDocument) {
+			this.master = master;
+			this.interactionDocument = interactionDocument;
+			this.callParent(arguments);
+		},
 
-//						buildEditControls : function buildEditControls(layer,
-//								type) {
-//						},
-//
-//						destroyEditControls : function destroyEditControls(
-//								layer) {
-//						},
-//
-						addFeatureButtonHasBeenToggled : function onAddFeatureButtonToggle(
-								toggled) {
-							this.interactionDocument.feature.operation = "Draw";
-							this.interactionDocument.changedFeature();
-						},
+		addFeatureButtonHasBeenToggled : function onAddFeatureButtonToggle(toggled) {
+			this.interactionDocument.feature.operation = "Draw";
+			this.interactionDocument.changedFeature();
+		},
 
-						removeFeatureButtonHasBeenClicked : function onRemoveFeatureButtonClick() {
-						},
+		removeFeatureButtonHasBeenClicked : function onRemoveFeatureButtonClick() {
+		},
 
-						geoAttributeMenuItemHasBeenClicked : function(item) {
-							this.interactionDocument.setCurrentFeature(item.name, item.geoType, 'Modify');
-							this.interactionDocument.changedFeature();
-						},
+		geoAttributeMenuItemHasBeenClicked : function(item) {
+			this.interactionDocument.setCurrentFeature(item.name, item.geoType, 'Modify');
+			this.interactionDocument.changedFeature();
+		},
 
-//						activateTransformConrol : function(layerName) {
-//						},
-//
-//						deactivateEditControls : function deactivateEditControls() {
-//						}
-					});
+	});
 
 })();
--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/classes/map/CMMap.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/classes/map/CMMap.js
@@ -1,13 +1,13 @@
-(function () {
+(function() {
 	Ext.define('CMDBuild.Management.CMMap', {
-		extend: 'CMDBuild.view.management.classes.map.geoextension.Map',
-		requires: ['CMDBuild.view.management.classes.map.geoextension.Map'],
-		
+		extend : 'CMDBuild.view.management.classes.map.geoextension.Map',
+		requires : [ 'CMDBuild.view.management.classes.map.geoextension.Map' ],
+
 		interactionDocument : undefined,
 
 		/**
 		 * @returns {Void}
-		 *
+		 * 
 		 * @override
 		 */
 		configure : function() {
@@ -16,23 +16,17 @@
 		},
 
 		refresh : function() {
-			var currentClassId = (Ext.isEmpty(_CMCardModuleState.entryType)) ?
-					undefined : _CMCardModuleState.entryType.getId();
-			if (! currentClassId) {
-				return;
-			}
-			var currentClassName = (Ext.isEmpty(_CMCardModuleState.entryType)) ?
-					undefined : _CMCardModuleState.entryType.getName();
-			var currentCardId = (Ext.isEmpty(_CMCardModuleState.card)) ?
-					undefined : _CMCardModuleState.card.raw.Id;//getId();
+			var card = this.interactionDocument.getCurrentCard();
+			var currentClassName = card.className;
+			var currentCardId = card.cardId;
 			var me = this;
 			this.center(this.interactionDocument.getConfigurationMap());
 			this.interactionDocument.getAllLayers(function(layers) {
-				me.refreshAllLayers(layers, currentClassId, currentClassName, currentCardId);
+				me.refreshAllLayers(layers, currentClassName, currentCardId);
 			}, this);
 		},
-		refreshAllLayers : function(layers, currentClassId, currentClassName, currentCardId) {
-			if (! currentCardId) {
+		refreshAllLayers : function(layers, currentClassName, currentCardId) {
+			if (!currentCardId) {
 				return;
 			}
 			var visibleLayers = [];
@@ -40,16 +34,19 @@
 				var layer = layers[i];
 				var visible = this.interactionDocument.isVisible(layer, currentClassName, currentCardId);
 				var hide = this.interactionDocument.isHide(layer);
-				if (! hide && visible) {
+				if (!visible || hide) {
+					var pippo = 1;
+				}
+				if (!hide && visible) {
 					var geoAttribute = {
-							description : layer.description,	
-							masterTableName : "_Geoserver",	
-							name : layer.name,
-							type : layer.type
+						description : layer.description,
+						masterTableName : "_Geoserver",
+						name : layer.name,
+						type : layer.type
 					};
 					var geoLayer = this.getLayerByName(layer.name);
-					if (! geoLayer) {
-						geoLayer = this.makeLayer(currentClassId, geoAttribute, true);
+					if (!geoLayer) {
+						geoLayer = this.makeLayer(geoAttribute, true);
 					}
 					var adapter = geoLayer.get("adapter");
 					if (adapter && adapter.refresh) {
@@ -57,12 +54,12 @@
 					}
 					visibleLayers.push(layer);
 				}
-			}	
+			}
 			this.removeNotVisibleLayers(layers, visibleLayers)
 		},
-		removeNotVisibleLayers: function(all, visibles) {
+		removeNotVisibleLayers : function(all, visibles) {
 			for (var i = 0; i < all.length; i++) {
-				if (! visibles.find(function(l) {
+				if (!visibles.find(function(l) {
 					return l.name === all[i].name;
 				})) {
 					if (this.getLayerByName(all[i].name)) {
@@ -70,7 +67,6 @@
 					}
 				}
 			}
-		}
+		},
 	});
 })();
-						
