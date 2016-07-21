23546e4315ef742626aa090049d84a09b92b6433
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-07-21 17:29:49 +0200

    #2209: Gis Refactoring - Cleaning code

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/classes/map/geoextension/Layer.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/classes/map/geoextension/Layer.js
@@ -5,9 +5,9 @@
 	var DEFAULT_MIN_ZOOM = 0;
 	var DEFAULT_MAX_ZOOM = 25;
 	Ext.define('CMDBuild.view.management.classes.map.geoextension.Layer', {
-		constructor : function(classId, geoAttribute, withEditWindow, interactionDocument) {
+		constructor : function(geoAttribute, withEditWindow, interactionDocument) {
 			this.interactionDocument = interactionDocument;
-			this.layer = oPrivate.buildGeoserverLayer(geoAttribute);
+			this.layer = buildGeoserverLayer(geoAttribute);
 			this.layer.set("name",  geoAttribute.name);
 			this.callParent(arguments);
 		},
@@ -18,38 +18,36 @@
 			return this.layer.getSource();
 		}
 	});
-	var oPrivate = {
-			buildGeoserverLayer : function(geoAttribute) {
-				var geoserver_ws = CMDBuild.configuration.gis.get([
-						CMDBuild.core.constants.Proxy.GEO_SERVER, 'workspace' ]); 
-				var geoserver_url = CMDBuild.configuration.gis.get([
-						CMDBuild.core.constants.Proxy.GEO_SERVER, 'url' ]); 
-				var source = new ol.source.TileWMS({
-					url : geoserver_url + "/" + GOESERVER_SERVICE_TYPE,
-					params : {
-						layers : geoserver_ws + ":" + geoAttribute.name,
-						format : WMS_IMAGE_FORMAT,
-						transparent : true
-					}
-				});
-					
-				var layer = new ol.layer.Tile({
-					title : geoAttribute.description,
-					source : source
-				});
-				layer.cmdb_minZoom = geoAttribute.minZoom || DEFAULT_MIN_ZOOM;
-				layer.cmdb_maxZoom = geoAttribute.maxZoom || DEFAULT_MAX_ZOOM;
-				layer.geoAttribute = geoAttribute;
-				layer.editLayer = undefined;
-				layer.strategies = [new OpenLayers.Strategy.BBOX()];
-				layer.cmdb_index = geoAttribute.cmdb_index;
-				layer.set("name",  geoAttribute.name);
+	function buildGeoserverLayer(geoAttribute) {
+			var geoserver_ws = CMDBuild.configuration.gis.get([
+					CMDBuild.core.constants.Proxy.GEO_SERVER, 'workspace' ]); 
+			var geoserver_url = CMDBuild.configuration.gis.get([
+					CMDBuild.core.constants.Proxy.GEO_SERVER, 'url' ]); 
+			var source = new ol.source.TileWMS({
+				url : geoserver_url + "/" + GOESERVER_SERVICE_TYPE,
+				params : {
+					layers : geoserver_ws + ":" + geoAttribute.name,
+					format : WMS_IMAGE_FORMAT,
+					transparent : true
+				}
+			});
+				
+			var layer = new ol.layer.Tile({
+				title : geoAttribute.description,
+				source : source
+			});
+			layer.cmdb_minZoom = geoAttribute.minZoom || DEFAULT_MIN_ZOOM;
+			layer.cmdb_maxZoom = geoAttribute.maxZoom || DEFAULT_MAX_ZOOM;
+			layer.geoAttribute = geoAttribute;
+			layer.editLayer = undefined;
+			layer.strategies = [new OpenLayers.Strategy.BBOX()];
+			layer.cmdb_index = geoAttribute.cmdb_index;
+			layer.set("name",  geoAttribute.name);
 
-				layer.CMDBuildLayer = false;
-				layer.CM_Layer = false;
-				layer.CM_geoserverLayer = true;
-				return layer;
-			}
-		};
+			layer.CMDBuildLayer = false;
+			layer.CM_Layer = false;
+			layer.CM_geoserverLayer = true;
+			return layer;
+		}
 
 })();
