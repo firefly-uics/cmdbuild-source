cdff57dcaceaef6da85d1883b1d1683948674f8e
Davide Pavan <d.pavan@tecnoteca.com>
2016-08-02 09:50:14 +0200

    #2222: [Fix] "Privileged" status for user invalidates "impersonate" operation of WorkflowAPI

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/core/fieldManager/builders/String.js
@@ -0,0 +1,77 @@
+(function () {
+
+	/**
+	 * Specific field attributes:
+	 * 		- {Number} length: max number digits
+	 */
+	Ext.define('CMDBuild.core.fieldManager.builders.String', {
+		extend: 'CMDBuild.core.fieldManager.builders.Abstract',
+
+		requires: [
+			'CMDBuild.core.constants.FieldWidths',
+			'CMDBuild.core.constants.Proxy'
+		],
+
+		/**
+		 * @cfg {CMDBuild.core.fieldManager.FieldManager}
+		 */
+		parentDelegate: undefined,
+
+		/**
+		 * @param {Object} parameters
+		 *
+		 * @returns {Ext.grid.column.Column or Object}
+		 */
+		buildColumn: function (parameters) {
+			return this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN) ? {} : Ext.create('Ext.grid.column.Column', {
+				dataIndex: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME),
+				disabled: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
+				editor: parameters.withEditor ? this.buildEditor() : null,
+				flex: 1,
+				hidden: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.SHOW_COLUMN),
+				renderer: this.rendererColumn,
+				scope: this,
+				sortable: true,
+				text: this.applyMandatoryLabelFlag(this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION))
+			});
+		},
+
+		/**
+		 * @returns {Object}
+		 */
+		buildEditor: function () {
+			return this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN) ? {} : {
+				xtype: 'textfield',
+				allowBlank: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
+				disabled: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
+				enforceMaxLength: true,
+				maxLength: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.LENGTH),
+				name: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME),
+				readOnly: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE)
+			};
+		},
+
+		/**
+		 * @returns {Ext.form.field.Text}
+		 */
+		buildField: function () {
+			return Ext.create('Ext.form.field.Text', {
+				allowBlank: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.MANDATORY),
+				disabled: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE),
+				enforceMaxLength: true,
+				fieldLabel: this.applyMandatoryLabelFlag(
+					this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.DESCRIPTION)
+					|| this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME)
+				),
+				hidden: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.HIDDEN),
+				labelAlign: 'right',
+				labelWidth: CMDBuild.core.constants.FieldWidths.LABEL,
+				maxLength: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.LENGTH),
+				maxWidth: CMDBuild.core.constants.FieldWidths.STANDARD_BIG,
+				name: this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.NAME),
+				readOnly: !this.cmfg('fieldManagerAttributeModelGet', CMDBuild.core.constants.Proxy.WRITABLE)
+			});
+		}
+	});
+
+})();
