9271312e8329b18ded9a04556845e22c65ad1b24
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-08-08 12:52:16 +0200

    #2240: Preselection of workflow activities - Workflow UI
    Form panel

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/workflow/panel/form/OperativeInstructionsPanel.js
@@ -0,0 +1,24 @@
+(function () {
+
+	Ext.define('CMDBuild.view.management.workflow.panel.form.OperativeInstructionsPanel', {
+		extend: 'Ext.panel.Panel',
+
+		/**
+		 * @cfg {CMDBuild.controller.management.workflow.panel.form.Form}
+		 */
+		delegate: undefined,
+
+		bodyCls: 'cmdb-blue-panel',
+		border: false,
+		cls: 'cmdb-border-left',
+		collapsed: true,
+		collapsible: true,
+		frame: false,
+		overflowY: 'auto',
+		region: 'east',
+		split: true,
+		title: CMDBuild.Translation.management.modworkflow.activitydocumentation,
+		width: '30%'
+	});
+
+})();
