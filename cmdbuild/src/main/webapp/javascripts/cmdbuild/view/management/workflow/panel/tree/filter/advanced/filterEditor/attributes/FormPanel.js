16a481ec344ee978b40762a6f7374fb5b14a28fc
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-08-08 12:50:54 +0200

    #2240: Preselection of workflow activities - Workflow UI
    Tree panel - Toolbars

---
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/workflow/panel/tree/filter/advanced/filterEditor/attributes/FormPanel.js
@@ -0,0 +1,57 @@
+(function () {
+
+	/**
+	 * @link CMDBuild.view.common.panel.gridAndForm.filter.advanced.filterEditor.attributes.FormPanel
+	 */
+	Ext.define('CMDBuild.view.management.workflow.panel.tree.filter.advanced.filterEditor.attributes.FormPanel', {
+		extend: 'Ext.form.Panel',
+
+		requires: ['CMDBuild.core.constants.Proxy'],
+
+		/**
+		 * @cfg {CMDBuild.controller.common.panel.gridAndForm.filter.advanced.filterEditor.Attributes}
+		 */
+		delegate: undefined,
+
+		/**
+		 * @property {CMDBuild.core.buttons.iconized.split.add.Add}
+		 */
+		addAttributeButton: undefined,
+
+		bodyCls: 'cmdb-blue-panel',
+		border: false,
+		frame: false,
+		overflowY: 'auto',
+
+		layout: {
+			type: 'vbox',
+			align: 'stretch'
+		},
+
+		/**
+		 * @returns {Void}
+		 *
+		 * @override
+		 */
+		initComponent: function () {
+			Ext.apply(this, {
+				dockedItems: [
+					Ext.create('Ext.toolbar.Toolbar', {
+						dock: 'top',
+						itemId: CMDBuild.core.constants.Proxy.TOOLBAR_TOP,
+
+						items: [
+							this.addAttributeButton = Ext.create('CMDBuild.core.buttons.iconized.split.add.Add', {
+								text: CMDBuild.Translation.chooseAnAttribute,
+								disabled: true
+							})
+						]
+					})
+				]
+			});
+
+			this.callParent(arguments);
+		}
+	});
+
+})();
