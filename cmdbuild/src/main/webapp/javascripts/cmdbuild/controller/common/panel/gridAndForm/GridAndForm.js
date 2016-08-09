a9fc1491904e9d59096aee8e700554fd43619060
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-08-09 09:54:36 +0200

    #2240: Preselection of workflow activities - Base panels

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/common/panel/gridAndForm/GridAndForm.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/common/panel/gridAndForm/GridAndForm.js
@@ -1,7 +1,7 @@
 (function () {
 
 	/**
-	 * NOTE: form and grid pointers are required to work with UI state module
+	 * NOTE: "form" and "grid" (or "tree") pointers are required to work with UI state module
 	 *
 	 * @abstract
 	 */
@@ -56,11 +56,11 @@
 
 					Ext.suspendLayouts();
 
-					topPanel.hide();
-					topPanel.region = '';
+					this.panelHide(topPanel);
+					this.panelShowToCenter(this.form);
 
-					this.form.show();
-					this.form.region = 'center';
+					// CSS style setup
+					this.removeStyleClsFromPanel(this.form);
 
 					Ext.resumeLayouts(true);
 				} else {
@@ -80,11 +80,11 @@
 
 					Ext.suspendLayouts();
 
-					topPanel.show();
-					topPanel.region = 'center';
+					this.panelHide(this.form);
+					this.panelShowToCenter(topPanel);
 
-					this.form.hide();
-					this.form.region = '';
+					// CSS style setup
+					this.removeStyleClsFromPanel(topPanel);
 
 					Ext.resumeLayouts(true);
 				} else {
@@ -104,17 +104,122 @@
 
 					Ext.suspendLayouts();
 
-					topPanel.show();
-					topPanel.region = 'center';
+					this.panelShowToCenter(topPanel);
+					this.panelShowToSouth(this.form);
 
-					this.form.show();
-					this.form.region = 'south';
+					// CSS style setup
+					this.addStyleClsToPanel(topPanel, 'bottom');
+					this.addStyleClsToPanel(this.form, 'top');
 
 					Ext.resumeLayouts(true);
 				} else {
 					_warning('onFullScreenChangeToOff(): form or grid property missing', this);
 				}
-			}
+			},
+
+			// Service methods
+				/**
+				 * @param {Ext.panel.Panel} panel
+				 * @param {String} mode - ['top' || 'bottom']
+				 *
+				 * @returns {Void}
+				 *
+				 * @private
+				 */
+				addStyleClsToPanel: function (panel, mode) {
+					if (
+						Ext.isObject(panel) && !Ext.Object.isEmpty(panel)
+						&& Ext.isFunction(panel.addCls)
+					) {
+						switch (mode) {
+							case 'bottom': {
+								if (Ext.isFunction(panel.hasCls) && !panel.hasCls('cmdb-border-bottom'))
+									panel.addCls('cmdb-border-bottom');
+							} break;
+
+							case 'top': {
+								if (Ext.isFunction(panel.hasCls) && !panel.hasCls('cmdb-border-top'))
+									panel.addCls('cmdb-border-top');
+							} break;
+
+							default: {
+								_error('addStyleClsToPanel(): unmanaged type parameter', this, type);
+							}
+						}
+					} else {
+						_error('addStyleClsToPanel(): unmanaged panel parameter', this, panel);
+					}
+				},
+
+				/**
+				 * @param {Ext.panel.Panel} panel
+				 *
+				 * @returns {Void}
+				 *
+				 * @private
+				 */
+				panelHide: function (panel) {
+					if (Ext.isObject(panel) && !Ext.Object.isEmpty(panel)) {
+						panel.hide();
+						panel.region = '';
+					} else {
+						_error('panelHide(): unmanaged panel parameter', this, panel);
+					}
+				},
+
+				/**
+				 * @param {Ext.panel.Panel} panel
+				 *
+				 * @returns {Void}
+				 *
+				 * @private
+				 */
+				panelShowToCenter: function (panel) {
+					if (Ext.isObject(panel) && !Ext.Object.isEmpty(panel)) {
+						panel.show();
+						panel.region = 'center';
+					} else {
+						_error('panelHide(): unmanaged panel parameter', this, panel);
+					}
+				},
+
+				/**
+				 * @param {Ext.panel.Panel} panel
+				 *
+				 * @returns {Void}
+				 *
+				 * @private
+				 */
+				panelShowToSouth: function (panel) {
+					if (Ext.isObject(panel) && !Ext.Object.isEmpty(panel)) {
+						panel.show();
+						panel.region = 'south';
+					} else {
+						_error('panelHide(): unmanaged panel parameter', this, panel);
+					}
+				},
+
+				/**
+				 * @param {Ext.panel.Panel} panel
+				 *
+				 * @returns {Void}
+				 *
+				 * @private
+				 */
+				removeStyleClsFromPanel: function (panel) {
+					if (
+						Ext.isObject(panel) && !Ext.Object.isEmpty(panel)
+						&& Ext.isFunction(panel.removeCls)
+					) {
+						if (Ext.isFunction(panel.hasCls) && panel.hasCls('cmdb-border-bottom'))
+							panel.removeCls('cmdb-border-bottom');
+
+						if (Ext.isFunction(panel.hasCls) && panel.hasCls('cmdb-border-top'))
+							panel.removeCls('cmdb-border-top');
+					} else {
+						_error('removeStyleClsFromPanel(): unmanaged panel parameter', this, panel);
+					}
+				}
 	});
 
 })();
