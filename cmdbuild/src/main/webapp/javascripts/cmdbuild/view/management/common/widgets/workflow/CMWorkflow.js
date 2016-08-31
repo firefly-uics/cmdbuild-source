2399faa6ad169f1bdd03a9c13b0ce3e609af13d9
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-08-31 09:43:58 +0200

    #2240: Preselection of workflow activities - Workflow UI
    Widget workflow fixes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/common/widgets/workflow/CMWorkflow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/common/widgets/workflow/CMWorkflow.js
@@ -1,47 +1,6 @@
 (function() {
-	var queryWorkflowStore = Ext.create('Ext.data.Store', {
-	    fields: ['Code', 'Description'],
-	    data : [
-	    ]
-	});
-	Ext.define("CMDBuild.view.management.common.widgets.CMWorkflowCombo", {
-		extend: "Ext.panel.Panel",
-		panel: undefined,
-		bodyCls: "x-panel-body-default-framed",
-		frame : true,
-		border: true,
-		initComponent: function() {
-			this.queryWorkflow = Ext.create('Ext.form.ComboBox', {
-			    fieldLabel: CMDBuild.Translation.workflow,
-				labelWidth: CMDBuild.core.constants.FieldWidths.LABEL,
-				width: CMDBuild.core.constants.FieldWidths.ADMINISTRATION_BIG,
-				labelAlign: "right",
-			    store: queryWorkflowStore,
-			    queryMode: 'local',
-			    displayField: 'Description',
-			    valueField: 'Code',
-			    autoSelect: true,
-			    listeners:{
-			         scope: this.panel,
-			         'select': function(item, param) {
-			        	 this.delegate.changeWorkflow(param[0].get("Code"));
-			         }
-			    }
-			});
-			this.items = [this.queryWorkflow];
-			this.callParent(arguments);
-		},
-		clearComboValues: function() {
-			queryWorkflowStore.removeAll(true);
-		},
-		loadComboValues: function(data) {
-			queryWorkflowStore.loadRawData(data);
-		},
-		clearCombo: function() {
-			this.queryWorkflow.setValue("");
-		}
-	});
-	Ext.define("CMDBuild.view.management.common.widgets.CMWorkflow", {
+
+	Ext.define("CMDBuild.view.management.common.widgets.workflow.CMWorkflow", {
 		extend: "Ext.panel.Panel",
 
 		statics: {
@@ -58,7 +17,7 @@
 					saveButtonClick: "cm-save-click",
 					advanceButtonClick: "cm-advance-click"
 			};
-			this.comboPanel = Ext.create("CMDBuild.view.management.common.widgets.CMWorkflowCombo", {
+			this.comboPanel = Ext.create("CMDBuild.view.management.common.widgets.workflow.CMWorkflowCombo", {
 				region : 'north',
 				panel: this,
 				hidden: true
