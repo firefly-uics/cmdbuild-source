3d6d3243f5759b6a751b8fcb710ae4ce452bf5c2
Davide Pavan <d.pavan@tecnoteca.com>
2016-09-06 10:46:05 +0200

    #2273: Metadata for functions through REST interface
    Details.

--- services/rest/v2/src/test/java/unit/cxf/CxfFunctionsTest.java
+++ services/rest/v2/src/test/java/unit/cxf/CxfFunctionsTest.java
@@ -4,8 +4,7 @@ import static com.google.common.collect.Iterables.get;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyMap;
 import static org.cmdbuild.service.rest.v2.model.Models.newAttribute;
-import static org.cmdbuild.service.rest.v2.model.Models.newFunctionWithBasicDetails;
-import static org.cmdbuild.service.rest.v2.model.Models.newFunctionWithFullDetails;
+import static org.cmdbuild.service.rest.v2.model.Models.newFunction;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
@@ -29,8 +28,7 @@ import org.cmdbuild.dao.view.CMDataView;
 import org.cmdbuild.service.rest.v2.cxf.CxfFunctions;
 import org.cmdbuild.service.rest.v2.cxf.ErrorHandler;
 import org.cmdbuild.service.rest.v2.model.Attribute;
-import org.cmdbuild.service.rest.v2.model.FunctionWithBasicDetails;
-import org.cmdbuild.service.rest.v2.model.FunctionWithFullDetails;
+import org.cmdbuild.service.rest.v2.model.Function;
 import org.cmdbuild.service.rest.v2.model.ResponseMultiple;
 import org.cmdbuild.service.rest.v2.model.ResponseSingle;
 import org.junit.Before;
@@ -56,32 +54,54 @@ public class CxfFunctionsTest {
 				.when(dataView).findAllFunctions();
 
 		// when
-		underTest.readAll(123, 456, null);
+		underTest.readAll(null, null, null, false);
 	}
 
 	@Test
 	public void allFunctionsReturned() throws Exception {
 		// given
-		final CMFunction _1 = function(1L);
-		final CMFunction _2 = function(2L);
-		final CMFunction _3 = function(3L);
+		final CMFunction _1 = function(1L, ChainablePutMap.of(new HashMap<String, Object>()) //
+				.chainablePut("foo", "FOO"));
+		final CMFunction _2 = function(2L, ChainablePutMap.of(new HashMap<String, Object>()) //
+				.chainablePut("bar", "BAR"));
+		final CMFunction _3 = function(3L, ChainablePutMap.of(new HashMap<String, Object>()) //
+				.chainablePut("baz", "BAZ"));
+		doReturn(asList(_1, _2, _3)) //
+				.when(dataView).findAllFunctions();
 		doReturn(asList(_1, _2, _3)) //
 				.when(dataView).findAllFunctions();
 
 		// when
-		final ResponseMultiple<FunctionWithBasicDetails> response = underTest.readAll(null, null, null);
+		final ResponseMultiple<Function> responseWithNoDetails = underTest.readAll(null, null, null, false);
 
 		// then
-		assertThat(response.getMetadata().getTotal(), equalTo(3L));
-		assertThat(response.getElements(), hasSize(3));
-		assertThat(get(response.getElements(), 0),
-				equalTo(newFunctionWithBasicDetails() //
+		assertThat(responseWithNoDetails.getMetadata().getTotal(), equalTo(3L));
+		assertThat(responseWithNoDetails.getElements(), hasSize(3));
+		assertThat(get(responseWithNoDetails.getElements(), 0),
+				equalTo(newFunction() //
 						.withId(_1.getId()) //
 						.withName(_1.getName()) //
 						.withDescription(_1.getName()) //
 						.build()));
-		assertThat(get(response.getElements(), 1).getId(), equalTo(_2.getId()));
-		assertThat(get(response.getElements(), 2).getId(), equalTo(_3.getId()));
+		assertThat(get(responseWithNoDetails.getElements(), 1).getId(), equalTo(_2.getId()));
+		assertThat(get(responseWithNoDetails.getElements(), 2).getId(), equalTo(_3.getId()));
+
+		// when
+		final ResponseMultiple<Function> responseWithFullDetails = underTest.readAll(null, null, null, true);
+
+		// then
+		assertThat(responseWithFullDetails.getMetadata().getTotal(), equalTo(3L));
+		assertThat(responseWithFullDetails.getElements(), hasSize(3));
+		assertThat(get(responseWithFullDetails.getElements(), 0),
+				equalTo(newFunction() //
+						.withId(_1.getId()) //
+						.withName(_1.getName()) //
+						.withDescription(_1.getName()) //
+						.withMetadata(ChainablePutMap.of(new HashMap<String, Object>()) //
+								.chainablePut("foo", "FOO")) //
+						.build()));
+		assertThat(get(responseWithFullDetails.getElements(), 1).getId(), equalTo(_2.getId()));
+		assertThat(get(responseWithFullDetails.getElements(), 2).getId(), equalTo(_3.getId()));
 	}
 
 	@Test
@@ -94,7 +114,7 @@ public class CxfFunctionsTest {
 				.when(dataView).findAllFunctions();
 
 		// when
-		final ResponseMultiple<FunctionWithBasicDetails> response = underTest.readAll(null, null,
+		final ResponseMultiple<Function> response = underTest.readAll(null, null,
 				"" //
 						+ "{" //
 						+ "	\"attribute\": {" //
@@ -104,13 +124,14 @@ public class CxfFunctionsTest {
 						+ "			\"value\": [\"2\"]" //
 						+ "		}" //
 						+ "	}" //
-						+ "}");
+						+ "}",
+				false);
 
 		// then
 		assertThat(response.getMetadata().getTotal(), equalTo(1L));
 		assertThat(response.getElements(), hasSize(1));
 		assertThat(get(response.getElements(), 0),
-				equalTo(newFunctionWithBasicDetails() //
+				equalTo(newFunction() //
 						.withId(_2.getId()) //
 						.withName(_2.getName()) //
 						.withDescription(_2.getName()) //
@@ -129,13 +150,13 @@ public class CxfFunctionsTest {
 				.when(dataView).findAllFunctions();
 
 		// when
-		final ResponseMultiple<FunctionWithBasicDetails> response = underTest.readAll(2, 1, null);
+		final ResponseMultiple<Function> response = underTest.readAll(2, 1, null, false);
 
 		// then
 		assertThat(response.getMetadata().getTotal(), equalTo(5L));
 		assertThat(response.getElements(), hasSize(2));
 		assertThat(get(response.getElements(), 0),
-				equalTo(newFunctionWithBasicDetails() //
+				equalTo(newFunction() //
 						.withId(_2.getId()) //
 						.withName(_2.getName()) //
 						.withDescription(_2.getName()) //
@@ -182,11 +203,11 @@ public class CxfFunctionsTest {
 				.when(dataView).findAllFunctions();
 
 		// when
-		final ResponseSingle<FunctionWithFullDetails> response = underTest.read(2L);
+		final ResponseSingle<Function> response = underTest.read(2L);
 
 		// then
 		assertThat(response.getElement(),
-				equalTo(newFunctionWithFullDetails() //
+				equalTo(newFunction() //
 						.withId(_2.getId()) //
 						.withName(_2.getName()) //
 						.withDescription(_2.getName()) //
