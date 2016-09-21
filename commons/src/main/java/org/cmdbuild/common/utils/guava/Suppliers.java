b4f3e9d50844fd54ced99d6378047f29a87aa9b4
Davide Pavan <d.pavan@tecnoteca.com>
2016-09-21 14:08:59 +0200

    #2298: [Fix] When updating an activity of a process instance and the activity is not found an unclear error is returned

--- commons/src/main/java/org/cmdbuild/common/utils/guava/Suppliers.java
+++ commons/src/main/java/org/cmdbuild/common/utils/guava/Suppliers.java
@@ -52,6 +52,25 @@ public class Suppliers {
 		return new FirstNotNull<T>(delegates);
 	}
 
+	private static class ExceptionSupplier<T> implements Supplier<T> {
+
+		private final RuntimeException delegate;
+
+		public ExceptionSupplier(final RuntimeException delegate) {
+			this.delegate = delegate;
+		}
+
+		@Override
+		public T get() {
+			throw delegate;
+		}
+
+	}
+
+	public static <T> Supplier<T> exception(final RuntimeException exception) {
+		return new ExceptionSupplier<T>(exception);
+	}
+
 	private Suppliers() {
 		// prevents instantiation
 	}
