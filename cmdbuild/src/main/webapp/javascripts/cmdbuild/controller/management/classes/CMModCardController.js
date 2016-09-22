cdb55f9c7563fd617440b3b0a4a9346f5d66662e
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-09-22 10:53:19 +0200

    #2303: [Fix] DataViews from filter clear filter fixes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/classes/CMModCardController.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/management/classes/CMModCardController.js
@@ -32,6 +32,8 @@
 				var filter = entryType.get(CMDBuild.core.constants.Proxy.FILTER);
 				var newEntryId = entryType.get(idPropertyName);
 
+				this.selectedAccordionNode = entryType; // FIXME: hack to temporary fix DataView bug
+
 				// If we haven't a filter try to get default one from server
 				if (Ext.isEmpty(filter)) {
 					var params = {};
@@ -470,6 +472,17 @@
 			_CMUIState.onlyGridIfFullScreen();
 
 			this.changeClassUIConfigurationForGroup(entryTypeId);
+
+			// FIXME: hack to temporary fix DataView bug
+			if (
+				Ext.isString(filter) && !Ext.isEmpty(filter)
+				&& Ext.Array.contains(this.selectedAccordionNode.get(CMDBuild.core.constants.Proxy.SECTION_HIERARCHY), 'filter')
+			) {
+				CMDBuild.global.dataViewHack = {
+					filter: filter,
+					entryType: entryType
+				};
+			}
 		}
 	});
 
