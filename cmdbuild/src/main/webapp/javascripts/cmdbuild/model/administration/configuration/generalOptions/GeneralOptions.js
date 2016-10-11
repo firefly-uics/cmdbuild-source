9013077484431619358e09aac5a4423710f1ecd0
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-10-11 10:48:21 +0200

    #2221: Notification of errors by email
    Fix

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/model/administration/configuration/generalOptions/GeneralOptions.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/model/administration/configuration/generalOptions/GeneralOptions.js
@@ -9,17 +9,17 @@
 		extend: 'Ext.data.Model',
 
 		fields: [
-			{ name: CMDBuild.core.constants.Proxy.CARD_FORM_RATIO, type: 'int', defaultValue: 50 },
-			{ name: CMDBuild.core.constants.Proxy.CARD_LOCK_TIMEOUT, type: 'int', defaultValue: 300 },
-			{ name: CMDBuild.core.constants.Proxy.CARD_TABS_POSITION, type: 'string', defaultValue: 'bottom' },
+			{ name: CMDBuild.core.constants.Proxy.CARD_FORM_RATIO, type: 'int' },
+			{ name: CMDBuild.core.constants.Proxy.CARD_LOCK_TIMEOUT, type: 'int' },
+			{ name: CMDBuild.core.constants.Proxy.CARD_TABS_POSITION, type: 'string' },
 			{ name: CMDBuild.core.constants.Proxy.DISPLAY_CARD_LOCKER_NAME, type: 'boolean' },
-			{ name: CMDBuild.core.constants.Proxy.ENABLE_CARD_LOCK, type: 'boolean', defaultValue: true },
+			{ name: CMDBuild.core.constants.Proxy.ENABLE_CARD_LOCK, type: 'boolean' },
 			{ name: CMDBuild.core.constants.Proxy.INSTANCE_NAME, type: 'string' },
-			{ name: CMDBuild.core.constants.Proxy.POPUP_HEIGHT_PERCENTAGE, type: 'int', defaultValue: 80 },
-			{ name: CMDBuild.core.constants.Proxy.POPUP_WIDTH_PERCENTAGE, type: 'int', defaultValue: 80 },
-			{ name: CMDBuild.core.constants.Proxy.REFERENCE_COMBO_STORE_LIMIT, type: 'int', defaultValue: 500 }, // Limit of dropDown displayed relations before force pop-up window show
-			{ name: CMDBuild.core.constants.Proxy.RELATION_LIMIT, type: 'int', defaultValue: 20 },
-			{ name: CMDBuild.core.constants.Proxy.ROW_LIMIT, type: 'int', defaultValue: 20 }, // Rows per page to display in card's grid
+			{ name: CMDBuild.core.constants.Proxy.POPUP_HEIGHT_PERCENTAGE, type: 'int' },
+			{ name: CMDBuild.core.constants.Proxy.POPUP_WIDTH_PERCENTAGE, type: 'int' },
+			{ name: CMDBuild.core.constants.Proxy.REFERENCE_COMBO_STORE_LIMIT, type: 'int' }, // Limit of dropDown displayed relations before force pop-up window show
+			{ name: CMDBuild.core.constants.Proxy.RELATION_LIMIT, type: 'int' },
+			{ name: CMDBuild.core.constants.Proxy.ROW_LIMIT, type: 'int' }, // Rows per page to display in card's grid
 			{ name: CMDBuild.core.constants.Proxy.SESSION_TIMEOUT, type: 'int', useNull: true },
 			{ name: CMDBuild.core.constants.Proxy.STARTING_CLASS, type: 'int', useNull: true } // Default selected class on UI display
 		],
