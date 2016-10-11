3714790a4cc178fac027387ee8465b1ac5e3a1cf
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-10-11 10:08:53 +0200

    #2240: Preselection of workflow activities - Workflow UI
    Performance optimizations (workflow)
    Temporary rollback

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/administration/workflow/Workflow.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/controller/administration/workflow/Workflow.js
@@ -132,36 +132,64 @@
 		 *
 		 * @override
 		 */
-		onWorkflowModuleInit: function (node) {
+		onWorkflowModuleInit: function (node) { // FIXME: waiting for refactor (server)
 			this.cmfg('workflowSelectedWorkflowReset');
 
 			if (Ext.isObject(node) && !Ext.Object.isEmpty(node)) {
 				var params = {};
-				params[CMDBuild.core.constants.Proxy.ID] = node.get(CMDBuild.core.constants.Proxy.ENTITY_ID);
+				params[CMDBuild.core.constants.Proxy.ACTIVE] = false;
+//				params[CMDBuild.core.constants.Proxy.ID] = node.get(CMDBuild.core.constants.Proxy.ENTITY_ID);
 
-				CMDBuild.proxy.administration.workflow.Workflow.readById({
+				CMDBuild.proxy.administration.workflow.Workflow.getAll({
 					params: params,
 					scope: this,
 					success: function (response, options, decodedResponse) {
-						decodedResponse = decodedResponse[CMDBuild.core.constants.Proxy.RESPONSE];
+//						decodedResponse = decodedResponse[CMDBuild.core.constants.Proxy.RESPONSE];
+						decodedResponse = decodedResponse[CMDBuild.core.constants.Proxy.CLASSES];
 
-						if (Ext.isObject(decodedResponse) && !Ext.Object.isEmpty(decodedResponse)) {
-							this.workflowSelectedWorkflowSet({ value: decodedResponse });
+						if (Ext.isArray(decodedResponse) && !Ext.isEmpty(decodedResponse)) {
+							var selectedWorkflow = Ext.Array.findBy(decodedResponse, function (workflowObject, i) {
+								return node.get(CMDBuild.core.constants.Proxy.ENTITY_ID) == workflowObject[CMDBuild.core.constants.Proxy.ID];
+							}, this);
 
-							this.setViewTitle(this.cmfg('workflowSelectedWorkflowGet', CMDBuild.core.constants.Proxy.DESCRIPTION));
+							if (Ext.isObject(selectedWorkflow) && !Ext.Object.isEmpty(selectedWorkflow)) {
+								this.workflowSelectedWorkflowSet({ value: selectedWorkflow });
 
-							this.cmfg('onWorkflowWokflowSelected');
+								this.setViewTitle(this.cmfg('workflowSelectedWorkflowGet', CMDBuild.core.constants.Proxy.DESCRIPTION));
 
-							// Manage tab selection
-							if (Ext.isEmpty(this.tabPanel.getActiveTab()))
-								this.tabPanel.setActiveTab(0);
+								this.cmfg('onWorkflowWokflowSelected');
 
-							this.tabPanel.getActiveTab().fireEvent('show'); // Manual show event fire because was already selected
+								// Manage tab selection
+								if (Ext.isEmpty(this.tabPanel.getActiveTab()))
+									this.tabPanel.setActiveTab(0);
 
-							this.onModuleInit(node); // Custom callParent() implementation
+								this.tabPanel.getActiveTab().fireEvent('show'); // Manual show event fire because was already selected
+
+								this.onModuleInit(node); // Custom callParent() implementation
+							} else {
+								_error('onWorkflowModuleInit(): workflow not found', this, node.get(CMDBuild.core.constants.Proxy.ENTITY_ID));
+							}
 						} else {
 							_error('onWorkflowModuleInit(): unmanaged response', this, decodedResponse);
 						}
+
+//						if (Ext.isObject(decodedResponse) && !Ext.Object.isEmpty(decodedResponse)) {
+//							this.workflowSelectedWorkflowSet({ value: decodedResponse });
+//
+//							this.setViewTitle(this.cmfg('workflowSelectedWorkflowGet', CMDBuild.core.constants.Proxy.DESCRIPTION));
+//
+//							this.cmfg('onWorkflowWokflowSelected');
+//
+//							// Manage tab selection
+//							if (Ext.isEmpty(this.tabPanel.getActiveTab()))
+//								this.tabPanel.setActiveTab(0);
+//
+//							this.tabPanel.getActiveTab().fireEvent('show'); // Manual show event fire because was already selected
+//
+//							this.onModuleInit(node); // Custom callParent() implementation
+//						} else {
+//							_error('onWorkflowModuleInit(): unmanaged response', this, decodedResponse);
+//						}
 					}
 				});
 			} else {
