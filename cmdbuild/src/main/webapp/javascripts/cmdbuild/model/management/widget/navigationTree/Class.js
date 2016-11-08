ca4c5a1c19259e36b618f31838c892f7f13d8bb7
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-11-08 16:11:58 +0100

    #2345: Widget NavigationTree: destination class description added to tree nodes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/model/management/widget/navigationTree/Class.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/model/management/widget/navigationTree/Class.js
@@ -2,14 +2,29 @@
 
 	Ext.require('CMDBuild.core.constants.Proxy');
 
-	Ext.define('CMDBuild.model.management.widget.navigationTree.Class', {
+	Ext.define('CMDBuild.model.management.widget.navigationTree.Class', { // FIXME: waiting for refactor (rename)
 		extend: 'Ext.data.Model',
 
 		fields: [
+			{ name: CMDBuild.core.constants.Proxy.DESCRIPTION, type: 'string' },
 			{ name: CMDBuild.core.constants.Proxy.ID, type: 'int', useNull: true },
 			{ name: CMDBuild.core.constants.Proxy.NAME, type: 'string' },
 			{ name: CMDBuild.core.constants.Proxy.PARENT, type: 'int', useNull: true }
-		]
+		],
+
+		/**
+		 * @param {Object} data
+		 *
+		 * @returns {Void}
+		 *
+		 * @override
+		 */
+		constructor: function (data) {
+			data = Ext.isObject(data) ? data : {};
+			data[CMDBuild.core.constants.Proxy.DESCRIPTION] = data['text'];
+
+			this.callParent(arguments);
+		}
 	});
 
 })();
