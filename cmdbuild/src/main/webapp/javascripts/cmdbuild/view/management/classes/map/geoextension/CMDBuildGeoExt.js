395ba790d27060f3ff7b126122b7a3e301764a9b
Marco Cepparo <m.cepparo@tecnoteca.com>
2016-11-23 12:52:57 +0100

    #2358: Map Controls

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/classes/map/geoextension/CMDBuildGeoExt.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/view/management/classes/map/geoextension/CMDBuildGeoExt.js
@@ -15,25 +15,19 @@
 		initBaseLayer : function() {
 			var baseLayer = undefined;
 			if (CMDBuild.configuration.gis.get([ CMDBuild.gis.constants.MAP_OSM, CMDBuild.gis.constants.ENABLED ])) {
-				var osm_source = new ol.source.OSM({
-					url : 'http://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				});
+				var osm_source = new ol.source.OSM();
 				baseLayer = new ol.layer.Tile({
 					source : osm_source
 				});
 			} else if (CMDBuild.configuration.gis.get([ CMDBuild.gis.constants.MAP_GOOGLE,
 					CMDBuild.gis.constants.ENABLED ])) {
-				var osm_source = new ol.source.OSM({
-					url : 'http://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				});
+				var osm_source = new ol.source.OSM();
 				baseLayer = new ol.layer.Tile({
 					source : osm_source
 				});
 			} else if (CMDBuild.configuration.gis.get([ CMDBuild.gis.constants.MAP_YAHOO,
 					CMDBuild.gis.constants.ENABLED ])) {
-				var osm_source = new ol.source.OSM({
-					url : 'http://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-				});
+				var osm_source = new ol.source.OSM();
 				baseLayer = new ol.layer.Tile({
 					source : osm_source
 				});
