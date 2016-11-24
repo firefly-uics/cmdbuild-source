c56f638cf63692bb9ac6cbe57059203f64e761f8
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-11-24 09:48:02 +0100

    #2327: Client libraries update
    FilterChooser remove

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/model/common/field/filter/advanced/window/Configuration.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/model/common/field/filter/advanced/window/Configuration.js
@@ -7,9 +7,10 @@
 
 		fields: [
 			{ name: CMDBuild.core.constants.Proxy.ATTRIBUTES_PRIVILEGES, type: 'auto', defaultValue: [] },
-			{ name: CMDBuild.core.constants.Proxy.CLASS_NAME, type: 'string' },
 			{ name: CMDBuild.core.constants.Proxy.CLASS_DESCRIPTION, type: 'string' },
+			{ name: CMDBuild.core.constants.Proxy.CLASS_NAME, type: 'string' },
 			{ name: CMDBuild.core.constants.Proxy.DISABLED_FEATURES, type: 'auto', defaultValue: [] }, // Managed values: 'inputParameter'
+			{ name: CMDBuild.core.constants.Proxy.DISABLED_PANELS, type: 'auto', defaultValue: [] }, // Managed values: 'attributes', 'relations', 'functions', 'columnPrivileges'
 			{ name: CMDBuild.core.constants.Proxy.MODE, type: 'string', defaultValue: 'field' } // Managed values: 'field', 'grid'
 		]
 	});
