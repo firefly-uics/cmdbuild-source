b83950d963c140d7a3fffe21d97c280451a73b92
Alessandro Romanin <a.romanin@tecnoteca.com>
2016-12-01 09:30:50 +0100

    #2327: Client libraries update
    Store errors fixes

--- cmdbuild/src/main/webapp/javascripts/cmdbuild/override/data/Store.js
+++ cmdbuild/src/main/webapp/javascripts/cmdbuild/override/data/Store.js
@@ -2,12 +2,39 @@
 
 	Ext.require([
 		'CMDBuild.core.interfaces.messages.Error',
-		'CMDBuild.core.interfaces.messages.Warning',
-		'CMDBuild.core.Message'
+		'CMDBuild.core.interfaces.messages.Warning'
 	]);
 
 	Ext.define('CMDBuild.override.data.Store', {
 		override: 'Ext.data.Store',
+
+		/**
+		 * Creates callback interceptor to print error message on store load - 02/10/2015
+		 *
+		 * @param {Array} records
+		 * @param {Ext.data.Operation} operation
+		 * @param {Boolean} success
+		 *
+		 * @returns {Boolean}
+		 *
+		 * @private
+		 */
+		callbackInterceptor: function (records, operation, success) {
+			var decodedResponse = {};
+
+			if (!Ext.isEmpty(operation) && !Ext.isEmpty(operation.response) && !Ext.isEmpty(operation.response.responseText))
+				decodedResponse = CMDBuild.core.interfaces.Ajax.decodeJson(operation.response.responseText);
+
+			if (Ext.isFunction(CMDBuild.global.interfaces.Configurations.get) && !CMDBuild.global.interfaces.Configurations.get('disableAllMessages')) {
+				if (!CMDBuild.global.interfaces.Configurations.get('disableWarnings'))
+					CMDBuild.core.interfaces.messages.Warning.display(decodedResponse);
+
+				if (!CMDBuild.global.interfaces.Configurations.get('disableErrors'))
+					CMDBuild.core.interfaces.messages.Error.display(decodedResponse, operation.request);
+			}
+
+			return true;
+		},
 
 		/**
 		 * Creates callback interceptor to print error message on store load - 02/10/2015
@@ -19,48 +46,18 @@
 		 * @override
 		 */
 		load: function (options) {
-			if (!Ext.isEmpty(CMDBuild.global) && !Ext.isEmpty(CMDBuild.global.Data))
+			options = Ext.isObject(options) ? options : {};
+			options.callback = Ext.isFunction(options.callback) ? options.callback : Ext.emptyFn;
+
+			Ext.ns('CMDBuild.global.Data');
+			Ext.ns('CMDBuild.global.interfaces.Configurations');
+
+			if (Ext.isFunction(CMDBuild.global.Data.dataDefaultHeadersUpdate))
 				CMDBuild.global.Data.dataDefaultHeadersUpdate();
 
-			if (!Ext.isEmpty(options)) {
-				options.callback = Ext.isEmpty(options.callback) || !Ext.isFunction(options.callback) ? Ext.emptyFn : options.callback;
-				options.callback = Ext.Function.createInterceptor(options.callback, this.interceptorFunction, this);
-			}
+			options.callback = Ext.Function.createInterceptor(options.callback, this.callbackInterceptor, this);
 
-			this.callParent(arguments);
-		},
-
-		/**
-		 * @param {Array} records
-		 * @param {Ext.data.Operation} operation
-		 * @param {Boolean} success
-		 *
-		 * @returns {Boolean}
-		 *
-		 * @private
-		 */
-		interceptorFunction: function (records, operation, success) {
-			var decodedResponse = undefined;
-
-			if (!success) {
-				if (
-					!Ext.isEmpty(operation)
-					&& !Ext.isEmpty(operation.response)
-					&& !Ext.isEmpty(operation.response.responseText)
-				) {
-					decodedResponse = Ext.decode(operation.response.responseText);
-				}
-
-				if (!CMDBuild.global.interfaces.Configurations.get('disableAllMessages')) {
-					if (!CMDBuild.global.interfaces.Configurations.get('disableWarnings'))
-						CMDBuild.core.interfaces.messages.Warning.display(decodedResponse);
-
-					if (!CMDBuild.global.interfaces.Configurations.get('disableErrors'))
-						CMDBuild.core.interfaces.messages.Error.display(decodedResponse, operation.request);
-				}
-			}
-
-			return true;
+			this.callParent([options]);
 		}
 	});
 
